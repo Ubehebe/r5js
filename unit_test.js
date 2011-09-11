bad7f6fd2b222e2b8043daaa9ec373cb4014e1a0
Brendan Linn <brendan.linn@gmail.com>
2011-09-11 09:19:36 -0700

    added more parse unit tests. the parser passes them all.

--- unit_test.js
+++ unit_test.js
@@ -79,118 +79,182 @@ function testParser() {
     var tests = {};
 
     tests['variable'] = {
-	'...': true, 
-	'+': true,
-	'-': true,
-	'x': true,
-	'=>': false,
-	'cond': false,
-	'(': false};
+        '...': true,
+        '+': true,
+        '-': true,
+        'x': true,
+        '=>': false,
+        'cond': false,
+        '(': false};
 
     tests['quotation'] = {
-	"'1": true,
-	"''1": true,
-	'(quote quote)': true,
-	"'()": true,
-	'(quote ())': true,
-	"'quote": true,
-	'quote': false,
-	"''": false
+        "'1": true,
+        "''1": true,
+        '(quote quote)': true,
+        "'()": true,
+        '(quote ())': true,
+        "'quote": true,
+        'quote': false,
+        "''": false
     };
 
     tests['self-evaluating'] = {
-	'#t': true,
-	'1': true,
-	'#\\a': true,
-	'#\\space': true,
-	'3.14159': true,
-	'"hello, world"': true,
-	'"(define foo x y)"': true,
-	'(define foo (+ 1 2))': false,
-	'+': false
+        '#t': true,
+        '1': true,
+        '#\\a': true,
+        '#\\space': true,
+        '3.14159': true,
+        '"hello, world"': true,
+        '"(define foo x y)"': true,
+        '(define foo (+ 1 2))': false,
+        '+': false
     };
 
     tests['procedure-call'] = {
-	'(+)': true,
-	'+': false,
-	'(foo x': false,
-	'foo x)': false, 
-	'()': false,
-	'(define x)': false,
-	'(foo x y . z)': false,
-	'((foo) (foo))': true,
-	'((define) foo)': false,
-	'((lambda () +) 1 2)': true
+        '(+)': true,
+        '+': false,
+        '(foo x': false,
+        'foo x)': false,
+        '()': false,
+        '(define x)': false,
+        '(foo x y . z)': false,
+        '((foo) (foo))': true,
+        '((define) foo)': true,
+        '((define) define)': false,
+        '((lambda () +) 1 2)': true
     };
 
     tests['lambda-expression'] = {
-	'(lambda () 1)': true,
-	'(lambda x 1)': true,
-	'(lambda (x y) (x y))': true,
-	'(lambda (x y))': false,
-	'(lambda (x . y) z)': true,
-	'(lambda x . y z)': false,
-	'(lambda lambda)': false
+        '(lambda () 1)': true,
+        '(lambda x 1)': true,
+        '(lambda (x y) (x y))': true,
+        '(lambda (x y))': false,
+        '(lambda (x . y) z)': true,
+        '(lambda x . y z)': false,
+        '(lambda lambda)': false
+    };
+
+    tests['formals'] = {
+        '(x y z)': true,
+        'x': true,
+        '(x . z)': true,
+        '( . x)': false,
+        '(x . y . z)': false
     };
 
     tests['definition'] = {
-	'(define x x)': true,
-	'(define (foo x y) (foo x y))': true,
-	'(begin (define x x) (define y y))': true,
-	'(define (x . y) 1)': true,
-	'define': false,
-	'(define)': false,
-	'(define x)': false,
-	'(begin 1)': false,
-	'(begin ())': false,
-	'(begin)': true
+        '(define x x)': true,
+        '(define define 1)': false,
+        '(define (foo x y) (foo x y))': true,
+        '(begin (define x x) (define y y))': true,
+        '(define (x . y) 1)': true,
+        'define': false,
+        '(define)': false,
+        '(define x)': false,
+        '(begin 1)': false,
+        '(begin ())': false,
+        '(begin)': true,
+        '(define (x) (define y 1) x)': true,
+        '(begin (define x 1) (define y 2))': true
     };
 
     tests['conditional'] = {
-	'(if x y z)': true,
-	'(if x y)': true,
-	'(if x)': false,
-	'(if)': false,
-	'if': false,
-	'(IF x y)': true
+        '(if x y z)': true,
+        '(if x y)': true,
+        '(if x)': false,
+        '(if)': false,
+        'if': false,
+        '(IF x y)': true,
+        '(if x (define x 1))': true
     };
 
     tests['assignment'] = {
-	'(set! let! met!)': true,
-	'(set!)': false,
-	'(set! set!)': false, 
-	'(set! x)': false
+        '(set! let! met!)': true,
+        '(set!)': false,
+        '(set! set!)': false,
+        '(set! x)': false
     };
 
-    function assertSuccess(text, result) {
-        if (result.fail) {
-            console.log('failed parsing ' + text + ':');
-            console.log(result);
-        }
-    }
+    tests['cond-clause'] = {
+        '(1 2 3)': true,
+        '(1)': true,
+        '(x => (x x x))': true,
+        '1': false,
+        '(x => =>)': false
+    };
 
-    function assertFailure(text, result) {
-        if (!result.fail) {
-            console.log('mistakenly parsed malformed text ' + text + ':');
-            console.log(result);
-        }
-    }
+    tests['case-clause'] = {
+        "(('1 '1 '()) 1)": true,
+        '(() 1)': true,
+        '((x y))': false
+    };
+
+    tests['binding-spec'] = {
+        '(x 1)': true,
+        '(1 x)': false,
+        '(define x)': false,
+        '(x define)': false,
+        '(x (define))': true
+    };
+
+    tests['iteration-spec'] = {
+        '(x 1 1)': true,
+        '(1 x 1)': false,
+        '(x 1)': true,
+        '(1 x)': false
+    };
+
+    tests['transformer-spec'] = {
+      '(syntax-rules ())': true,
+        '(syntax-rules)': false
+    };
+
+    tests['pattern-identifier'] = {
+        'define': true,
+        '...': false,
+        'x': true
+    };
+
+    tests['pattern'] = {
+        '()': true,
+        '(define)': true,
+        '(define ...)': true,
+        '(define . define)': true,
+        '(define . ...)': false,
+        '(...)': false,
+        '#()': true,
+        '#(define ...)': true
+    };
+
+    tests['pattern-datum'] = {
+        'x': false,
+        '"x"': true,
+        "'x": false
+    };
+
+    tests['template'] = {
+        '()': true,
+        '#()': true,
+        '(x...)': true,
+        '(x... . x)': true,
+        '(x... y...)': true
+    };
 
     var numErrors = 0;
     var numTests = 0;
 
     for (var type in tests) {
-	var testsForType = tests[type];
+        var testsForType = tests[type];
         for (var toParse in testsForType) {
-	    var datumRoot = new Reader(toParse).read();
-	    var ans = (datumRoot instanceof Datum) && new Parser(datumRoot).parse(type);
-	    if (!!ans ^ testsForType[toParse]) {
-		++numErrors;
-		console.log('testParser ' + type + ': ' + toParse + ': expected ' + testsForType[toParse] + ', got ');
-		console.log(ans);
-	    }
-	    ++numTests;
-	}
+            var datumRoot = new Reader(toParse).read();
+            var ans = (datumRoot instanceof Datum) && new Parser(datumRoot).parse(type);
+            if (!!ans ^ testsForType[toParse]) {
+                ++numErrors;
+                console.log('testParser ' + type + ': ' + toParse + ': expected ' + testsForType[toParse] + ', got ');
+                console.log(ans);
+            }
+            ++numTests;
+        }
     }
     console.log('testParser: ' + numTests + ' tests, ' + numErrors + ' errors');
 }
