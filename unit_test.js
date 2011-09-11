7f8d01d4c89f1d3e9e552e7726cad77af602fd0a
Brendan Linn <brendan.linn@gmail.com>
2011-09-10 18:13:19 -0700

    moved parser tests into unit tests

--- unit_test.js
+++ unit_test.js
@@ -72,3 +72,125 @@ function testScanner() {
     }
     console.log('testScanner: ' + numTests + ' tests, ' + numErrors + ' errors');
 }
+
+function testParser() {
+
+
+    var tests = {};
+
+    tests['variable'] = {
+	'...': true, 
+	'+': true,
+	'-': true,
+	'x': true,
+	'=>': false,
+	'cond': false,
+	'(': false};
+
+    tests['quotation'] = {
+	"'1": true,
+	"''1": true,
+	'(quote quote)': true,
+	"'()": true,
+	'(quote ())': true,
+	"'quote": true,
+	'quote': false,
+	"''": false
+    };
+
+    tests['self-evaluating'] = {
+	'#t': true,
+	'1': true,
+	'#\\a': true,
+	'#\\space': true,
+	'3.14159': true,
+	'"hello, world"': true,
+	'"(define foo x y)"': true,
+	'(define foo (+ 1 2))': false,
+	'+': false
+    };
+
+    tests['procedure-call'] = {
+	'(+)': true,
+	'+': false,
+	'(foo x': false,
+	'foo x)': false, 
+	'()': false,
+	'(define x)': false,
+	'(foo x y . z)': false,
+	'((foo) (foo))': true,
+	'((define) foo)': false,
+	'((lambda () +) 1 2)': true
+    };
+
+    tests['lambda-expression'] = {
+	'(lambda () 1)': true,
+	'(lambda x 1)': true,
+	'(lambda (x y) (x y))': true,
+	'(lambda (x y))': false,
+	'(lambda (x . y) z)': true,
+	'(lambda x . y z)': false,
+	'(lambda lambda)': false
+    };
+
+    tests['definition'] = {
+	'(define x x)': true,
+	'(define (foo x y) (foo x y))': true,
+	'(begin (define x x) (define y y))': true,
+	'(define (x . y) 1)': true,
+	'define': false,
+	'(define)': false,
+	'(define x)': false,
+	'(begin 1)': false,
+	'(begin ())': false,
+	'(begin)': true
+    };
+
+    tests['conditional'] = {
+	'(if x y z)': true,
+	'(if x y)': true,
+	'(if x)': false,
+	'(if)': false,
+	'if': false,
+	'(IF x y)': true
+    };
+
+    tests['assignment'] = {
+	'(set! let! met!)': true,
+	'(set!)': false,
+	'(set! set!)': false, 
+	'(set! x)': false
+    };
+
+    function assertSuccess(text, result) {
+        if (result.fail) {
+            console.log('failed parsing ' + text + ':');
+            console.log(result);
+        }
+    }
+
+    function assertFailure(text, result) {
+        if (!result.fail) {
+            console.log('mistakenly parsed malformed text ' + text + ':');
+            console.log(result);
+        }
+    }
+
+    var numErrors = 0;
+    var numTests = 0;
+
+    for (var type in tests) {
+	var testsForType = tests[type];
+        for (var toParse in testsForType) {
+	    var datumRoot = new Reader(toParse).read();
+	    var ans = (datumRoot instanceof Datum) && new Parser(datumRoot).parse(type);
+	    if (!!ans ^ testsForType[toParse]) {
+		++numErrors;
+		console.log('testParser ' + type + ': ' + toParse + ': expected ' + testsForType[toParse] + ', got ');
+		console.log(ans);
+	    }
+	    ++numTests;
+	}
+    }
+    console.log('testParser: ' + numTests + ' tests, ' + numErrors + ' errors');
+}
