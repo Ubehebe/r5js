Ubehebe/r5js

commit 1ec13ad90d109317a35a030f83c51a9e3836bc09

make a new 'datum' pseudo-nonterminal, strictly for convenience

parse.js
@@ -30,7 +30,6 @@ Parser.prototype.alternation = function() {
     for (var i = 0; i < arguments.length; ++i) {
         if (possibleRhs = this.rhs.apply(this, arguments[i]))
             return possibleRhs;
-        else console.log('alternation: failed ' + arguments[i]);
     }
     return null;
 };
@@ -109,6 +108,8 @@ Parser.prototype.onDatum = function(element) {
     if (typeof element.type === 'string') {
 
         switch (element.type) {
+            case 'datum':
+            return true;
             case '(':
                 return this.advanceToChildIf(function(datum) {
                     return datum.type === 'list';
@@ -179,6 +180,8 @@ Parser.prototype.onDatum = function(element) {
 
 
 function isSyntacticKeyword(str) {
+    /* todo bl: why are define-syntax, let-syntax, letrec-syntax not listed
+        in 7.1.1 as syntactic keywords? */
     var kws = ['else', '=>', 'define', 'define-syntax', 'unquote', 'unquote-splicing', 'quote', 'lambda',
         'if', 'set!', 'begin', 'cond', 'and', 'or', 'case', 'let', 'let*', 'letrec', 'let-syntax', 'letrec-syntax', 'do',
         'delay', 'quasiquote'];
@@ -257,16 +260,12 @@ Parser.prototype['quotation'] = function() {
     return this.alternation(
         [
             {type: "'"},
-            {type: function(datum) {
-                return true;
-            } }
+            {type: 'datum'}
         ],
         [
             {type: '('},
             {type: 'quote'},
-            {type: function(datum) {
-                return true;
-            } },
+            {type: 'datum'},
             {type: ')'}
         ]);
 };
@@ -619,9 +618,7 @@ Parser.prototype['case-clause'] = function() {
     return this.rhs(
         {type: '('},
         {type: '('},
-        {type: function(datum) {
-            return true;
-        }, atLeast: 0},
+        {type: 'datum'},
         {type: ')'},
         {type: 'sequence'},
         {type: ')'}
@@ -675,9 +672,7 @@ Parser.prototype['macro-use'] = function() {
     return this.rhs(
         {type: '('},
         {type: 'keyword'},
-        {type: function(datum) {
-            return true;
-        }, atLeast: 0},
+        {type: 'datum', atLeast: 0},
         {type: ')'});
 };
 

commit a75755b8bcdbcba00c14bd35d45a869ce22ee056

whoa, fix regression in datum handling introduced by 1ec13ad90d109317a35a030f83c51a9e3836bc09. also added a mission condition rhs

parse.js
@@ -110,6 +110,7 @@ Parser.prototype.onDatum = function(element) {
         switch (element.type) {
             // just for convenience; if the reader succeeded, everything is already a datum
             case 'datum':
+                return this.advanceToSiblingIf(function (datum) { return true; });
             // vacuous; we already rewrote ( ... . as .( ...
             case '.':
                 return true;
@@ -450,6 +451,12 @@ Parser.prototype['assignment'] = function() {
  */
 Parser.prototype['derived-expression'] = function() {
     return this.alternation(
+        [
+            {type: '('},
+            {type: 'cond'},
+            {type: 'cond-clause', atLeast: 1},
+            {type: ')'}
+        ],
         [
             {type: '('},
             {type: 'cond'},