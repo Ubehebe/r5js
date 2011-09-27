5b2b21b90bda29ec6ecf3b0f33e0de8f472b21e0
Brendan Linn <brendan.linn@gmail.com>
2011-09-27 08:43:38 -0700

    commented various eval functions

--- eval.js
+++ eval.js
@@ -2,17 +2,28 @@ Datum.prototype.eval = function(env) {
     return this.values.pop()(this, env);
 };
 
+/* For when we want to evaluate a list of things and return all their values
+    (for example, we have to evaluate all operands to a procedure-call). */
 Datum.prototype.evalSiblingsReturnAll = function(env) {
     var ans = [];
-    // The cur.value check ensures new Datum().evalSiblings() is [] for convenience.
+    // The cur.values check ensures new Datum().evalSiblings() is [] for convenience.
     for (var cur = this; cur && cur.values; cur = cur.nextSibling)
         ans.push(cur.eval(env));
     return ans;
 };
 
+/* For when we want to evaluate a list of things for their side effects, except that
+    we want to return the value of the last. For example, a list of expressions. */
 Datum.prototype.evalSiblingsReturnLast = function(env) {
     var ans;
     for (var cur = this; cur && cur.values; cur = cur.nextSibling)
         ans = cur.eval(env);
     return ans;
+};
+
+/* For when we want to evaluate something for its side effects
+    (for example a list of definitions). */
+Datum.prototype.evalSiblingsReturnNone = function(env) {
+    for (var cur = this; cur && cur.values; cur = cur.nextSibling)
+        cur.eval(env);
 };
