00784b8b2ca9723005e6c2aafaea8e56600790ae
Brendan Linn <brendan.linn@gmail.com>
2011-09-29 07:10:41 -0700

    finally found a 'solution' to load the Scheme std lib at page load time

--- eval.js
+++ eval.js
@@ -3,7 +3,7 @@ Datum.prototype.eval = function(env) {
 };
 
 /* For when we want to evaluate a list of things and return all their values
-    (for example, we have to evaluate all operands to a procedure-call). */
+ (for example, we have to evaluate all operands to a procedure-call). */
 Datum.prototype.evalSiblingsReturnAll = function(env) {
     var ans = [];
     // The cur.values check ensures new Datum().evalSiblings() is [] for convenience.
@@ -13,7 +13,7 @@ Datum.prototype.evalSiblingsReturnAll = function(env) {
 };
 
 /* For when we want to evaluate a list of things for their side effects, except that
-    we want to return the value of the last. For example, a list of expressions. */
+ we want to return the value of the last. For example, a list of expressions. */
 Datum.prototype.evalSiblingsReturnLast = function(env) {
     var ans;
     for (var cur = this; cur && cur.values; cur = cur.nextSibling)
@@ -22,8 +22,18 @@ Datum.prototype.evalSiblingsReturnLast = function(env) {
 };
 
 /* For when we want to evaluate something for its side effects
-    (for example a list of definitions). */
+ (for example a list of definitions). */
 Datum.prototype.evalSiblingsReturnNone = function(env) {
     for (var cur = this; cur && cur.values; cur = cur.nextSibling)
         cur.eval(env);
 };
+
+// Top-level convenience function
+function eval(text, env, lhs) {
+    return new Parser(
+        new Reader(
+            new Scanner(text)
+        ).read()
+    ).parse(lhs)
+        .eval(env);
+}
