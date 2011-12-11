Ubehebe/r5js

commit 4226aa89e82c36c8ed8091a3f0752d34812848d1

Remove histogram recording of formal parameters in favor of associating environments with continuables.
Need to make sure we're implementing lexical scoping -- at one point I was
dangerously close to inadvertently implementing dynamic scoping!

cps.js
@@ -15,13 +15,16 @@ Continuation.prototype.toString = function() {
 };
 
 /* todo bl this seems to be required for non-tail recursion, but it is slow.
-    Can we improve or eliminate it? */
+ Can we improve or eliminate it? */
 Continuation.prototype.clone = function() {
     var ans = new Continuation(this.lastResultName);
     if (this.nextContinuable) {
         ans.nextContinuable = new Continuable(
             this.nextContinuable.subtype,
             this.nextContinuable.continuation.clone());
+        // todo bl
+        if (this.nextContinuable.env)
+            ans.nextContinuable.env = this.nextContinuable.env;
     }
     return ans;
 };
@@ -40,79 +43,10 @@ function Continuable(subtype, continuation) {
  continuable c such that c.continuation.nextContinuable is null. */
 Continuable.prototype.getLastContinuable = function() {
     if (!this.continuation)
-        throw new InternalInterpreterError('invariant incorrect');
+        throw new InternalInterpreterError('invariant violated');
     return this.continuation.nextContinuable
         ? this.continuation.nextContinuable.getLastContinuable()
         : this;
-};
-
-/* One of the drawbacks of using a trampoline for evaluation, instead
-    of a stack, is that bindings can get confusing. For example, consider
-    the program
-
-    (define (foo x) (* x x x))
-    (define x 10)
-    (+ x (foo 3))
-
-    The body of the procedure is desugared as
-
-    (* x x x [_0 ...])
-
-    The expression (+ x (foo 3)) is desugared as
-
-    (foo 3 [_1 (+ x _1 [_2 ...])])
-
-    The x should be bound to 10, according to (define x 10). If we didn't
-    make any special preparations, when that desugared procedure call
-    shows up on the trampoline, it will:
-
-    1. Bind 3 to x
-    2. Push [_1 (+ x _1 [_2 ...])] as the procedure body's last continuation
-    3. Advance to the procedure body, giving
-
-    (* x x x [_1 (+ x _1 [_2 ...])])
-
-    Using the binding x=3 for all occurrences of x clearly gives the wrong
-    answer.
-
-    One solution would be to resolve all the bindings in the continuation
-    [_1 (+ x _1 [_2 ...])] before rebinding x. This would require a walk of
-    the continuation prior to step 1. In fact, we do oftentimes walk this
-    continuation around this time in order to clone it. But there is no cloning
-    in tail recursion (the continuation is simply reused), and I did not want
-    to sacrifice performance in that important case.
-
-    Here is another solution. We know at definition time that foo uses x
-    exactly 3 times. The fourth use of x in
-
-    (* x x x [_1 (+ x _1 [_2 ...])])
-
-    should therefore refer to a different binding. So at procedure
-    definition time, we construct a histogram of the uses of the formal
-    parameters. We can use the histogram at evaluation time to figure
-    out what the correct binding of x should be. (That is what the
-    Environment class does.)
-
-    (Actually, this is a simplification. We cannot collect the histogram
-    across a branch, because we don't know at definition time which branch
-    we're going to take. So what we actually do is collect the histogram down
-    to the first branch. Each branch then collects its own histogram down to
-    its next branch, and so on. At evaluation time, when the trampoline comes
-    across a branch that has a histogram, it uses the histogram to extend the
-    "lifetimes" of the bindings accordingly. */
-Continuable.prototype.parameterHistogram = function(histogram) {
-
-    this.subtype.parameterHistogram(histogram);
-
-    /* todo bl I don't think a formal parameter can ever occur
-     as the lastResultName of a continuation, but this is not enforced
-     by the type system. We can remove this once we are sure of this
-     invariant. */
-    if (histogram[this.continuation.lastResultName] !== undefined)
-        ++histogram[this.continuation.lastResultName];
-
-    if (this.continuation.nextContinuable)
-        this.continuation.nextContinuable.parameterHistogram(histogram);
 };
 
 // delegate to subtype, passing in the continuation
@@ -129,11 +63,6 @@ function IdShim(payload) {
  of IdShims looks like a procedure call of an "id" procedure. */
 IdShim.prototype.toString = function(continuation) {
     return '(id ' + this.payload + ' ' + continuation + ')';
-};
-
-IdShim.prototype.parameterHistogram = function(histogram) {
-    if (this.payload.isIdentifier() && histogram[this.payload.payload] !== undefined)
-        ++histogram[this.payload.payload];
 };
 
 IdShim.prototype.evalAndAdvance = function(env, continuation, resultStruct) {
@@ -178,9 +107,6 @@ function Branch(testIdOrLiteral, consequentContinuable, alternateContinuable) {
     this.alternate = alternateContinuable;
     this.consequentLastContinuable = consequentContinuable.getLastContinuable();
     this.alternateLastContinuable = alternateContinuable && alternateContinuable.getLastContinuable();
-    /* this.consequentFormalHistogram = {};
-     this.alternateFormalHistogram = {};
-     */
 }
 
 Branch.prototype.toString = function(continuation) {
@@ -189,36 +115,6 @@ Branch.prototype.toString = function(continuation) {
         + ' : ' + (this.alternate && this.alternate.toString())
         + ' ' + continuation
         + '}';
-};
-
-Branch.prototype.parameterHistogram = function(histogram) {
-    if (this.test.isIdentifier() && histogram[this.test.payload] !== undefined)
-        ++histogram[this.test.payload];
-
-    /* Branches act as barriers to collecting frequency information
-     on formal parameters. The consequent and alternate branches
-     might have different frequencies of formal parameters, and there
-     is no general way of knowing at definition time which branch will
-     be taken. What we can collect is the frequency information on
-     each branch up to the next branch node. We store the resulting
-     histograms in this branch node, so that at evaluation time, when
-     we know which branch is actually taken, we can adopt the correct
-     frequency information. */
-
-    var consequentHistogram = {};
-    var alternateHistogram = {};
-
-    for (var name in histogram) {
-        consequentHistogram[name] = 0;
-        alternateHistogram[name] = 0;
-    }
-
-    this.consequent.parameterHistogram(consequentHistogram);
-    this.alternate.parameterHistogram(alternateHistogram);
-
-    this.consequentFormalHistogram = consequentHistogram;
-    this.alternateFormalHistogram = alternateHistogram;
-
 };
 
 // For composition; should only be called from newProcCall
@@ -230,16 +126,6 @@ function ProcCall(operatorName, firstOperand) {
 function newProcCall(operatorName, firstOperand, continuation) {
     return new Continuable(new ProcCall(operatorName, firstOperand), continuation);
 }
-
-ProcCall.prototype.parameterHistogram = function(histogram) {
-    if (histogram[this.operatorName] !== undefined)
-        ++histogram[this.operatorName];
-
-    for (var op = this.firstOperand; op; op = op.nextSibling) {
-        if (op.isIdentifier() && histogram[op.payload] !== undefined)
-            ++histogram[op.payload];
-    }
-};
 
 ProcCall.prototype.toString = function(continuation) {
     var ans = '(' + this.operatorName + ' ';
@@ -272,13 +158,15 @@ function trampoline(continuable, env) {
         tmp.currentEnv = null;
 
         console.log('boing: ' + curContinuable);
-
-        curContinuable.subtype.evalAndAdvance(env, curContinuable.continuation, tmp);
+        if (curContinuable.env)
+            console.log('hey, this continuable has an environment attached from ' + curContinuable.env.name);
+        curContinuable.subtype.evalAndAdvance(curContinuable.env || env, curContinuable.continuation, tmp);
         ans = tmp.ans;
         curContinuable = tmp.nextContinuable;
-        if (tmp.currentEnv)
+        if (tmp.currentEnv) {
+            console.log('adopting env ' + tmp.currentEnv.name);
             env = tmp.currentEnv;
-
+        }
     }
 
     /* If we're about to return a JavaScript function, return its name instead.
@@ -304,13 +192,9 @@ Branch.prototype.evalAndAdvance = function(env, continuation, resultStruct) {
         ? env.get(this.test.payload)
         : maybeWrapResult(this.test, this.test.type);
     if (testResult.payload === false) {
-        if (this.alternateFormalHistogram)
-            env.extendBindingLifetimes(this.alternateFormalHistogram);
         this.alternateLastContinuable.continuation = continuation;
         resultStruct.nextContinuable = this.alternate;
     } else {
-        if (this.consequentFormalHistogram)
-            env.extendBindingLifetimes(this.consequentFormalHistogram);
         this.consequentLastContinuable.continuation = continuation;
         resultStruct.nextContinuable = this.consequent;
     }
@@ -330,7 +214,12 @@ ProcCall.prototype.evalAndAdvance = function(env, continuation, resultStruct) {
     if (typeof proc === 'function') {
         args = gatherArgs(this.firstOperand, env);
         ans = proc.apply(null, args);
-        env.addBinding(continuation.lastResultName, ans);
+        if (continuation.nextContinuable && continuation.nextContinuable.env) {
+            console.log('hey, we should be adding to the env ' + continuation.nextContinuable.env.name);
+            continuation.nextContinuable.env.addBinding(continuation.lastResultName, ans);
+        } else {
+            env.addBinding(continuation.lastResultName, ans);
+        }
         resultStruct.ans = ans;
         resultStruct.nextContinuable = continuation.nextContinuable && continuation.nextContinuable;
         resultStruct.primitiveName = this.operatorName;
@@ -365,10 +254,14 @@ ProcCall.prototype.evalAndAdvance = function(env, continuation, resultStruct) {
         // todo bl do we need to wrap these in datums anymore?
         unwrappedProc = proc.payload;
         args = gatherArgs(this.firstOperand, env);
+        if (continuation.nextContinuable) {
+            console.log('attaching env ' + env.name + ' to continuable ' + continuation.nextContinuable);
+            continuation.nextContinuable.env = env;
+        }
         // This will be a no-op if tail recursion is detected
         unwrappedProc.setContinuation(continuation);
         unwrappedProc.checkNumArgs(args.length);
-        unwrappedProc.bindArgs(args);
+        unwrappedProc.bindArgs(args, unwrappedProc.env);
         resultStruct.nextContinuable = unwrappedProc.body;
         resultStruct.currentEnv = unwrappedProc.env;
     }

datum.js
@@ -218,6 +218,29 @@ Datum.prototype.sequence = function(env, disableContinuationWrappers, cpsNames) 
                 tmp = newIdShim(tmp);
 
             if (tmp instanceof Continuable) {
+                if (tmp.definitionHelper) {
+                    var defn = tmp.definitionHelper;
+                    //var proc = env.get(tmp.reuseProc).payload;
+                    var rest = cur.nextSibling.sequence(env, disableContinuationWrappers, cpsNames);
+                    console.log('sequenced rest: ' + rest);
+
+                    if (rest.definitionHelper) {
+                        console.log('rest needs body: ' + rest.definitionHelper.getName());
+                        console.log('using var ' + defn.formals[0]);
+                        console.log('targeting ' + defn.precedingContinuable);
+                        rest.definitionHelper.prependBinding(defn.formals[0], defn.precedingContinuable.continuation.lastResultName);
+                        defn.precedingContinuable.continuation.nextContinuable = rest;
+                    }
+
+                    else {
+                        var proc = new SchemeProcedure(defn.formals, false, null, env, defn.getName());
+                        console.log('made a procedure with formals ' + defn.formals);
+                        proc.setBody(rest);
+                        env.addBinding(defn.getName(), newProcedureDatum(proc));
+                    }
+                    return tmp;
+                }
+
                 if (!first)
                     first = tmp;
                 else if (curEnd) {
@@ -541,3 +564,21 @@ LocalStructure.prototype.toString = function() {
  (Not in the spec anywhere, I'm just trying to reduce the grammar to simplify
  evaluation.)
  */
+
+function DefinitionHelper(continuableToTarget, precedingContinuable, firstFormal) {
+    this.procCallToTarget = continuableToTarget;
+    this.precedingContinuable = precedingContinuable;
+    this.formals = [firstFormal];
+}
+
+DefinitionHelper.prototype.getName = function() {
+    return this.procCallToTarget.subtype.operatorName;
+};
+
+DefinitionHelper.prototype.prependBinding = function(formal, actual) {
+    this.formals.unshift(formal);
+    var oldFirstOp = this.procCallToTarget.subtype.firstOperand;
+    var newFirstOp = newIdOrLiteral(actual);
+    this.procCallToTarget.subtype.firstOperand = newFirstOp;
+    newFirstOp.nextSibling = oldFirstOp;
+};

env.js
@@ -1,3 +1,7 @@
+/* todo bl: we seem not to need binding stacks anymore, since
+    we're also nesting Environment objects which serves much the same
+    purpose. */
+
 var allEnvironments = [];
 
 function Environment(name, enclosingEnv) {
@@ -23,82 +27,15 @@ Environment.prototype.get = function(name) {
         if (this.enclosingEnv)
             return this.enclosingEnv.get(name);
         else throw new UnboundVariable(name + ' in env ' + this.name);
-    }
-    var topmostBindingForName = allBindingsForName[allBindingsForName.length - 1];
-
-    if (topmostBindingForName instanceof BindingCounter) {
-        // If the topmost binding is exhausted, pop it and try the call again.
-        if (topmostBindingForName.exhausted()) {
-            allBindingsForName.pop();
-            return this.get(name);
-        }
-
-        // Otherwise, return and decrement that binding.
-        else {
-            return topmostBindingForName.getAndDecrement();
-        }
-    }
-
-    /* If we did not get a BindingCounter object, we must not be dealing with
-     a formal parameter, so we really don't have to worry about
-     repeated bindings. Just return the binding. */
-    else {
-        return topmostBindingForName;
+    } else {
+        return allBindingsForName[allBindingsForName.length - 1];
     }
 };
 
 Environment.prototype.addBinding = function(name, val) {
-
     if (!this.bindings[name])
         this.bindings[name] = [val];
-    else
-        this.bindings[name].push(val);
-};
-
-// Should only be called from within procs, where we have id frequency data
-Environment.prototype.addRepeatedBinding = function(name, val, numRepetitions) {
-
-    var allBindingsForName = this.bindings[name];
-    var topmostBindingForName = allBindingsForName
-        && allBindingsForName[allBindingsForName.length - 1];
-    var reuseTopmostBinding = topmostBindingForName
-        && topmostBindingForName instanceof BindingCounter
-        && topmostBindingForName.exhausted();
-
-    /* If the topmost binding is exhausted, it will never be needed again,
-     so we can reuse its BindingCounter object. This is not merely an
-     optimization. If we didn't do this (or something equivalent, such as
-     popping the exhausted object), the stack of exhausted bindings
-     would accumulate during tail calls, meaning that we couldn't support
-     an unbounded number of active tail calls.
-
-     todo bl: I discovered this invariant post hoc, which means I need to
-     think about it more.
-     */
-    if (reuseTopmostBinding) {
-        topmostBindingForName.val = val;
-        topmostBindingForName.numRemaining = numRepetitions;
-    } else {
-
-        var toPush = new BindingCounter(val, numRepetitions);
-        if (!allBindingsForName)
-            this.bindings[name] = [toPush];
-        else
-            allBindingsForName.push(toPush);
-    }
-};
-
-Environment.prototype.extendBindingLifetimes = function(histogram) {
-
-    var allBindingsForName, topmostBindingsForName;
-
-    for (var name in histogram) {
-        allBindingsForName = this.bindings[name];
-        topmostBindingsForName = allBindingsForName[allBindingsForName.length - 1];
-        if (!(topmostBindingsForName instanceof BindingCounter))
-            throw new InternalInterpreterError('invariant incorrect: not in pop mode for ' + name);
-        topmostBindingsForName.incBy(histogram[name]);
-    }
+    else throw new InternalInterpreterError('warning, redefining ' + name);
 };
 
 Environment.prototype.toString = function() {
@@ -112,30 +49,4 @@ Environment.prototype.toString = function() {
         ans += ']\n';
     }
     return ans;
-};
-
-function BindingCounter(val, numRemaining) {
-    this.val = val;
-    this.numRemaining = numRemaining;
-}
-
-BindingCounter.prototype.getAndDecrement = function() {
-    --this.numRemaining;
-    return this.val;
-};
-
-BindingCounter.prototype.incBy = function(delta) {
-    this.numRemaining += delta;
-};
-
-BindingCounter.prototype.exhausted = function() {
-    return this.numRemaining === 0;
-};
-
-BindingCounter.prototype.toString = function() {
-    return this.val.toString() + ' (' + this.numRemaining + ' uses)';
-};
-
-BindingCounter.prototype.clone = function() {
-    return new BindingCounter(this.val, this.numRemaining);
 };

parse.js
@@ -528,10 +528,37 @@ Parser.prototype['definition'] = function() {
             {type: 'expression'},
             {type: ')'},
             {desugar: function(node, env) {
-                var name = node.at('variable').payload;
-                var evaled = trampoline(node.at('expression').desugar(env, true), env);
-                env.addBinding(name, evaled);
-                return null; //definition falls off the tree
+
+                /* Example:
+
+                (define x (+ 1 2))
+                (define y (+ 3 4))
+                (+ x y)
+
+                should desugar to something like
+
+                (+ 1 2 [_0 (+ 3 4 [_1 (proc0 _0 _1 [_2 ...])])])
+
+                where proc0 is conceptually
+
+                (lambda (x y) (+ x y))
+
+                 */
+
+
+                var expr = node.at('expression').desugar(env, true);
+                var lastContinuable = expr.getLastContinuable();
+                var argToUse = lastContinuable.continuation.lastResultName;
+
+                //var formalsArray = [node.at('variable').payload];
+                var name = newAnonymousLambdaName();
+                //env.addBinding(name, newProcedureDatum(new SchemeProcedure(formalsArray, false, null, env, name)));
+                var procCall = newProcCall(name, newIdOrLiteral(argToUse), new Continuation(newCpsName()));
+
+                lastContinuable.continuation.nextContinuable = procCall;
+                console.log('returning ' + expr);
+                expr.definitionHelper = new DefinitionHelper(procCall, lastContinuable, node.at('variable').payload);
+                return expr;
             }
             }
         ],

types.js
@@ -23,21 +23,38 @@ function SchemeProcedure(formalsArray, isDotted, bodyStart, env, name) {
     not for resolving stuff in the environment. */
     this.name = name;
 
-    /* Prime the parameter histogram.
-        See Continuable.parameterHistogram for more information. */
-    this.parameterHistogram = {};
-    for (var i=0; i<formalsArray.length; ++i)
-        this.parameterHistogram[formalsArray[i]] = 0;
-
-
     if (bodyStart) {
         this.body = bodyStart.sequence(this.env);
-        this.body.parameterHistogram(this.parameterHistogram);
         this.lastContinuable = this.body.getLastContinuable();
         this.savedContinuation = this.lastContinuable.continuation;
     }
 
 }
+
+/* The only SchemeProcedure objects with null bodies should be
+ definitions, since we have to insert their bodies later in parsing. For
+ example:
+
+ (define x 1)
+ (+ x x)
+
+ The definition gets desugared to ((lambda (x) null) 1). The expression
+ (+ x x) later becomes the anonymous lambda's body:
+
+ ((lambda (x) (+ x x)) 1)
+ */
+SchemeProcedure.prototype.isDefinition = function() {
+    return !this.body;
+};
+
+SchemeProcedure.prototype.setBody = function(bodyContinuable) {
+    if (!this.isDefinition())
+        throw new InternalInterpreterError('not a definition: ' + this);
+    console.log('attempting to set body ' + bodyContinuable);
+    this.body = bodyContinuable;
+    this.lastContinuable = this.body.getLastContinuable();
+    this.savedContinuation = this.lastContinuable.continuation;
+};
 
 /* This function is a no-op for tail calls, thus should support an unlimited
     number of active tail calls. Example:
@@ -111,27 +128,27 @@ SchemeProcedure.prototype.checkNumArgs = function(numActuals) {
     }
 };
 
-SchemeProcedure.prototype.bindArgs = function(args) {
+SchemeProcedure.prototype.bindArgs = function(args, env) {
 
     var name, i;
 
     for (i = 0; i < this.formalsArray.length - 1; ++i) {
         name = this.formalsArray[i];
-        this.env.addRepeatedBinding(name, args[i], this.parameterHistogram[name] || 0);
+        env.addBinding(name, args[i]);
     }
 
     /* Thanks to non-scoped JavaScript local variables,
      i is now this.formalsArray.length - 1. */
     name = this.formalsArray[i];
     if (!this.isDotted) {
-        this.env.addRepeatedBinding(name, args[i], this.parameterHistogram[name] || 0);
+        env.addBinding(name, args[i]);
     } else {
         // Roll up the remaining arguments into a list
         var list = newEmptyList();
         // Go backwards and do prepends to avoid quadratic performance
         for (var j = args.length - 1; j >= this.formalsArray.length - 1; --j)
             list.prependChild(args[j]);
-        this.env.addRepeatedBinding(name, list, this.parameterHistogram[name] || 0);
+        env.addBinding(name, list);
     }
 };
 

commit 48391c668b8233b12794e87ab5ea408fb15b7192

Multiple active calls to the same procedure have been broken since 4226aa8. Fix that by allocating a new Environment object for each ProcCall.

cps.js
@@ -269,12 +269,24 @@ ProcCall.prototype.evalAndAdvance = function(env, continuation, resultStruct) {
         continuation.setEnv(env);
         if (continuation.nextContinuable)
             continuation.nextContinuable.setEnv(env);
+
+        /* We have to allocate a new Environment object for each procedure
+            call, since we have to support a since SchemeProcedure having
+            more than one active ProcCall at a time. We point it back to the
+            procedure's own environment so it can look up procedure-internal
+            definitions.
+
+            todo bl: don't allocate Environments in tail contexts. In theory
+            this shouldn't prevent an unlimited number of active tail calls
+            (because the old Environment objects will get garbage collected),
+            but I would imagine it would make tail recursion impracticable. */
+        var newEnv = new Environment(null, proc.env);
         // This will be a no-op if tail recursion is detected
         proc.setContinuation(continuation);
         proc.checkNumArgs(args.length);
-        proc.bindArgs(args, proc.env);
+        proc.bindArgs(args, newEnv);
         resultStruct.nextContinuable = proc.body;
-        resultStruct.currentEnv = proc.env;
+        resultStruct.currentEnv = newEnv;
     }
 
     else if (proc instanceof Continuation) {

unit_test.js
@@ -320,7 +320,8 @@ function testEvaluator() {
         '(define (foo) "hi") (define bar (foo)) bar': '"hi"',
         '(define (foo x . y) y) (foo 3 4 5)': "(4 5)",
         "(apply + '(1 2 3))": '6',
-        "(define (foo x) (* x x)) (+ (foo 3) (foo 4))": '25'
+        "(define (foo x) (* x x)) (+ (foo 3) (foo 4))": '25',
+        "(define (fac n) (if (= n 0) 1 (* n (fac (- n 1))))) (fac 10)": '3628800'
     };
 
     tests['continuations'] = {