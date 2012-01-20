2c460e8bd29cbb398427e3089943aff97db178db
Brendan Linn <brendan.linn@gmail.com>
2012-01-19 17:14:14 -0800

    Misc. additions to the non-primitive procedure library.

--- r5rs-env.scm
+++ r5rs-env.scm
@@ -1,5 +1,7 @@
-; This file contains definitions for all procedures in R5RS listed as
-; "procedure" or "library procedure".
+; This file contains definitions for all non-primitive procedures in R5RS
+; listed as "procedure" or "library procedure".
+
+(define (not p) (if p #f #t))
 
 (define (equal? x y)
   (if (and (pair? x) (pair? y))
@@ -18,3 +20,42 @@
 (define memq (member-abstract eq?))
 (define memv (member-abstract eqv?))
 (define member (member-abstract equal?))
+
+(define (zero? z) (= z 0))
+(define (positive? x) (> x 0))
+(define (negative? x) (< x 0))
+(define (odd? n) (= 1 (remainder n 2)))
+(define (even? n) (= 0 (remainder n 2)))
+
+(define (reduce f xs init)
+  (if (null? xs)
+      init
+      (reduce f (cdr xs) (f init (car xs)))))
+
+(define (max x . ys)
+  (if (null? ys)
+      x
+      (reduce
+       (lambda (y z) (if (< y z) z y))
+       ys
+       x)))
+
+(define (min x . ys)
+  (if (null? ys)
+      x
+      (reduce
+       (lambda (y z) (if (< y z) y z))
+       ys
+       x)))
+
+(define (abs x)
+  (if (> x 0)
+      x
+      (- x)))
+
+(define (compose f g) (lambda (x) (f (g x))))
+
+(define caar (compose car car))
+(define cadr (compose car cdr))
+
+(define (list . xs) xs) ; haha
