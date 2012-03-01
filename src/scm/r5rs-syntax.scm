d643e3feca6882dea87f760930830db2f23c6b61
Brendan Linn <brendan.linn@gmail.com>
2012-02-29 17:59:53 -0800

    Added GPL and copyright notices.

--- src/scm/r5rs-syntax.scm
+++ src/scm/r5rs-syntax.scm
@@ -1,3 +1,18 @@
+; Copyright 2011, 2012 Brendan Linn
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 					; R5RS 6.5: "Null-environment returns a specifier for an environment that is
 					; empty except for the (syntactic) bindings for all syntactic keywords
 					; defined in this report that are either required or both optional and
