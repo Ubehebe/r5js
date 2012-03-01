9786c0e6754028a0cbb79e34510b973019ecaeae
Brendan Linn <brendan.linn@gmail.com>
2012-03-01 15:26:54 -0800

    Remove no-longer-used fields on Token class.
    TODO: We should be able to remove Token altogether and generate Datums
    directly. It will require some small-ish changes in the reader.

--- src/js/scanner.js
+++ src/js/scanner.js
@@ -13,10 +13,8 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
-function Token(type, start, stop) {
+function Token(type) {
     this.type = type;
-    this.start = start;
-    this.stop = stop;
 }
 
 Token.prototype.setPayload = function(payload) {
