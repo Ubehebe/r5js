d3b15617f77dd0a4f79f4e97be25694a5848e900
Brendan Linn <brendan.linn@gmail.com>
2012-03-16 14:42:41 -0700

    Migrate all remaining JavaScript in repl.html to main.js.

---
+++ src/main/main.js
@@ -0,0 +1,29 @@
+/* Copyright 2011, 2012 Brendan Linn
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+addEventListener('load', function() {
+    new MockTerminal(document.getElementById('repl'))
+        .println(GayLisp.getMetadata().banner)
+        .println(';; Type (tutorial) and press enter for an interactive tutorial.')
+        .setPrompt('>> ')
+        .pushInterpreter(function(string, terminal) {
+            return GayLisp.repl(string, function(sideEffect) {
+                terminal.println(sideEffect);
+            });
+        })
+        .pushInterpreter(tutorial)
+        .setInputCompleteHandler(GayLisp.willParse)
+        .start();
+});
