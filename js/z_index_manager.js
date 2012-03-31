2de01358020e5bd98f8c4d657de567954bc65202
Brendan Linn <brendan.linn@gmail.com>
2012-03-31 16:00:15 -0700

    Rudimentary rotary-nav UI for switching between terminal and spec.
    Involves updates to all three submodules.

---
+++ js/z_index_manager.js
@@ -0,0 +1,35 @@
+// There's probably something in jQuery to do this...oh well.
+function ZIndexManager() {
+    this.elements = [];
+}
+
+ZIndexManager.prototype.registerAnchors = function(array) {
+    for (var i=0; i< array.length; ++i)
+        this.pushAnchor(array[i]);
+    return this;
+};
+
+ZIndexManager.prototype.pushAnchor = function(a) {
+    var target = a.hash.length && document.querySelector(a.hash);
+    if (target) {
+        target.style.zIndex = String(this.elements.length);
+        this.elements.push(target);
+        var self = this;
+        a.addEventListener('click', function() {
+            self.bringToFront(target);
+        });
+    }
+};
+
+ZIndexManager.prototype.bringToFront = function (element) {
+    console.log('bring to front: ' + element.id);
+    var i = this.elements.indexOf(element);
+    if (i !== -1) {
+        this.elements.splice(i, 1);
+        this.elements.push(element);
+        for (i = 0; i < this.elements.length; ++i) {
+            this.elements[i].style.zIndex = String(i);
+            console.log(this.elements[i].id + ': zIndex ' + i);
+        }
+    }
+};
