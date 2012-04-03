bc79effa5ca2761bea860817c15620421b181649
Brendan Linn <brendan.linn@gmail.com>
2012-04-02 19:45:49 -0700

    Use local copies of spec/r5rs.css and gay-lisp/src/main/main.js.
    These two files are very much "top level configuration" that change
    a lot, and as such, they should not be in the submodules where we have to
    make two commits each time.

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 website: spec
 website:
 	mkdir -p build
-	cp js/* build/
 	cp rotary-nav/*.js build/
 
 	cd gay-lisp && make clean && make repl && cp build/* ../build/
+	# Must be after we make gay-lisp because we intentionally overwrite
+	# build/main.js.
+	cp js/* build/
 
 spec: .fake
 spec:
 	mkdir -p build/img
-	cp spec/r5rs.* build/
+	cp spec/r5rs.html build/
 	cp spec/img/* build/img/
+	cp css/r5rs.css build/
 
 min: spec
 min:
---
+++ js/main.js
@@ -0,0 +1,63 @@
+addEventListener('load', function() {
+
+    // Set up the terminal
+    new MockTerminal(document.getElementById('repl'), 80, 5, 500)
+        .println(GayLisp.getMetadata().banner)
+        .println(';; Type (tutorial) and press enter for an interactive tutorial.')
+        .setPrompt('>> ')
+        .pushInterpreter(function(string, terminal) {
+            return GayLisp.repl(string, function(sideEffect) {
+                terminal.println(sideEffect);
+            });
+        })
+        .pushInterpreter(tutorial)
+        .setInputCompleteHandler(GayLisp.willParse)
+        .start();
+
+    var startOn;
+    switch (document.location.hash) {
+        // No fragment: start at the REPL
+        case '':
+            startOn = '#repl';
+            break;
+        // Fragment corresponding to one of the rotary nav items: start at that item
+        case '#repl':
+        case '#spec':
+        case '#about':
+            startOn = document.location.hash;
+            break;
+        // Other fragment: it's inside the spec, start there.
+        default:
+            startOn = '#spec';
+            break;
+    }
+
+    // Set up the layers on the page
+    new ZIndexManager()
+        .registerAnchors(document.querySelectorAll('#navlist a'))
+    .bringToFront(document.querySelector(startOn));
+
+    // Set up the rotary-phone-like nav thing in the corner
+    new RotaryNav(document.getElementById('logo'), 50, 45, -45)
+        .setTransitionSpeed(0.5)
+        .registerNodes(document.getElementById('navlist').children)
+        .rotateElementToFront(document.querySelector('a[href="' + startOn + '"]').parentElement);
+
+    var h1s = document.querySelectorAll('section > h1');
+
+    for (var i = 0; i < h1s.length; ++i) {
+        var a = document.createElement('a');
+        a.href = '#contents';
+        a.appendChild(document.createTextNode('⇧'));
+        h1s[i].parentElement.insertBefore(a, h1s[i].nextElementSibling);
+    }
+
+    var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+    function changeColor(i) {
+        document.getElementById('logo').style.color = colors[i];
+        setTimeout(changeColor, 10000, (i+1) % colors.length);
+    }
+
+    setTimeout(changeColor, 10000, 0);
+});
