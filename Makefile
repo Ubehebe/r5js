efa0b7616bd1954cd1ec0bdb5dce2657f68912c8
Brendan Linn <brendan.linn@gmail.com>
2012-04-06 12:50:00 -0700

    First attempt at async loading of spec.

--- Makefile
+++ Makefile
@@ -11,7 +11,14 @@ website:
 spec: .fake
 spec:
 	mkdir -p build/img
-	cp spec/r5rs.html build/
+	# The spec takes a long time to parse, delaying the load and
+	# DOMContentLoaded events, so we bring it in via ajax instead.
+	# Unfortunately, that means it has to be valid XHTML. I've
+	# carefully ensured spec/r5rs.html is valid HTML5 and (with
+	# the addition of the following line) XHTML(5), but perhaps
+	# we should run xmllint during the build process.
+	echo '<?xml version="1.0" encoding="UTF-8"?>' > build/r5rs.xhtml
+	cat spec/r5rs.html >> build/r5rs.xhtml
 	cp spec/img/* build/img/
 	cp css/r5rs.css build/
 
--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 addEventListener('load', function() {
 
-    setupMockTerminal();
-    setupNav();
+    asyncLoadSpec();
+    setupTerminal();
     setupColors();
-    setupHeroText();
     setupTweets();
-    setupSpecExamples();
 
-    function setupMockTerminal() {
+    function setupTerminal() {
         new MockTerminal(document.getElementById('repl'), 80, 5, 500)
             .println(GayLisp.getMetadata().banner)
             .println(';; Type (tutorial) and press enter for an interactive tutorial.')
@@ -20,6 +18,21 @@ addEventListener('load', function() {
             .pushInterpreter(tutorial)
             .setInputCompleteHandler(GayLisp.willParse)
             .start();
+    }
+
+    function asyncLoadSpec() {
+        var req = new XMLHttpRequest();
+        req.open('GET', 'r5rs.xhtml');
+        req.onreadystatechange = function() {
+            if (req.readyState === 4 && req.status === 200) {
+                document.body.appendChild(req.responseXML.getElementById('spec'));
+                setupSpecExamples();
+                setupBackLinks();
+                setupHeroText();
+                setupNav();
+            }
+        };
+        req.send();
     }
 
     function setupNav() {
@@ -50,6 +63,9 @@ addEventListener('load', function() {
             .registerNodes(document.getElementById('navlist').children)
             .setSelectClass('selected')
             .rotateElementToFront(document.querySelector('a[href="' + startOn + '"]').parentElement);
+    }
+
+    function setupBackLinks() {
 
         var h1s = document.querySelectorAll('section > h1');
 
@@ -103,34 +119,34 @@ addEventListener('load', function() {
         });
     }
 
+    function setupSpecExamples() {
 
-function setupSpecExamples() {
+        // Replace the answers in the spec with buttons to call the interpreter
+        var qs = document.querySelectorAll('.ex dt');
 
-    // Replace the answers in the spec with buttons to call the interpreter
-    var qs = document.querySelectorAll('.ex dt');
+        for (var i = 0; i < qs.length; ++i) {
+            var question = qs[i];
+            var input = question.textContent;
+            var answer = question.nextElementSibling; // <dd>
+            var button = document.createElement('button');
+            button['type'] = 'button';
+            button.className = 'evalButton';
+            button['data-input'] = input;
+            button.addEventListener('click', function (e) {
+                var output;
+                try {
+                    output = GayLisp.repl(e.target['data-input']);
+                } catch (x) {
+                    output = x.toString();
+                }
 
-    for (var i = 0; i < qs.length; ++i) {
-        var question = qs[i];
-        var input = question.textContent;
-        var answer = question.nextElementSibling; // <dd>
-        var button = document.createElement('button');
-        button['type'] = 'button';
-        button.className = 'evalButton';
-        button['data-input'] = input;
-        button.addEventListener('click', function (e) {
-            var output;
-            try {
-                output = GayLisp.repl(e.target['data-input']);
-            } catch (x) {
-                output = x.toString();
-            }
-
-            e.target.parentElement.replaceChild(
-                document.createTextNode('⇒ ' + output),
-                e.target);
-        });
-        button.appendChild(document.createTextNode('eval'));
-        answer.replaceChild(button, answer.firstChild);
+                e.target.parentElement.replaceChild(
+                    document.createTextNode('⇒ ' + output),
+                    e.target);
+            });
+            button.appendChild(document.createTextNode('eval'));
+            answer.replaceChild(button, answer.firstChild);
+        }
     }
 }
 });
