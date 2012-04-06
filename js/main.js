8ec00cc2916b9e5f25af7358699804a0000136e3
Brendan Linn <brendan.linn@gmail.com>
2012-04-05 19:01:50 -0700

    Split main routine into logical functions.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,49 @@
 addEventListener('load', function() {
 
-    // Set up the terminal
-    new MockTerminal(document.getElementById('repl'), 80, 5, 500)
-        .println(GayLisp.getMetadata().banner)
-        .println(';; Type (tutorial) and press enter for an interactive tutorial.')
-        .setPrompt('>> ')
-        .pushInterpreter(function(string, terminal) {
-            return GayLisp.repl(string, function(sideEffect) {
-                terminal.println(sideEffect);
-            });
-        })
-        .pushInterpreter(tutorial)
-        .setInputCompleteHandler(GayLisp.willParse)
-        .start();
+    setupMockTerminal();
+    setupVisibilityManager();
+    setupColors();
+    setupTextResizers();
+    setupTweets();
+    setupSpecExamples();
 
-    var startOn;
-    switch (document.location.hash) {
-        // No fragment: start at the REPL
-        case '':
-            startOn = '#repl';
-            break;
-        // Fragment corresponding to one of the rotary nav items: start at that item
-        case '#repl':
-        case '#spec':
-        case '#about':
-            startOn = document.location.hash;
-            break;
-        // Other fragment: it's inside the spec, start there.
-        default:
-            startOn = '#spec';
-            break;
+    function setupMockTerminal() {
+        new MockTerminal(document.getElementById('repl'), 80, 5, 500)
+            .println(GayLisp.getMetadata().banner)
+            .println(';; Type (tutorial) and press enter for an interactive tutorial.')
+            .setPrompt('>> ')
+            .pushInterpreter(function (string, terminal) {
+                return GayLisp.repl(string, function (sideEffect) {
+                    terminal.println(sideEffect);
+                });
+            })
+            .pushInterpreter(tutorial)
+            .setInputCompleteHandler(GayLisp.willParse)
+            .start();
     }
 
-    // Set up the layers on the page
-    new VisibilityManager()
-        .registerAnchors(document.querySelectorAll('.vis-manager'))
-    .bringToFront(document.querySelector(startOn));
+    function setupVisibilityManager() {
+        var startOn;
+        switch (document.location.hash) {
+            // No fragment: start at the REPL
+            case '':
+                startOn = '#repl';
+                break;
+            // Fragment corresponding to one of the rotary nav items: start at that item
+            case '#repl':
+            case '#spec':
+            case '#about':
+                startOn = document.location.hash;
+                break;
+            // Other fragment: it's inside the spec, start there.
+            default:
+                startOn = '#spec';
+                break;
+        }
+        new VisibilityManager()
+            .registerAnchors(document.querySelectorAll('.vis-manager'))
+            .bringToFront(document.querySelector(startOn));
+    }
 
     // Set up the rotary-phone-like nav thing in the corner
     new RotaryNav(document.getElementById('logo'), 40, 60, -60)
@@ -50,49 +58,59 @@ addEventListener('load', function() {
     for (var i = 0; i < h1s.length; ++i) {
         var a = document.createElement('a');
         a.href = '#contents';
-        a.appendChild(document.createTextNode('⇧'));
+        a.appendChild(document.createTextNode('⬆'));
         h1s[i].parentElement.insertBefore(a, h1s[i].nextElementSibling);
     }
 
+    function setupColors() {
 
-    // Make the logo in the rotary nav thing change colors intermittently.
-    var colors = [
-        'rgba(255,0,0,0.8)', // red
-        'rgba(0,0,0,0)',
-        'rgba(255,165,0,0.8)', // orange
-        'rgba(0,0,0,0)',
-        'rgba(255,255,0,0.8)', // yellow
-        'rgba(0,0,0,0)',
-        'rgba(0,255,0,0.8)', // green
-        'rgba(0,0,0,0)',
-        'rgba(0,0,255,0.8)', // blue
-        'rgba(0,0,0,0)',
-        'rgba(75,0,130,0.8)', // indigo
-        'rgba(0,0,0,0)',
-        'rgba(238,130,238,0.8)', // violet
-        'rgba(0,0,0,0)'];
-    function changeColor(i) {
-        document.getElementById('logo').style.backgroundColor = colors[i];
-        setTimeout(changeColor, 10000, (i+1) % colors.length);
+        // Make the logo in the rotary nav thing change colors intermittently.
+        var colors = [
+            'rgba(255,0,0,0.8)', // red
+            'rgba(0,0,0,0)',
+            'rgba(255,165,0,0.8)', // orange
+            'rgba(0,0,0,0)',
+            'rgba(255,255,0,0.8)', // yellow
+            'rgba(0,0,0,0)',
+            'rgba(0,255,0,0.8)', // green
+            'rgba(0,0,0,0)',
+            'rgba(0,0,255,0.8)', // blue
+            'rgba(0,0,0,0)',
+            'rgba(75,0,130,0.8)', // indigo
+            'rgba(0,0,0,0)',
+            'rgba(238,130,238,0.8)', // violet
+            'rgba(0,0,0,0)'];
+
+        function changeColor(i) {
+            document.getElementById('logo').style.backgroundColor = colors[i];
+            setTimeout(changeColor, 10000, (i + 1) % colors.length);
+        }
+
+        setTimeout(changeColor, 10000, 0);
     }
-    setTimeout(changeColor, 10000, 0);
 
-    // Set the title of the spec attractively
-    new TextResizer(document.getElementById('hero-top'));
-    new TextResizer(document.getElementById('hero'));
-    new TextResizer(document.getElementById('about-hero'));
+    function setupTextResizers() {
+        // Set the title of the spec attractively
+        new TextResizer(document.getElementById('hero-top'));
+        new TextResizer(document.getElementById('hero'));
+        new TextResizer(document.getElementById('about-hero'));
+    }
 
-    // Open Tweet in a popup, not a new tab/window.
-    document.getElementById('tweet').addEventListener('click', function(e) {
-       open(e.target.href, '_blank', 'width=550,height=450');
-        e.preventDefault();
-    });
+    function setupTweets() {
+        // Open Tweet in a popup, not a new tab/window.
+        document.getElementById('tweet').addEventListener('click', function (e) {
+            open(e.target.href, '_blank', 'width=550,height=450');
+            e.preventDefault();
+        });
+    }
 
+
+function setupSpecExamples() {
 
     // Replace the answers in the spec with buttons to call the interpreter
     var qs = document.querySelectorAll('.ex dt');
 
-    for (var i=0; i<qs.length; ++i) {
+    for (var i = 0; i < qs.length; ++i) {
         var question = qs[i];
         var input = question.textContent;
         var answer = question.nextElementSibling; // <dd>
@@ -100,11 +118,11 @@ addEventListener('load', function() {
         button['type'] = 'button';
         button.className = 'evalButton';
         button['data-input'] = input;
-        button.addEventListener('click', function(e) {
+        button.addEventListener('click', function (e) {
             var output;
             try {
                 output = GayLisp.repl(e.target['data-input']);
-            } catch(x) {
+            } catch (x) {
                 output = x.toString();
             }
 
@@ -115,4 +133,5 @@ addEventListener('load', function() {
         button.appendChild(document.createTextNode('eval'));
         answer.replaceChild(button, answer.firstChild);
     }
+}
 });
