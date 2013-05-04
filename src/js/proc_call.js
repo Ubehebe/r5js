Ubehebe/r5js

commit a7af7bf053216e6e3f866c49658cb8ad25d23ea9

IllegalEmptyApplication => r5js.IllegalEmptyApplication. Update callers.

src/js/proc_call.js
@@ -18,6 +18,7 @@ goog.provide('r5js.tmp.proc_call');
 
 
 goog.require('r5js.EvalError');
+goog.require('r5js.IllegalEmptyApplication');
 goog.require('r5js.InternalInterpreterError');
 
 // For composition; should only be called from newProcCall
@@ -124,7 +125,7 @@ ProcCall.prototype.operandsInCpsStyle = function() {
     for (var cur = this.firstOperand; cur; cur = cur.nextSibling) {
         if (cur instanceof Datum) {
             if (cur.isEmptyList())
-                throw new IllegalEmptyApplication(this.operatorName.payload);
+                throw new r5rs.IllegalEmptyApplication(this.operatorName.payload);
             else if (!cur.isLiteral())
                 return false;
         }

src/js/errors.js
@@ -20,6 +20,7 @@ goog.provide('r5js.EvalError');
 
 
 goog.provide('r5js.GeneralSyntaxError');
+goog.provide('r5js.IllegalEmptyApplication');
 goog.provide('r5js.IncorrectNumArgs');
 goog.provide('r5js.InternalInterpreterError');
 goog.provide('r5js.TooFewArgs');
@@ -177,11 +178,16 @@ function QuasiquoteError(what) {
     };
 }
 
-function IllegalEmptyApplication(where) {
+/**
+ * @param {*} where Object that caused the empty application.
+ * @constructor
+ * TODO bl: narrow the type of the parameter. Can it be string?
+ */
+r5js.IllegalEmptyApplication = function(where) {
     this.toString = function() {
         return 'illegal empty application in ' + where;
     };
-}
+};
 
 function ParseError(what) {
     this.toString = function() {

commit 72e086ac7bbcb109670a1cbb32cd523c64d1d003

Fix typo introduced by a7af7bf.

src/js/proc_call.js
@@ -131,7 +131,7 @@ ProcCall.prototype.operandsInCpsStyle = function() {
     for (var cur = this.firstOperand; cur; cur = cur.nextSibling) {
         if (cur instanceof Datum) {
             if (cur.isEmptyList())
-                throw new r5rs.IllegalEmptyApplication(this.operatorName.payload);
+                throw new r5js.IllegalEmptyApplication(this.operatorName.payload);
             else if (!cur.isLiteral())
                 return false;
         }