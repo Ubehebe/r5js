9917f6d834f9dfdbbea3fd4f31755c5dab8df93d
Brendan Linn <brendan.linn@gmail.com>
2014-02-10 20:47:36 -0800

    add r5js.parse.Terminals enum.

---
+++ src/js/parse/terminals.js
@@ -0,0 +1,30 @@
+goog.provide('r5js.parse.Terminals');
+
+
+/** @typedef {string} */
+r5js.parse.Terminal;
+
+
+/** @enum {string} */
+r5js.parse.Terminals = {
+  BACKTICK: '`',
+  BEGIN: 'begin',
+  DEFINE: 'define',
+  DEFINE_SYNTAX: 'define-syntax',
+  DOT: '.',
+  ELLIPSIS: '...',
+  IF: 'if',
+  LAMBDA: 'lambda',
+  LET_SYNTAX: 'let-syntax',
+  LETREC_SYNTAX: 'letrec-syntax',
+  LPAREN: '(',
+  LPAREN_VECTOR: '#(',
+  QUASIQUOTE: 'quasiquote',
+  QUOTE: 'quote',
+  RPAREN: ')',
+  SET: 'set!',
+  SYNTAX_RULES: 'syntax-rules',
+  TICK: "'",
+  UNQUOTE: 'unquote',
+  UNQUOTE_SPLICING: 'unquote-splicing'
+};
