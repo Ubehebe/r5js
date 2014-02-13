af3f8bc1e3201f53b5c12c83a6b449ed000eb910
Brendan Linn <brendan.linn@gmail.com>
2014-02-12 17:49:37 -0800

    scan: add r5js.Token, which was breaking compilation (but not tests)

---
+++ src/js/scan/token.js
@@ -0,0 +1,16 @@
+goog.provide('r5js.Token');
+
+
+
+/**
+ * @interface
+ * @template T
+ */
+r5js.Token = function() {};
+
+
+/** @return {T} */
+r5js.Token.prototype.getPayload = function() {};
+
+
+
