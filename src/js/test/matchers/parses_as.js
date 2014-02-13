3508fd9a2310251e934d685a3bd6cc15574799dc
Brendan Linn <brendan.linn@gmail.com>
2014-02-12 18:13:22 -0800

    test: parser tests should use r5js.parse.Nonterminals where appropriate

--- src/js/test/matchers/parses_as.js
+++ src/js/test/matchers/parses_as.js
@@ -11,44 +11,46 @@ goog.require('r5js.Scanner');
 
 
 /**
- * @param {string} expectedType
+ * @param {!r5js.parse.Nonterminal} expectedType
  * @return {!tdd.matchers.Matcher}
  */
 parseAs = function(expectedType) {
-    return new r5js.test.matchers.ParsesAs(expectedType);
+  return new r5js.test.matchers.ParsesAs(expectedType);
 };
 
 
+
 /**
- * @param {string} expectedType
+ * @param {!r5js.parse.Nonterminal} expectedType
  * @implements {tdd.matchers.Matcher}
  * @struct
  * @constructor
  */
 r5js.test.matchers.ParsesAs = function(expectedType) {
-    /** @const @private {string} */
-    this.expectedType_ = expectedType;
+  /** @const @private {!r5js.parse.Nonterminal} */
+  this.expectedType_ = expectedType;
 };
 
 
 /** @override */
 r5js.test.matchers.ParsesAs.prototype.matches = function(value) {
-    var datumRoot = new r5js.Reader(new r5js.Scanner(/** @type {string} */ (value))).read();
-    var actualResult = (datumRoot instanceof r5js.Datum) &&
-        new r5js.Parser(datumRoot).rhs({type: this.expectedType_});
-    return actualResult &&
-        actualResult.peekParse &&
-        actualResult.peekParse() === this.expectedType_;
+  var datumRoot = new r5js.Reader(
+      new r5js.Scanner(/** @type {string} */ (value))).read();
+  var actualResult = (datumRoot instanceof r5js.Datum) &&
+      new r5js.Parser(datumRoot).rhs({type: this.expectedType_});
+  return actualResult &&
+      actualResult.peekParse &&
+      actualResult.peekParse() === this.expectedType_;
 };
 
 
 /** @override */
 r5js.test.matchers.ParsesAs.prototype.getSuccessMessage = function(value) {
-    return value + ' correctly parses as ' + this.expectedType_;
+  return value + ' correctly parses as ' + this.expectedType_;
 };
 
 
 /** @override */
 r5js.test.matchers.ParsesAs.prototype.getFailureMessage = function(value) {
-    return 'expected ' + value + ' to parse as ' + this.expectedType_;
+  return 'expected ' + value + ' to parse as ' + this.expectedType_;
 };
--- src/js/test/parser.js
+++ src/js/test/parser.js
@@ -4,7 +4,9 @@ goog.setTestOnly('r5js.test.Parser');
 
 goog.require('expect');
 goog.require('parseAs');
+goog.require('r5js.parse.Nonterminals');
 goog.require('tdd.TestType');
+
 
 
 /**
@@ -17,7 +19,7 @@ r5js.test.Parser = function() {};
 
 /** @override */
 r5js.test.Parser.prototype.getType = function() {
-    return tdd.TestType.UNIT;
+  return tdd.TestType.UNIT;
 };
 
 
@@ -28,288 +30,290 @@ r5js.test.Parser.prototype.toString = function() {
 
 
 r5js.test.Parser.prototype['testVariable'] = function() {
-    [
-        '...',
-        '+',
-        '-',
-        'x'
-    ].forEach(function(text) {
-            expect(text).to(parseAs('variable'));
-        });
+  [
+   '...',
+   '+',
+   '-',
+   'x'
+  ].forEach(function(text) {
+    expect(text).to(parseAs(r5js.parse.Nonterminals.VARIABLE));
+  });
 
-    [
-        '('
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('variable'));
-        });
+  [
+   '('
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.VARIABLE));
+  });
 };
 
 
 r5js.test.Parser.prototype['testQuotation'] = function() {
-    [
-        "'1",
-        "''1",
-        '(quote quote)',
-        "'quote"
-    ].forEach(function(text) {
-            expect(text).to(parseAs('quotation'));
-        });
+  [
+   "'1",
+   "''1",
+   '(quote quote)',
+   "'quote"
+  ].forEach(function(text) {
+    expect(text).to(parseAs(r5js.parse.Nonterminals.QUOTATION));
+  });
 
-    [
-        'quote',
-        "''"
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('quotation'));
-        });
+  [
+   'quote',
+   "''"
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.QUOTATION));
+  });
 };
 
 
 r5js.test.Parser.prototype['testSelfEvaluating'] = function() {
-    [
-        '#t',
-        '1',
-        '#\\a',
-        '#\\space',
-        '3.14159',
-        '"hello, world"',
-        '"(define foo x y)"'
-    ].forEach(function(text) {
-            expect(text).to(parseAs('self-evaluating'));
-        });
-    [
-        '(define foo (+ 1 2))',
-        '+'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('self-evaluating'));
-        });
+  [
+   '#t',
+   '1',
+   '#\\a',
+   '#\\space',
+   '3.14159',
+   '"hello, world"',
+   '"(define foo x y)"'
+  ].forEach(function(text) {
+    expect(text).to(parseAs(r5js.parse.Nonterminals.SELF_EVALUATING));
+  });
+  [
+   '(define foo (+ 1 2))',
+   '+'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.SELF_EVALUATING));
+  });
 };
 
 
 r5js.test.Parser.prototype['testProcedureCall'] = function() {
-    [
-        '(+)',
-        '(define x)',
-        '((foo) (foo))',
-        '((define) foo)',
-        '((lambda () +) 1 2)'
-    ].forEach(function(text) {
-            expect(text).to(parseAs('procedure-call'));
-        });
+  [
+   '(+)',
+   '(define x)',
+   '((foo) (foo))',
+   '((define) foo)',
+   '((lambda () +) 1 2)'
+  ].forEach(function(text) {
+    expect(text).to(parseAs(r5js.parse.Nonterminals.PROCEDURE_CALL));
+  });
 
-    [
-        '(foo x',
-        'foo x)',
-        '()',
-        '(foo x y . z)'
-// TODO bl parses as a macro use '((define) define)'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('procedure-call'));
-        });
+  [
+   '(foo x',
+   'foo x)',
+   '()',
+   '(foo x y . z)'
+   // TODO bl parses as a macro use '((define) define)'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.PROCEDURE_CALL));
+  });
 };
 
 
 r5js.test.Parser.prototype['testLambdaExpression'] = function() {
   [
-      '(lambda () 1)',
-      '(lambda x 1)',
-      '(lambda (x) y z)',
-      '(lambda (x y) (x y))',
-      '(lambda (x . y) z)',
-      '(lambda () (define x 1) (define y 2) x)',
-      '(lambda () (define x 1) (define y 2) x y)'
+   '(lambda () 1)',
+   '(lambda x 1)',
+   '(lambda (x) y z)',
+   '(lambda (x y) (x y))',
+   '(lambda (x . y) z)',
+   '(lambda () (define x 1) (define y 2) x)',
+   '(lambda () (define x 1) (define y 2) x y)'
   ].forEach(function(text) {
-          expect(text).to(parseAs('lambda-expression'));
-      });
+    expect(text).to(parseAs(r5js.parse.Nonterminals.LAMBDA_EXPRESSION));
+  });
 
-    [
-        '(lambda (x y))',
-        '(lambda x . y z)',
-        '(lambda lambda)',
-        '(lambda () (define x 1) (define y 2))'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('lambda-expression'));
-        });
+  [
+   '(lambda (x y))',
+   '(lambda x . y z)',
+   '(lambda lambda)',
+   '(lambda () (define x 1) (define y 2))'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.LAMBDA_EXPRESSION));
+  });
 };
 
 r5js.test.Parser.prototype['testFormals'] = function() {
   [
-      '(x y z)',
-      'x',
-      '(x . z)'
+   '(x y z)',
+   'x',
+   '(x . z)'
   ].forEach(function(text) {
-          expect(text).to(parseAs('formals'));
-      });
+    expect(text).to(parseAs(r5js.parse.Nonterminals.FORMALS));
+  });
 
-    [
-        '( . x)',
-        '(x . y . z)'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('formals'));
-        });
+  [
+   '( . x)',
+   '(x . y . z)'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.FORMALS));
+  });
 };
 
 
 r5js.test.Parser.prototype['testDefinition'] = function() {
   [
-      '(define x x)',
-      '(define (foo x y) (foo x y))',
-      '(begin (define x x) (define y y))',
-      '(define (x . y) 1)',
-      '(begin)',
-      '(define (x) (define y 1) x)',
-      '(begin (define x 1) (define y 2))'
+   '(define x x)',
+   '(define (foo x y) (foo x y))',
+   '(begin (define x x) (define y y))',
+   '(define (x . y) 1)',
+   '(begin)',
+   '(define (x) (define y 1) x)',
+   '(begin (define x 1) (define y 2))'
   ].forEach(function(text) {
-          expect(text).to(parseAs('definition'));
-      });
+    expect(text).to(parseAs(r5js.parse.Nonterminals.DEFINITION));
+  });
 
-    [
-        'define',
-        '(define x)',
-        '(begin 1)',
-        '(begin ())'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('definition'));
-        });
+  [
+   'define',
+   '(define x)',
+   '(begin 1)',
+   '(begin ())'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.DEFINITION));
+  });
 };
 
 r5js.test.Parser.prototype['testConditional'] = function() {
-    [
-        '(if x y z)',
-        '(if x y)',
-        '(if x (define x 1))'
-    ].forEach(function(text) {
-            expect(text).to(parseAs('conditional'));
-        });
+  [
+   '(if x y z)',
+   '(if x y)',
+   '(if x (define x 1))'
+  ].forEach(function(text) {
+    expect(text).to(parseAs(r5js.parse.Nonterminals.CONDITIONAL));
+  });
 
-    [
-        '(if x)',
-        '(if)'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('conditional'));
-        });
+  [
+   '(if x)',
+   '(if)'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.CONDITIONAL));
+  });
 };
 
 
 r5js.test.Parser.prototype['testAssignment'] = function() {
   [
-      '(set! let! met!)'
+   '(set! let! met!)'
   ].forEach(function(text) {
-          expect(text).to(parseAs('assignment'));
-      });
+    expect(text).to(parseAs(r5js.parse.Nonterminals.ASSIGNMENT));
+  });
 
-    [
-        '(set!)',
-        '(set! set!)',
-        '(set! x)'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('assignment'));
-        });
+  [
+   '(set!)',
+   '(set! set!)',
+   '(set! x)'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.ASSIGNMENT));
+  });
 };
 
 
 r5js.test.Parser.prototype['testTransformerSpec'] = function() {
   expect('(syntax-rules ())').to(parseAs('transformer-spec'));
-    expect('(syntax-rules)').not().to(parseAs('transformer-spec'));
+  expect('(syntax-rules)').not().to(parseAs(
+      r5js.parse.Nonterminals.TRANSFORMER_SPEC));
 };
 
 
 r5js.test.Parser.prototype['testPatternIdentifier'] = function() {
-  expect('define').to(parseAs('pattern-identifier'));
-    expect('...').not().to(parseAs('pattern-identifier'));
-    expect('x').to(parseAs('pattern-identifier'));
+  expect('define').to(parseAs(r5js.parse.Nonterminals.PATTERN_IDENTIFIER));
+  expect('...').not().to(parseAs(r5js.parse.Nonterminals.PATTERN_IDENTIFIER));
+  expect('x').to(parseAs(r5js.parse.Nonterminals.PATTERN_IDENTIFIER));
 };
 
 
 r5js.test.Parser.prototype['testPattern'] = function() {
-    [
-        '()',
-        '(define)',
-        '(define ...)',
-        '(define . define)',
-        '#()',
-        '#(define ...)'
-    ].forEach(function(text) {
-            expect(text).to(parseAs('pattern'));
-        });
-    [
-        '(define . ...)',
-        '(...)'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('pattern'));
-        });
+  [
+   '()',
+   '(define)',
+   '(define ...)',
+   '(define . define)',
+   '#()',
+   '#(define ...)'
+  ].forEach(function(text) {
+    expect(text).to(parseAs(r5js.parse.Nonterminals.PATTERN));
+  });
+  [
+   '(define . ...)',
+   '(...)'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.PATTERN));
+  });
 };
 
 
 r5js.test.Parser.prototype['testPatternDatum'] = function() {
-    expect('x').not().to(parseAs('pattern-datum'));
-    expect('"x"').to(parseAs('pattern-datum'));
-    expect("'x").not().to(parseAs('pattern-datum'));
+  expect('x').not().to(parseAs(r5js.parse.Nonterminals.PATTERN_DATUM));
+  expect('"x"').to(parseAs(r5js.parse.Nonterminals.PATTERN_DATUM));
+  expect("'x").not().to(parseAs(r5js.parse.Nonterminals.PATTERN_DATUM));
 };
 
 
 r5js.test.Parser.prototype['testTemplate'] = function() {
-    [
-        '()',
-        '#()',
-        '(x...)',
-        '(x... . x)',
-        '(x... y...)'
-    ].forEach(function(text) {
-            expect(text).to(parseAs('template'));
-        });
+  [
+   '()',
+   '#()',
+   '(x...)',
+   '(x... . x)',
+   '(x... y...)'
+  ].forEach(function(text) {
+    expect(text).to(parseAs(r5js.parse.Nonterminals.TEMPLATE));
+  });
 };
 
 
 r5js.test.Parser.prototype['testQuasiquotation'] = function() {
   [
-      '`(list ,(+ 1 2) 4)',
-      "`(a ,(+ 1 2) ,@(map abs '(4 -5 6)) b)",
-      "`((foo ,(- 10 3)) ,@(cdr '(c)) . ,(car '(cons)))",
-      "`#(10 5 ,(sqrt 4) ,@(map sqrt '(16 9)) 8)",
-      "`(a `(b ,(+ 1 2) ,(foo ,(+ 1 3) d) e) f)"
+   '`(list ,(+ 1 2) 4)',
+   "`(a ,(+ 1 2) ,@(map abs '(4 -5 6)) b)",
+   "`((foo ,(- 10 3)) ,@(cdr '(c)) . ,(car '(cons)))",
+   "`#(10 5 ,(sqrt 4) ,@(map sqrt '(16 9)) 8)",
+   '`(a `(b ,(+ 1 2) ,(foo ,(+ 1 3) d) e) f)'
   ].forEach(function(text) {
-          expect(text).to(parseAs('quasiquotation'));
-      });
+    expect(text).to(parseAs(r5js.parse.Nonterminals.QUASIQUOTATION));
+  });
 
-    expect("(a ,(+ 1 2) ,@(map abs '(4 -5 6)) b)")
+  expect("(a ,(+ 1 2) ,@(map abs '(4 -5 6)) b)")
         .not()
-        .to(parseAs('quasiquotation'));
+        .to(parseAs(r5js.parse.Nonterminals.QUASIQUOTATION));
 };
 
 
 r5js.test.Parser.prototype['testSplicingUnquotation'] = function() {
-    [
-        ",@(cdr '(c))",
-        "(unquote-splicing (cdr '(c)))"
-    ].forEach(function(text) {
-            expect(text).to(parseAs('splicing-unquotation'));
-        });
+  [
+   ",@(cdr '(c))",
+   "(unquote-splicing (cdr '(c)))"
+  ].forEach(function(text) {
+    expect(text).to(parseAs(r5js.parse.Nonterminals.SPLICING_UNQUOTATION));
+  });
 
-    [
-        ',@',
-        'unquote-splicing'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('splicing-unquotation'));
-        });
+  [
+   ',@',
+   'unquote-splicing'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(
+        r5js.parse.Nonterminals.SPLICING_UNQUOTATION));
+  });
 };
 
 
 r5js.test.Parser.prototype['testMacroBlock'] = function() {
-    [
-        '(let-syntax () 1)',
-        '(letrec-syntax () 1)',
-        "(let-syntax ((foo (syntax-rules () ((foo x) 'x)))) 1)",
-        "(letrec-syntax ((foo (syntax-rules (x) ((foo x) 'x)))) (foo))",
-        "(let-syntax ((foo (syntax-rules () ((foo) (+ 1 2 3))))) (define x 12) x)"
-    ].forEach(function(text) {
-            expect(text).to(parseAs('macro-block'));
-        });
+  [
+   '(let-syntax () 1)',
+   '(letrec-syntax () 1)',
+   "(let-syntax ((foo (syntax-rules () ((foo x) 'x)))) 1)",
+   "(letrec-syntax ((foo (syntax-rules (x) ((foo x) 'x)))) (foo))",
+   '(let-syntax ((foo (syntax-rules () ((foo) (+ 1 2 3))))) (define x 12) x)'
+  ].forEach(function(text) {
+    expect(text).to(parseAs(r5js.parse.Nonterminals.MACRO_BLOCK));
+  });
 
-    [
-        '(let-syntax ())',
-        '(letrec-syntax ())'
-    ].forEach(function(text) {
-            expect(text).not().to(parseAs('macro-block'));
-        });
+  [
+   '(let-syntax ())',
+   '(letrec-syntax ())'
+  ].forEach(function(text) {
+    expect(text).not().to(parseAs(r5js.parse.Nonterminals.MACRO_BLOCK));
+  });
 };
 
