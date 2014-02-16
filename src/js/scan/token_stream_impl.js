9d1c62cdd5a4d8d425547ef5f37ae3532bdf0363
Brendan Linn <brendan.linn@gmail.com>
2014-02-15 16:20:37 -0800

    scan: add r5js.scan.TokenStream+impl to abstract token checkpoint+restore

---
+++ src/js/scan/token_stream_impl.js
@@ -0,0 +1,45 @@
+goog.provide('r5js.scan.TokenStreamImpl');
+
+
+
+/**
+ * @param {!r5js.IScanner} scanner
+ * @implements {r5js.scan.TokenStream}
+ * @struct
+ * @constructor
+ */
+r5js.scan.TokenStreamImpl = function(scanner) {
+  /** @const @private {!r5js.IScanner} */
+  this.tokenStream_ = scanner;
+
+  /** @const @private {!Array.<!r5js.Token>} */
+  this.readyTokens_ = [];
+
+  /** @private {number} */
+  this.nextTokenIndex_ = 0;
+};
+
+
+/** @override */
+r5js.scan.TokenStreamImpl.prototype.checkpoint = function() {
+  return this.nextTokenIndex_;
+};
+
+
+/** @override */
+r5js.scan.TokenStreamImpl.prototype.nextToken = function() {
+  while (this.nextTokenIndex_ >= this.readyTokens_.length) {
+    var token = this.tokenStream_.nextToken();
+    if (!token) {
+      return null;
+    }
+    this.readyTokens_.push(token);
+  }
+  return this.readyTokens_[this.nextTokenIndex_++];
+};
+
+
+/** @override */
+r5js.scan.TokenStreamImpl.prototype.restore = function(checkpoint) {
+  this.nextTokenIndex_ = checkpoint;
+};
