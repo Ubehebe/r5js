916d3b49e9f147ed8ce54cf6bdbb2635ab812c2c
Brendan Linn <brendan.linn@gmail.com>
2014-03-10 07:53:23 -0700

    macro: move id_or_literal_transformer.js under js/macro and run through linter

--- src/js/macro/id_or_literal_transformer.js
+++ src/js/macro/id_or_literal_transformer.js
@@ -20,6 +20,7 @@ goog.provide('r5js.IdOrLiteralTransformer');
 goog.require('r5js.OutputMode');
 
 
+
 /**
  * @param {!r5js.Datum} datum
  * @implements {r5js.ITransformer}
@@ -27,63 +28,62 @@ goog.require('r5js.OutputMode');
  * @constructor
  */
 r5js.IdOrLiteralTransformer = function(datum) {
-    /** @const @private {!r5js.Datum} */
-    this.datum_ = datum;
+  /** @const @private {!r5js.Datum} */
+  this.datum_ = datum;
 };
+
 
 /** @override */
 r5js.IdOrLiteralTransformer.prototype.matchInput = function(
     inputDatum, literalIds, definitionEnv, useEnv, bindings) {
-    if (this.datum_.isIdentifier()) {
-        /* R5RS 4.3.2: "A subform in the input matches a literal identifier
+  if (this.datum_.isIdentifier()) {
+    /* R5RS 4.3.2: "A subform in the input matches a literal identifier
          if and only if it is an identifier and either both its occurrence
          in the macro expression and its occurrence in the macro definition
          have the same lexical binding, or the two identifiers are equal
          and both have no lexical binding." */
-        if (literalIds[this.datum_.getPayload()]) {
-            if (inputDatum.isIdentifier()) {
-                var name = inputDatum.getPayload();
-                // Both have no lexical binding
-                if (name === this.datum_.getPayload()
-                    && (!definitionEnv.hasBindingRecursive(name, false)
-                    && !useEnv.hasBindingRecursive(name, false))) {
-                    bindings.addTemplateBinding(name, inputDatum);
-                    return true;
-                } else if (definitionEnv.get(name) === useEnv.get(name)) {
-                    bindings.addTemplateBinding(name, inputDatum);
-                    return true;
-                } else return false;
-            } else return false;
-        }
-        /* R5RS 4.3.2: "An input form F matches a pattern P if and only if
+    if (literalIds[this.datum_.getPayload()]) {
+      if (inputDatum.isIdentifier()) {
+        var name = inputDatum.getPayload();
+        // Both have no lexical binding
+        if (name === this.datum_.getPayload() &&
+            (!definitionEnv.hasBindingRecursive(name, false) &&
+                !useEnv.hasBindingRecursive(name, false))) {
+          bindings.addTemplateBinding(name, inputDatum);
+          return true;
+        } else if (definitionEnv.get(name) === useEnv.get(name)) {
+          bindings.addTemplateBinding(name, inputDatum);
+          return true;
+        } else return false;
+      } else return false;
+    }
+  /* R5RS 4.3.2: "An input form F matches a pattern P if and only if
          [...] P is a non-literal identifier [...]".
          That is, non-literal identifiers match anything. */
-        else {
-            bindings.addTemplateBinding(this.datum_.getPayload(), inputDatum);
-            return true;
-        }
-    } else {
-        /* R5RS 4.3.2: "An input form F matches a pattern P if and only if
+    else {
+      bindings.addTemplateBinding(this.datum_.getPayload(), inputDatum);
+      return true;
+    }
+  } else {
+    /* R5RS 4.3.2: "An input form F matches a pattern P if and only if
          [...] P is a datum and F is equal to P in the sense of the equal?
          procedure." */
-        return inputDatum.isEqual(this.datum_);
-    }
+    return inputDatum.isEqual(this.datum_);
+  }
 };
+
 
 /** @override */
 r5js.IdOrLiteralTransformer.prototype.toDatum = function(bindings) {
-    return bindings.resolveDatum(this.datum_);
-};
-
-r5js.IdOrLiteralTransformer.prototype.toString = function() {
-    return this.datum_.stringForOutputMode(r5js.OutputMode.DISPLAY);
+  return bindings.resolveDatum(this.datum_);
 };
 
 
 /** @return {!r5js.Datum} */
 r5js.IdOrLiteralTransformer.prototype.getDatum = function() {
-    return this.datum_;
+  return this.datum_;
 };
+
 
 /**
  * @param {function(this: T, !r5js.ITransformer, number)} callback
@@ -95,6 +95,6 @@ r5js.IdOrLiteralTransformer.prototype.getDatum = function() {
  */
 r5js.IdOrLiteralTransformer.prototype.forEachSubtransformer = function(
     callback, ellipsisLevel, opt_context) {
-    callback.call(opt_context, this, ellipsisLevel);
+  callback.call(opt_context, this, ellipsisLevel);
 };
 
