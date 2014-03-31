0b45203aab2fa8328ced2a3d0a5d98f6073cdaf9
Brendan Linn <brendan.linn@gmail.com>
2014-03-31 07:37:48 -0700

    ast+datum: put r5js.ast.SimpleDatum in its own file.

--- src/js/datum.js
+++ src/js/datum.js
@@ -15,7 +15,6 @@
 
 
 goog.provide('r5js.ast.Literal');
-goog.provide('r5js.ast.SimpleDatum');
 goog.provide('r5js.Datum');
 
 
@@ -698,64 +697,5 @@ r5js.Datum.prototype.fixParserSensitiveIds = function(helper) {
     if (this.nextSibling_) {
         this.nextSibling_.fixParserSensitiveIds(helper);
     }
-};
-
-
-/**
- * @param {T} payload
- * @extends {r5js.ast.Literal}
- * @struct
- * @constructor
- * @template T
- */
-r5js.ast.SimpleDatum = function(payload) {
-  goog.base(this);
-
-    /** @private {T} */ this.payload_ = payload;
-};
-goog.inherits(r5js.ast.SimpleDatum, r5js.ast.Literal);
-
-
-/**
- * Booleans, characters, and numbers have value equality semantics.
- * @override
- */
-r5js.ast.SimpleDatum.prototype.eqv = function(other) {
-    return this.constructor === other.constructor && this.payload_ ===
-        (/** @type {!r5js.ast.SimpleDatum} */ (other)).payload_;
-};
-
-
-/** @return {T} */
-r5js.ast.SimpleDatum.prototype.getPayload = function() {
-    return this.payload_;
-};
-
-
-/** @param {T} payload */
-r5js.ast.SimpleDatum.prototype.setPayload = function(payload) {
-    this.payload_ = payload;
-};
-
-
-/** @override */
-r5js.ast.SimpleDatum.prototype.clone = function(parent) {
-    var clone = /** @type {!r5js.ast.SimpleDatum} */ (
-        goog.base(this, 'clone', parent));
-    clone.setPayload(this.payload_);
-    return clone;
-};
-
-
-/** @override */
-r5js.ast.SimpleDatum.prototype.isEqual = function(other) {
-    return other instanceof r5js.ast.SimpleDatum &&
-        this.payload_ === other.payload_;
-};
-
-
-/** @override */
-r5js.ast.SimpleDatum.prototype.unwrap = function() {
-    return this.payload_;
 };
 
