ad938d12da1917aab2fbe7977aeef1d839779c47
Brendan Linn <brendan.linn@gmail.com>
2014-04-05 11:44:27 -0700

    port: inline r5js.NodeBackedPort into r5js.js.NodeEnvironment.

--- src/js/env/node_environment.js
+++ src/js/env/node_environment.js
@@ -2,7 +2,6 @@ goog.provide('r5js.js.NodeEnvironment');
 
 
 goog.require('goog.Promise');
-goog.require('r5js.NodeBackedPort');
 
 
 
@@ -37,11 +36,79 @@ r5js.js.NodeEnvironment.prototype.exit = function(statusCode) {
 
 /** @override */
 r5js.js.NodeEnvironment.prototype.newInputPort = function(name) {
-  return new r5js.NodeBackedPort(name, 'r');
+  return new r5js.js.NodeEnvironment.Port_(name, 'r');
 };
 
 
 /** @override */
 r5js.js.NodeEnvironment.prototype.newOutputPort = function(name) {
-  return new r5js.NodeBackedPort(name, 'w');
+  return new r5js.js.NodeEnvironment.Port_(name, 'w');
+};
+
+
+goog.require('r5js.IOError');
+
+
+
+/**
+ * @param {string} filename
+ * @param {string} mode
+ * @implements {r5js.InputPort}
+ * @implements {r5js.OutputPort}
+ * @struct
+ * @constructor
+ * @private
+ */
+r5js.js.NodeEnvironment.Port_ = function(filename, mode) {
+
+  /* We set this inside the constructor instead of the usual way
+     so that a ReferenceError isn't thrown during parsing. */
+  if (!r5js.js.NodeEnvironment.Port_.prototype.fsModule) {
+    try {
+      /* Of course, require might be defined but do something other
+             than what we expect, which is to import the filesystem module.
+             We don't check for that. */
+      r5js.js.NodeEnvironment.Port_.prototype.fsModule = require('fs');
+    } catch (re) {
+      if (re instanceof ReferenceError) {
+        throw new r5js.IOError('the JavaScript environment lacks ' +
+            'filesystem access required for this IO procedure. ' +
+                    '(This probably means you are running in a browser.)');
+      }
+    }
+  }
+
+  this.fd = this.fsModule.openSync(filename, mode);
+  this.size = this.fsModule.statSync(filename).size;
+  this.offset = 0;
+};
+
+
+/** @override */
+r5js.js.NodeEnvironment.Port_.prototype.close = function() {
+  this.fsModule.closeSync(this.fd);
+};
+
+
+/** @override */
+r5js.js.NodeEnvironment.Port_.prototype.isCharReady = function() {
+  return true;
+};
+
+
+/** @override */
+r5js.js.NodeEnvironment.Port_.prototype.peekChar = function() {
+  return this.fsModule.readSync(this.fd, 1, this.offset)[0];
+};
+
+
+/** @override */
+r5js.js.NodeEnvironment.Port_.prototype.readChar = function() {
+  return this.fsModule.readSync(this.fd, 1, this.offset++)[0];
+};
+
+
+/** @override */
+r5js.js.NodeEnvironment.Port_.prototype.write = function(str) {
+  this.fsModule.writeSync(this.fd, str, null);
 };
