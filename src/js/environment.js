56cf227bcda6c6a28259d3ed8fb17259eaa0da6c
Brendan Linn <brendan.linn@gmail.com>
2014-04-08 19:15:01 -0700

    env: clean up r5js.Environment and run through linter.

--- src/js/environment.js
+++ src/js/environment.js
@@ -19,20 +19,20 @@ goog.provide('r5js.Environment');
 
 goog.require('r5js.Continuation');
 goog.require('r5js.Datum');
-goog.require('r5js.DatumType');
 goog.require('r5js.EvalError');
-goog.require('r5js.InternalInterpreterError');
-goog.require('r5js.JsObjOrMethod');
-goog.require('r5js.ast.Lambda');
-goog.require('r5js.Macro');
-goog.require('r5js.runtime.UNSPECIFIED_VALUE');
-goog.require('r5js.Procedure');
 goog.require('r5js.IEnvironment');
 goog.require('r5js.InputPort');
+goog.require('r5js.InternalInterpreterError');
+goog.require('r5js.JsObjOrMethod');
+goog.require('r5js.Macro');
 goog.require('r5js.OutputPort');
 goog.require('r5js.PrimitiveProcedure');
+goog.require('r5js.Procedure');
 goog.require('r5js.Ref');
 goog.require('r5js.UnboundVariable');
+goog.require('r5js.ast.Lambda');
+goog.require('r5js.runtime.UNSPECIFIED_VALUE');
+
 
 
 /**
@@ -41,219 +41,224 @@ goog.require('r5js.UnboundVariable');
  * @implements {r5js.IEnvironment}
  */
 r5js.Environment = function(enclosingEnv) {
-    if (enclosingEnv) {
-        this.enclosingEnv_ = enclosingEnv;
-        // useful for debugging console.log('created env ' + this + ' referencing ' + enclosingEnv);
-    }
 
-    /** @const @private {!Object.<string,!r5js.runtime.Value>} */
-    this.bindings_ = {};
+  /** @const @private */ this.enclosingEnv_ = enclosingEnv;
 
-    /**
-     * @type {!Object.<string, !r5js.Procedure>}
-     * @private
-     */
-    this.closures_ = {};
+  /** @const @private {!Object.<string,!r5js.runtime.Value>} */
+  this.bindings_ = {};
 
-    /**
-     * @type {boolean}
-     * @private
-     */
-    this.redefsOk_ = false;
+  /** @private {!Object.<string, !r5js.Procedure>} */ this.closures_ = {};
+
+  /** @private */ this.redefsOk_ = false;
 };
 r5js.IEnvironment.addImplementation(r5js.Environment);
-
-/**
- * @type {r5js.IEnvironment}
- * @private
- */
-r5js.Environment.prototype.enclosingEnv_;
 
 
 /** @override */
 r5js.Environment.prototype.seal = function() {
-    this.sealed = true;
+  this.sealed = true;
 };
+
 
 /**
  * @return {!r5js.Environment} This object, for chaining.
  */
 r5js.Environment.prototype.allowRedefs = function() {
-    this.redefsOk_ = true;
-    return this;
+  this.redefsOk_ = true;
+  return this;
 };
+
 
 /** @return {!r5js.Environment} A clone of this environment. */
 r5js.Environment.prototype.clone = function() {
 
-      if (this.enclosingEnv_) {
-        throw new r5js.InternalInterpreterError('clone should only be used during '
-        + 'interpreter bootstrapping');
-      }
+  if (this.enclosingEnv_) {
+    throw new r5js.InternalInterpreterError(
+        'clone should only be used during ' +
+            'interpreter bootstrapping');
+  }
 
-    var cloned = new r5js.Environment(null /* enclosingEnv */);
+  var cloned = new r5js.Environment(null /* enclosingEnv */);
 
-    for (var name_ in this.bindings_) {
-        var val = this.bindings_[name_];
-        cloned.bindings_[name_] = val instanceof r5js.Macro
-            ? val.clone(cloned)
-            : val;
-    }
+  for (var name_ in this.bindings_) {
+    var val = this.bindings_[name_];
+    cloned.bindings_[name_] = val instanceof r5js.Macro ?
+        val.clone(cloned) :
+        val;
+  }
 
-    return cloned;
+  return cloned;
 };
+
 
 /** @override */
 r5js.Environment.prototype.hasBinding = function(name) {
-    // We must never store null or undefined as a value.
-    return this.bindings_[name] != null;
+  return name in this.bindings_;
 };
 
 
 /** @override */
 r5js.Environment.prototype.hasBindingRecursive = function(name) {
-    return this.hasBinding(name)
-        || (!!this.enclosingEnv_ &&
-        this.enclosingEnv_.hasBindingRecursive(name));
+  return this.hasBinding(name) ||
+      (!!this.enclosingEnv_ && this.enclosingEnv_.hasBindingRecursive(name));
 };
+
 
 /** @override */
 r5js.Environment.prototype.get = function(name) {
-
-    var maybe = this.bindings_[name];
-
-    if (maybe != null) {
-
-        // Redirects for free ids in macro transcriptions
-        if (r5js.IEnvironment.isImplementedBy(maybe) &&
-            maybe.hasBindingRecursive(name))
-            return maybe.get(name);
-        /* We store primitive and non-primitive procedures unwrapped,
-         but wrap them in a Datum if they are requested through get.
-         (getProcedure, which is intended just for evaluating the operator
-         on the trampoline, will return the unwrapped procedures.) */
-        else if (r5js.PrimitiveProcedure.isImplementedBy(maybe)
-            || maybe instanceof r5js.Procedure)
-            return new r5js.ast.Lambda(name,
-                /** @type {!r5js.PrimitiveProcedure|!r5js.Procedure} */ (
-                    maybe));
-        else if (maybe === r5js.runtime.UNSPECIFIED_VALUE)
-            return maybe;
-        else if (maybe instanceof r5js.Datum)
-            return new r5js.Ref(maybe);
-        // Everything else
-        else return maybe;
-    } else if (maybe = this.closures_[name]) {
-        /* I think this is only used for ProcCall.prototype.cpsify, where
+  if (name in this.bindings_) {
+    var binding = this.bindings_[name];
+    if (r5js.IEnvironment.isImplementedBy(binding) &&
+            binding.hasBindingRecursive(name)) {
+      // Redirects for free ids in macro transcriptions
+      return binding.get(name);
+    } else if (r5js.PrimitiveProcedure.isImplementedBy(binding) ||
+            binding instanceof r5js.Procedure) {
+      /* We store primitive and non-primitive procedures unwrapped,
+             but wrap them in a Datum if they are requested through get.
+             (getProcedure, which is intended just for evaluating the operator
+             on the trampoline, will return the unwrapped procedures.) */
+      return new r5js.ast.Lambda(name,
+          /** @type {!r5js.PrimitiveProcedure|!r5js.Procedure} */ (
+          binding));
+    } else if (binding === r5js.runtime.UNSPECIFIED_VALUE) {
+      return binding;
+    } else if (binding instanceof r5js.Datum) {
+      return new r5js.Ref(binding);
+    } else {
+      return binding;
+    }
+  } else if (name in this.closures_) {
+    /* I think this is only used for ProcCall.prototype.cpsify, where
          identifiers are used to keep track of things while the structure
          is changed. Semantic use of procedures should be gated by
          Environment.prototype.getProcedure, and since that doesn't check
          the closures map, there should be no danger of accidentally
          returning a closure. */
-        return maybe;
-    }
+    return this.closures_[name];
+  } else if (this.enclosingEnv_) {
     // If the current environment has no binding for the name, look one level up
-    else if (this.enclosingEnv_) {
-        return this.enclosingEnv_.get(name);
-    } else {
-        throw new r5js.UnboundVariable(name + ' in env');
-    }
+    return this.enclosingEnv_.get(name);
+  } else {
+    throw new r5js.UnboundVariable(name + ' in env');
+  }
 };
+
 
 /** @override */
 r5js.Environment.prototype.getProcedure = function(name) {
-    var maybe = this.bindings_[name];
-
-    if (maybe != null) {
-        if (r5js.IEnvironment.isImplementedBy(maybe)) {
-            return maybe.getProcedure(name);
-        } else if (r5js.PrimitiveProcedure.isImplementedBy(maybe)
-            || maybe instanceof r5js.Procedure
-            || maybe instanceof r5js.Macro
-            || maybe instanceof r5js.Continuation
-            || maybe instanceof r5js.JsObjOrMethod) {
-            return maybe;
-        } else throw new r5js.EvalError('expected procedure, given ' + name);
-    } else if (this.enclosingEnv_) {
-        return this.enclosingEnv_.getProcedure(name);
+  if (name in this.bindings_) {
+    var binding = this.bindings_[name];
+    if (r5js.IEnvironment.isImplementedBy(binding)) {
+      return binding.getProcedure(name);
+    } else if (r5js.PrimitiveProcedure.isImplementedBy(binding) ||
+            binding instanceof r5js.Procedure ||
+            binding instanceof r5js.Macro ||
+            binding instanceof r5js.Continuation ||
+            binding instanceof r5js.JsObjOrMethod) {
+      return binding;
     } else {
-        return null;
+      throw new r5js.EvalError('expected procedure, given ' + name);
     }
+  } else if (this.enclosingEnv_) {
+    return this.enclosingEnv_.getProcedure(name);
+  } else {
+    return null;
+  }
 };
+
 
 /**
  * @param {!r5js.IEnvironment} other Environment to add closures from.
- * See comment to {@link r5js.IEnvironment.addClosure}.
+ * @return {!r5js.IEnvironment} This environment, for chaining.
+ * @see {r5js.IEnvironment#addClosure}.
  */
 r5js.Environment.prototype.addClosuresFrom = function(other) {
-    /* todo bl: we have to clone the SchemeProcedures to prevent
+  /* todo bl: we have to clone the SchemeProcedures to prevent
      some kind of infinite loop. I'm not entirely clear about what loop, though,
      since SchemeProcedure.prototype.cloneWithEnv itself does not do a lot
      of copying. */
-    for (var name in other.closures_) {
-        this.addBinding(name, other.closures_[name].cloneWithEnv(this));
-    }
-    return this;
+  for (var name in other.closures_) {
+    this.addBinding(name, other.closures_[name].cloneWithEnv(this));
+  }
+  return this;
 };
-
 
 
 /** @override */
 r5js.Environment.prototype.addClosure = function(name, proc) {
-    if (this.sealed) {
-        throw new r5js.InternalInterpreterError('tried to bind '
-            + name
-            + ' in sealed environment');
-    } else if (!(proc instanceof r5js.Procedure)) {
-        throw new r5js.InternalInterpreterError('invariant incorrect');
-    } else if (this.closures_[name]) {
-        throw new r5js.InternalInterpreterError('invariant incorrect');
-    } else {
-        this.closures_[name] = proc;
-    }
+  if (this.sealed) {
+    throw new r5js.InternalInterpreterError('tried to bind ' +
+        name +
+        ' in sealed environment');
+  } else if (!(proc instanceof r5js.Procedure)) {
+    throw new r5js.InternalInterpreterError('invariant incorrect');
+  } else if (this.closures_[name]) {
+    throw new r5js.InternalInterpreterError('invariant incorrect');
+  } else {
+    this.closures_[name] = proc;
+  }
 };
 
-/** @override */
+
+/**
+ * @param {string} name
+ * @return {boolean}
+ * @private
+ * @suppress {accessControls} for r5js.Datum.CPS_PREFIX_.
+ */
+r5js.Environment.prototype.bindingIsAcceptable_ = function(name) {
+  return !(name in this.bindings_) ||
+      this.redefsOk_ ||
+      name.charAt(0) === r5js.Datum.CPS_PREFIX_;
+};
+
+
+/**
+ * @override
+ */
 r5js.Environment.prototype.addBinding = function(name, val) {
+  if (this.sealed) {
+    throw new r5js.InternalInterpreterError(
+        'tried to bind ' +
+            name +
+            ' in sealed environment ' +
+            this);
+  }
 
-    if (this.sealed) {
-        throw new r5js.InternalInterpreterError('tried to bind ' + name + ' in sealed environment ' + this);
-    }
+  if (!this.bindingIsAcceptable_(name)) {
+    throw new r5js.InternalInterpreterError(
+        'redefining ' +
+        name +
+                ' in same env, not allowed');
+  }
 
-    else if (this.bindings_[name] == null ||
-        this.redefsOk_ ||
-        name.charAt(0) === '@') {
+  /* Macros require a backlink to the environment they were defined in
+    to resolve literal identifiers.
+    todo bl: is there a better place to put this? */
+  if (val instanceof r5js.Macro) {
+    val.setDefinitionEnv(this);
+  }
 
-        // useful for debugging if (val instanceof Datum)
-        //    console.log(this + ' addBinding ' + name + ' = ' + val);
-
-        /* Macros require a backlink to the environment they were defined in to resolve
-         literal identifiers. todo bl: is there a better place to put this? */
-        if (val instanceof r5js.Macro) {
-            val.setDefinitionEnv(this);
-        }
-
-        // Store primitive values directly.
-        if (typeof val === 'number'
-             || typeof val === 'string'
-             || val === true
-             || val === false
-             || r5js.IEnvironment.isImplementedBy(val)
-             || r5js.InputPort.isImplementedBy(val)
-             || r5js.OutputPort.isImplementedBy(val)) {
-             this.bindings_[name] = val;
-         } else if (val === r5js.runtime.UNSPECIFIED_VALUE) {
-            this.bindings_[name] = val;
-        } else if (r5js.PrimitiveProcedure.isImplementedBy(val)
-            || val instanceof r5js.Procedure /* non-primitive procedure */
-            || val instanceof r5js.Continuation /* call-with-current-continuation etc. */
-            || val instanceof Array /* values and call-with-values */
-            || val instanceof r5js.Macro /* macros */
-            || val instanceof r5js.JsObjOrMethod /* JavaScript interop (experimental!) */) {
-            this.bindings_[name] = val;
-        } else if (val instanceof r5js.Datum) {
-        // lots of stuff, including wrapped procedures
-            /* r5js.Environment.prototype.get should honor requests to store
+  // Store primitive values directly.
+  if (typeof val === 'number' ||
+      typeof val === 'string' ||
+      val === true ||
+      val === false ||
+      val === r5js.runtime.UNSPECIFIED_VALUE ||
+      r5js.IEnvironment.isImplementedBy(val) ||
+      r5js.InputPort.isImplementedBy(val) ||
+      r5js.OutputPort.isImplementedBy(val) ||
+      r5js.PrimitiveProcedure.isImplementedBy(val) ||
+      val instanceof r5js.Procedure /* non-primitive procedure */ ||
+      val instanceof r5js.Continuation /* call/cc, etc. */ ||
+      val instanceof Array /* values and call-with-values */ ||
+      val instanceof r5js.Macro /* macros */ ||
+      val instanceof r5js.JsObjOrMethod /* JavaScript interop */) {
+    this.bindings_[name] = val;
+  } else if (val instanceof r5js.Datum) {
+    // lots of stuff, including wrapped procedures
+    /* r5js.Environment.prototype.get should honor requests to store
              both unwrapped procedures (= JavaScript functions and
              SchemeProcedure objects) and those things wrapped in a Datum.
              In the latter case, we should store the unwrapped thing.
@@ -269,36 +274,30 @@ r5js.Environment.prototype.addBinding = function(name, val) {
              then the trampoline will call
              r5js.Environment.prototype.addBinding with this wrapped procedure
              as the second argument. */
-            this.bindings_[name] = val.unwrap();
-        } else {
-            throw new r5js.InternalInterpreterError('tried to store '
-                + name
-                + ' = '
-                + val
-                + ', which is not an acceptable value');
-        }
-    } else {
-        throw new r5js.InternalInterpreterError('redefining '
-            + name
-            + ' in same env, not allowed');
-    }
+    this.bindings_[name] = val.unwrap();
+  } else {
+    throw new r5js.InternalInterpreterError('tried to store ' +
+        name +
+        ' = ' +
+        val +
+        ', which is not an acceptable value');
+  }
 };
-
 
 
 /** @override */
 r5js.Environment.prototype.mutate = function(name, newVal, isTopLevel) {
-    var maybeBinding = this.bindings_[name];
-    if (maybeBinding != null || isTopLevel) {
-        if (r5js.IEnvironment.isImplementedBy(maybeBinding)) {
-            maybeBinding.mutate(name, newVal, isTopLevel);
-        } else {
-            delete this.bindings_[name];
-            this.addBinding(name, newVal);
-        }
-    } else if (this.enclosingEnv_) {
-        this.enclosingEnv_.mutate(name, newVal, isTopLevel);
-    } else throw new r5js.UnboundVariable(name);
+  var maybeBinding = this.bindings_[name];
+  if (maybeBinding != null || isTopLevel) {
+    if (r5js.IEnvironment.isImplementedBy(maybeBinding)) {
+      maybeBinding.mutate(name, newVal, isTopLevel);
+    } else {
+      delete this.bindings_[name];
+      this.addBinding(name, newVal);
+    }
+  } else if (this.enclosingEnv_) {
+    this.enclosingEnv_.mutate(name, newVal, isTopLevel);
+  } else throw new r5js.UnboundVariable(name);
 };
 
 
