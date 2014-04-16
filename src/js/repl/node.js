71b85570f8d5a53e817374eb17022dd2b2595425
Brendan Linn <brendan.linn@gmail.com>
2014-04-15 18:07:14 -0700

    repl: add src/js/repl/node.js as a better typed src/js/node/node_repl.js.

---
+++ src/js/repl/node.js
@@ -0,0 +1,70 @@
+goog.provide('r5js.repl.node');
+goog.provide('r5js.repl.node.main');
+
+
+goog.require('r5js.boot');
+goog.require('r5js.js.Environment');
+goog.require('r5js.test.SchemeSources');
+
+
+
+/**
+ * @param {!r5js.Evaluator} evaluator
+ * @param {!readline.Interface} readline
+ * @struct
+ * @constructor
+ * @private
+ */
+r5js.repl.Node_ = function(evaluator, readline) {
+  /** @private @const */ this.evaluator_ = evaluator;
+  /** @private @const */ this.readline_ = readline;
+  this.readline_.on('line', goog.bind(this.handleLine_, this));
+  this.readline_.on('close', goog.bind(this.handleClose_, this));
+};
+
+
+/**
+ * @param {string} line
+ * @private
+ */
+r5js.repl.Node_.prototype.handleLine_ = function(line) {
+  console.log(this.evaluator_.evaluate(line));
+  this.readline_.prompt();
+};
+
+
+/** @private */
+r5js.repl.Node_.prototype.handleClose_ = function() {
+  process.exit(0);
+};
+
+
+/**
+ * Main entry point for the NodeJS REPL.
+ * @suppress {checkTypes} TODO bl my Node version is 0.6x, but the externs
+ * I'm using are for 0.10x, which has an incompatible readline API.
+ */
+r5js.repl.node.main = function() {
+  var readline = require('readline');
+  var rl = readline.createInterface(process.stdin, process.stdout);
+  rl.setPrompt(
+      r5js.repl.node.PROMPT_, r5js.repl.node.PROMPT_.length);
+
+  var jsEnv = r5js.js.Environment.get();
+  r5js.test.SchemeSources.get(goog.bind(jsEnv.fetchUrl, jsEnv)).
+      then(function(sources) {
+        return r5js.boot(sources.syntax, sources.procedures);
+      }).then(function(evaluator) {
+        new r5js.repl.Node_(evaluator, /** @type {!readline.Interface} */ (rl));
+      });
+
+  rl.prompt();
+};
+
+
+/** @const @private */ r5js.repl.node.PROMPT_ = '>> ';
+
+
+goog.exportSymbol('r5js.repl.node.main', r5js.repl.node.main);
+// nodejs hack. See comment in goog.promise.testSuiteAdapter.
+goog.exportSymbol('setTimeout', setTimeout);
