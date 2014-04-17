b7647b8c62b1db604f8818d85e715505e083b4ac
Brendan Linn <brendan.linn@gmail.com>
2014-04-16 18:56:12 -0700

    runtime: add r5js.ProcCallLike interface abstracting r5js.{Branch_,ProcCall}.

---
+++ src/js/runtime/proc_call_like.js
@@ -0,0 +1,17 @@
+goog.provide('r5js.ProcCallLike');
+
+
+
+/** @interface */
+r5js.ProcCallLike = function() {};
+
+
+/**
+ * @param {!r5js.Continuation} continuation
+ * @param {!r5js.TrampolineHelper} trampolineHelper
+ * @param {!r5js.EnvBuffer} envBuffer
+ * @param {function(!r5js.Datum):!r5js.Parser} parserProvider Function
+ * that will return a new Parser for the given Datum when called.
+ */
+r5js.ProcCallLike.prototype.evalAndAdvance = function(
+    continuation, trampolineHelper, envBuffer, parserProvider) {};
