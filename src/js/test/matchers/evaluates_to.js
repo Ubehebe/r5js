8abf3bd2a51b8cfbd7224c70600e1b6e51ef27e4
Brendan Linn <brendan.linn@gmail.com>
2014-05-04 09:44:18 -0700

    test: add matcher for testing output port outputs.

--- src/js/test/matchers/evaluates_to.js
+++ src/js/test/matchers/evaluates_to.js
@@ -1,12 +1,15 @@
+goog.provide('haveJsOutput');
 goog.provide('haveJsValue');
 goog.provide('haveStringValue');
 goog.provide('r5js.test.matchers.setSharedEvaluator');
+goog.setTestOnly('haveJsOutput');
 goog.setTestOnly('haveJsValue');
 goog.setTestOnly('haveStringValue');
 goog.setTestOnly('r5js.test.matchers.setSharedEvaluator');
 
 
 goog.require('goog.array');
+goog.require('r5js.OutputSavingPort');
 goog.require('r5js.ToJsEvaluator');
 goog.require('r5js.ToStringEvaluator');
 
@@ -30,6 +33,18 @@ haveStringValue = function(value) {
   return new r5js.test.matchers.HasStringValue_(
       value, /** @type {!r5js.EvaluateToExternalRepresentation.<string>} */ (
           r5js.test.matchers.HasStringValue_.sharedEvaluator_));
+};
+
+
+/**
+ * @param {?} output
+ * @return {!tdd.matchers.Matcher}
+ */
+haveJsOutput = function(output) {
+  return new r5js.test.matchers.HasJsOutput_(output,
+      /** @type {!r5js.Evaluator} */(
+      r5js.test.matchers.HasJsOutput_.sharedEvaluator_),
+      r5js.test.matchers.HasJsOutput_.sharedOutputPort_);
 };
 
 
@@ -137,10 +152,61 @@ r5js.test.matchers.HasStringValue_.prototype.getFailureMessage =
 };
 
 
+
+/**
+ * @param {?} expectedOutput
+ * @param {!r5js.Evaluator} evaluator
+ * @param {!r5js.OutputSavingPort} outputPort
+ * @implements {tdd.matchers.Matcher}
+ * @struct
+ * @constructor
+ * @private
+ */
+r5js.test.matchers.HasJsOutput_ = function(
+    expectedOutput, evaluator, outputPort) {
+  /** @const @private */ this.expectedOutput_ = expectedOutput;
+  /** @const @private */ this.evaluator_ = evaluator;
+  /** @const @private */ this.outputPort_ = outputPort;
+};
+
+
+/** @private {r5js.Evaluator} */
+r5js.test.matchers.HasJsOutput_.sharedEvaluator_;
+
+
+/** @const @private */
+r5js.test.matchers.HasJsOutput_.sharedOutputPort_ = new r5js.OutputSavingPort();
+
+
+/** @override */
+r5js.test.matchers.HasJsOutput_.prototype.matches = function(input) {
+  this.evaluator_.evaluate(input);
+  var actualOutput = this.outputPort_.getAndClearOutput()[0];
+  return r5js.test.matchers.HasJsValue_.equals(
+      actualOutput, this.expectedOutput_);
+};
+
+
+/** @override */
+r5js.test.matchers.HasJsOutput_.prototype.getSuccessMessage = function(input) {
+  return 'ok';
+};
+
+
+/** @override */
+r5js.test.matchers.HasJsOutput_.prototype.getFailureMessage = function(input) {
+  return 'want ' +
+      this.expectedOutput_ +
+      ' got ';
+};
+
+
 /** @param {!r5js.Evaluator} evaluator */
 r5js.test.matchers.setSharedEvaluator = function(evaluator) {
   r5js.test.matchers.HasJsValue_.sharedEvaluator_ =
       new r5js.ToJsEvaluator(evaluator);
   r5js.test.matchers.HasStringValue_.sharedEvaluator_ =
       new r5js.ToStringEvaluator(evaluator);
+  r5js.test.matchers.HasJsOutput_.sharedEvaluator_ = evaluator.withPorts(
+      r5js.InputPort.NULL, r5js.test.matchers.HasJsOutput_.sharedOutputPort_);
 };
