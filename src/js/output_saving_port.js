1c1b94b418bda6f3d6cdfb7d484676cd4fe37bf7
Brendan Linn <brendan.linn@gmail.com>
2014-05-04 09:33:24 -0700

    io: add r5js.OutputSavingPort for use in testing output from JS.

---
+++ src/js/output_saving_port.js
@@ -0,0 +1,42 @@
+goog.provide('r5js.OutputSavingPort');
+
+
+goog.require('r5js.OutputPort');
+goog.require('r5js.ToJsEvaluator');
+
+
+
+/**
+ * @implements {r5js.OutputPort}
+ * @struct
+ * @constructor
+ */
+r5js.OutputSavingPort = function() {
+  /** @private {!Array.<boolean|number|string|!Array|undefined>} */
+  this.values_ = [];
+};
+r5js.OutputPort.addImplementation(r5js.OutputSavingPort);
+
+
+/**
+ * @override
+ * @suppress {accessControls} for {@link r5js.ToJsEvaluator#schemeToJsValue_}.
+ * TODO bl move that method somewhere appropriate.
+ */
+r5js.OutputSavingPort.prototype.write = function(value) {
+  this.values_.push(r5js.ToJsEvaluator.schemeToJsValue_(value));
+};
+
+
+/** @override */
+r5js.OutputSavingPort.prototype.close = goog.nullFunction;
+
+
+/** @return {!Array.<boolean|number|string|!Array|undefined>} */
+r5js.OutputSavingPort.prototype.getAndClearOutput = function() {
+  var values = this.values_;
+  this.values_ = [];
+  return values;
+};
+
+
