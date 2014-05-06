a16f3471f7b33dcd5e0e84033bb2e2f1af5cf630
Brendan Linn <brendan.linn@gmail.com>
2014-05-05 19:20:38 -0700

    io: split port.js into {input,output}_port.js.

--- src/js/io/output_port.js
+++ src/js/io/output_port.js
@@ -14,51 +14,7 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 
-goog.provide('r5js.InputPort');
 goog.provide('r5js.OutputPort');
-
-
-goog.require('goog.functions');
-
-
-
-/** @interface */
-r5js.InputPort = function() {};
-
-
-/** @return {boolean} */
-r5js.InputPort.prototype.isCharReady = function() {};
-
-
-/** @return {?string} */
-r5js.InputPort.prototype.peekChar = function() {};
-
-
-/** @return {?string} */
-r5js.InputPort.prototype.readChar = function() {};
-
-
-/** @see R5RS 6.6.1 */
-r5js.InputPort.prototype.close = function() {};
-
-
-/** @const @private */ r5js.InputPort.IMPLEMENTED_BY_PROP_ = '$r5js.InputPort';
-
-
-/**
- * @param {*} obj
- * @return {boolean}
- * TODO bl temporary shim. Remove.
- */
-r5js.InputPort.isImplementedBy = function(obj) {
-  return !!(obj && obj[r5js.InputPort.IMPLEMENTED_BY_PROP_]);
-};
-
-
-/** @param {function(new: r5js.InputPort, ...)} ctor */
-r5js.InputPort.addImplementation = function(ctor) {
-  ctor.prototype[r5js.InputPort.IMPLEMENTED_BY_PROP_] = true;
-};
 
 
 
@@ -98,38 +54,6 @@ r5js.OutputPort.isImplementedBy = function(obj) {
 r5js.OutputPort.addImplementation = function(ctor) {
   ctor.prototype[r5js.OutputPort.IMPLEMENTED_BY_PROP_] = true;
 };
-
-
-
-/**
- * An input port that has no available input.
- * @implements {r5js.InputPort}
- * @struct
- * @constructor
- * @private
- */
-r5js.InputPort.Null_ = function() {};
-r5js.InputPort.addImplementation(r5js.InputPort.Null_);
-
-
-/** @override */
-r5js.InputPort.Null_.prototype.isCharReady = goog.functions.FALSE;
-
-
-/** @override */
-r5js.InputPort.Null_.prototype.peekChar = goog.functions.NULL;
-
-
-/** @override */
-r5js.InputPort.Null_.prototype.readChar = goog.functions.NULL;
-
-
-/** @override */
-r5js.InputPort.Null_.prototype.close = goog.nullFunction;
-
-
-/** @const {!r5js.InputPort} */
-r5js.InputPort.NULL = new r5js.InputPort.Null_();
 
 
 
