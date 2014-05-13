ce3bae1946cd960ce1ba617f88fa43a849f82df2
Brendan Linn <brendan.linn@gmail.com>
2014-05-13 08:28:21 -0700

    env: plug r5js.Pipe into r5js.js.Html5Environment.

--- src/js/env/html5_environment.js
+++ src/js/env/html5_environment.js
@@ -2,6 +2,7 @@ goog.provide('r5js.js.Html5Environment');
 
 
 goog.require('goog.labs.net.xhr');
+goog.require('r5js.Pipe');
 
 
 
@@ -10,7 +11,9 @@ goog.require('goog.labs.net.xhr');
  * @struct
  * @constructor
  */
-r5js.js.Html5Environment = function() {};
+r5js.js.Html5Environment = function() {
+  /** @const @private {!Object.<string, !r5js.Pipe>} */ this.ports_ = {};
+};
 
 
 /** @override */
@@ -21,15 +24,15 @@ r5js.js.Html5Environment.prototype.exit = goog.nullFunction;
 r5js.js.Html5Environment.prototype.fetchUrl = goog.labs.net.xhr.get;
 
 
-/**
- * @override
- * @suppress {checkTypes} TODO bl implement
- */
-r5js.js.Html5Environment.prototype.newInputPort = function(name) {};
+/** @override */
+r5js.js.Html5Environment.prototype.newInputPort = function(name) {
+  if (!(name in this.ports_)) {
+    this.ports_[name] = new r5js.Pipe();
+  }
+  return this.ports_[name];
+};
 
 
-/**
- * @override
- * @suppress {checkTypes} TODO bl implement
- */
-r5js.js.Html5Environment.prototype.newOutputPort = function(name) {};
+/** @override */
+r5js.js.Html5Environment.prototype.newOutputPort =
+    r5js.js.Html5Environment.prototype.newInputPort;
