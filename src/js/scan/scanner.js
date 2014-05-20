e426370d11fc26cb43d58c2a6f670cf826627b55
Brendan Linn <brendan.linn@gmail.com>
2014-05-20 07:07:59 -0700

    lint: run scanner.js through the linter.

--- src/js/scan/scanner.js
+++ src/js/scan/scanner.js
@@ -17,9 +17,9 @@
 goog.provide('r5js.Scanner');
 
 
+goog.require('r5js.Datum');
 goog.require('r5js.InternalInterpreterError');
 goog.require('r5js.ScanError');
-goog.require('r5js.Datum');
 goog.require('r5js.ast.Boolean');
 goog.require('r5js.ast.Character');
 goog.require('r5js.ast.Identifier');
@@ -125,19 +125,19 @@ r5js.Scanner.prototype.nextToken = function() {
  * @suppress {checkTypes} for setNextSibling(null) TODO bl remove
  */
 r5js.Scanner.prototype.restore = function(checkpoint) {
-    for (var i = checkpoint+1; i < this.readyTokens_.length; ++i) {
-        var token = this.readyTokens_[i];
-        if (token instanceof r5js.Datum) {
-            token.setNextSibling(null);
-        }
+  for (var i = checkpoint + 1; i < this.readyTokens_.length; ++i) {
+    var token = this.readyTokens_[i];
+    if (token instanceof r5js.Datum) {
+      token.setNextSibling(null);
     }
+  }
   this.nextTokenIndex_ = checkpoint;
 
 };
 
 
 /**
- * @return {!r5js.Token|null}
+ * @return {?r5js.Token}
  * @private
  */
 r5js.Scanner.prototype.readNextToken_ = function() {
@@ -185,7 +185,7 @@ r5js.Scanner.prototype.matchToToken_ = function(matchArray) {
     return payload;
   } else if (matchArray[5]) {
     this.needDelimiter_ = true;
-      /* Converting Scheme identifiers to a canonical case makes
+    /* Converting Scheme identifiers to a canonical case makes
        interoperability with JavaScript awkward. For example:
 
        (((window 'document) 'querySelector) "body")
@@ -198,7 +198,7 @@ r5js.Scanner.prototype.matchToToken_ = function(matchArray) {
        I see little downside to making Scheme case-sensitive
        (and R6RS might require it, I haven't looked), so I went ahead
        and did it, commenting out the few test cases that thereby failed. */
-      return new r5js.ast.Identifier(payload/*.toLowerCase()*/);
+    return new r5js.ast.Identifier(payload/*.toLowerCase()*/);
   } else if (matchArray[2]) {
     this.needDelimiter_ = false;
     return new r5js.ast.Boolean(payload === '#t' || payload === '#T');
@@ -208,20 +208,22 @@ r5js.Scanner.prototype.matchToToken_ = function(matchArray) {
         r5js.Scanner.normalizeCharacterPayload_(payload));
   } else if (matchArray[4]) {
     this.needDelimiter_ = false;
-      // String literals could have escaped backslashes and double quotes,
-      // but we want to store them unescaped.
-      // TODO bl: there seem to be no tests exercising string unescaping. Add some.
-      //      replace(/\\(["\\])/g, "$1");
-      var actualPayload = payload.substr(1, payload.length - 2);
-      return new r5js.ast.String(actualPayload);
+    // String literals could have escaped backslashes and double quotes,
+    // but we want to store them unescaped.
+    // TODO bl: there seem to be no tests exercising string unescaping.
+    // Add some.
+    //      replace(/\\(["\\])/g, "$1");
+    var actualPayload = payload.substr(1, payload.length - 2);
+    return new r5js.ast.String(actualPayload);
   } else if (matchArray[1]) {
     this.needDelimiter_ = true;
-      var numericPayload = r5js.Scanner.NUMBER_FUNNY_BUSINESS_.test(payload) ?
-          r5js.Scanner.parseNumericPayload_(payload) :
-          parseFloat(payload);
+    var numericPayload = r5js.Scanner.NUMBER_FUNNY_BUSINESS_.test(payload) ?
+        r5js.Scanner.parseNumericPayload_(payload) :
+        parseFloat(payload);
     return new r5js.ast.Number(numericPayload);
   } else throw new r5js.InternalInterpreterError('invariant incorrect');
 };
+
 
 /**
  * @param {string} payload
@@ -229,19 +231,19 @@ r5js.Scanner.prototype.matchToToken_ = function(matchArray) {
  * @private
  */
 r5js.Scanner.normalizeCharacterPayload_ = function(payload) {
-    var afterSlash = payload.substr(2);
-    if (afterSlash.length === 1) {
-        return afterSlash;
-        /* R5RS 6.3.4: "Case is significant in #\<character>, but not in
+  var afterSlash = payload.substr(2);
+  if (afterSlash.length === 1) {
+    return afterSlash;
+  /* R5RS 6.3.4: "Case is significant in #\<character>, but not in
          #\<character name>.*/
-    } else if (afterSlash.toLowerCase() === 'space') {
-        return ' ';
-    } else if (afterSlash.toLowerCase() === 'newline') {
-        return '\n';
-    } else {
-        throw new r5js.InternalInterpreterError(
-            'invalid character payload ' + payload);
-    }
+  } else if (afterSlash.toLowerCase() === 'space') {
+    return ' ';
+  } else if (afterSlash.toLowerCase() === 'newline') {
+    return '\n';
+  } else {
+    throw new r5js.InternalInterpreterError(
+        'invalid character payload ' + payload);
+  }
 };
 
 /** @const @private */ r5js.Scanner.NUMBER_FUNNY_BUSINESS_ = /[esfdli#\/]/i;
@@ -255,46 +257,45 @@ r5js.Scanner.normalizeCharacterPayload_ = function(payload) {
  * @private
  */
 r5js.Scanner.parseNumericPayload_ = function(payload) {
-    /* Get rid of all exactness annotations. Because we're
+  /* Get rid of all exactness annotations. Because we're
      using JavaScript math, all numbers are inexact, so the
      exactness annotations have no semantic significance. */
-    payload = payload.replace(/#i|#e/i, '');
+  payload = payload.replace(/#i|#e/i, '');
 
-    var originalLength = payload.length;
+  var originalLength = payload.length;
 
-    if (r5js.Scanner.NUMBER_FORBIDDEN_.test(payload))
-        throw new r5js.ScanError('unsupported number literal: ' + payload);
+  if (r5js.Scanner.NUMBER_FORBIDDEN_.test(payload))
+    throw new r5js.ScanError('unsupported number literal: ' + payload);
 
-    var base = 10;
-    if ((payload = payload.replace(/#x/i, '')).length < originalLength) {
-        base = 16;
-    } else if ((payload = payload.replace(/#d/i, '')).length < originalLength) {
-        // nothing to do
-    } else if ((payload = payload.replace(/#o/i, '')).length < originalLength) {
-        base = 8;
-    } else if ((payload = payload.replace(/#b/i, '')).length < originalLength) {
-        base = 2;
-    }
+  var base = 10;
+  if ((payload = payload.replace(/#x/i, '')).length < originalLength) {
+    base = 16;
+  } else if ((payload = payload.replace(/#d/i, '')).length < originalLength) {
+  // nothing to do
+  } else if ((payload = payload.replace(/#o/i, '')).length < originalLength) {
+    base = 8;
+  } else if ((payload = payload.replace(/#b/i, '')).length < originalLength) {
+    base = 2;
+  }
 
-    /* Get rid of all lone hashes. The lone hashes appear in the <decimal 10>
+  /* Get rid of all lone hashes. The lone hashes appear in the <decimal 10>
      rule, but don't appear to have any semantic significance. */
-    payload = payload.replace('#', '');
+  payload = payload.replace('#', '');
 
-    var maybeRational = payload.split('/');
-    if (maybeRational.length === 2) {
-        return parseInt(maybeRational[0], base) / parseInt(maybeRational[1], base);
-    } else {
-        /* If the base is 10, it could have additional features like an exponent
+  var maybeRational = payload.split('/');
+  if (maybeRational.length === 2) {
+    return parseInt(maybeRational[0], base) / parseInt(maybeRational[1], base);
+  } else {
+    /* If the base is 10, it could have additional features like an exponent
          or a decimal point. ([sfdl] are precision annotations for exponents,
          which we ignore.) If the base is not 10, it can't have any features
          other than a base annotation (like "#x") and a division sign, both of
          which have already been taken care of. */
-        return base === 10 ?
-            parseFloat(payload.replace(/[sfdl]/i, 'e')) :
-            parseInt(payload, base);
-    }
+    return base === 10 ?
+        parseFloat(payload.replace(/[sfdl]/i, 'e')) :
+        parseInt(payload, base);
+  }
 };
-
 
 
 /**
