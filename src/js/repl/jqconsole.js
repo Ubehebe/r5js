8a0da83b324da0f4f3ccec8abd2c8b2ecae6193f
Brendan Linn <brendan.linn@gmail.com>
2014-05-21 07:22:51 -0700

    repl+ui: improve structure of jqconsole repl.

--- src/js/repl/jqconsole.js
+++ src/js/repl/jqconsole.js
@@ -21,15 +21,48 @@ r5js.repl.jqconsole.main = function(jqconsole) {
         });
         var evaluator = r5js.boot(
             sources.syntax, sources.procedures, r5js.InputPort.NULL, stdout);
-        var ugh = function() {
-          jqconsole.Prompt(true, function(input) {
-            jqconsole.Write(
-                r5js.EvalAdapter.toDisplayString(
-                            evaluator.evaluate(input)) + '\n',
-                'jqconsole-output');
-            ugh();
-          });
-        };
-        ugh();
+        new r5js.repl.jqconsole.Console_(evaluator, jqconsole).prompt();
       });
+};
+
+
+
+/**
+ * @param {!r5js.Evaluator} evaluator
+ * @param {?} jqconsole
+ * @struct
+ * @constructor
+ * @private
+ */
+r5js.repl.jqconsole.Console_ = function(evaluator, jqconsole) {
+  /** @const @private */ this.evaluator_ = evaluator;
+  /** @const @private */ this.jqconsole_ = jqconsole;
+};
+
+
+/** @suppress {checkTypes} for the jqconsole integration */
+r5js.repl.jqconsole.Console_.prototype.prompt = function() {
+  this.jqconsole_.Prompt(
+      true /* history_enabled */, this.handleInput_.bind(this));
+};
+
+
+/**
+ * @param {string} input
+ * @private
+ * @suppress {checkTypes} for the jqconsole integration
+ */
+r5js.repl.jqconsole.Console_.prototype.handleInput_ = function(input) {
+  var value;
+  var outputClass = 'jqconsole-output';
+  try {
+    value = r5js.EvalAdapter.toDisplayString(
+        this.evaluator_.evaluate(input));
+  } catch (e) {
+    value = e.toString();
+    outputClass = 'jqconsole-error';
+  } finally {
+    this.jqconsole_.Write(value + '\n', outputClass);
+    this.prompt();
+  }
 };
