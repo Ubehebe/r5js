8446a704226896bd450e2b4318fb1578e0af0be0
Brendan Linn <brendan.linn@gmail.com>
2014-05-21 07:02:39 -0700

    repl: jqconsole should use a R5RSCompliantOutputPort.

--- src/js/repl/jqconsole.js
+++ src/js/repl/jqconsole.js
@@ -1,9 +1,8 @@
 goog.provide('r5js.repl.jqconsole.main');
 
 
-goog.require('r5js.EvalAdapter');
-goog.require('r5js.InMemoryInputPort');
-goog.require('r5js.InMemoryOutputPort');
+goog.require('r5js.InputPort');
+goog.require('r5js.R5RSCompliantOutputPort');
 goog.require('r5js.boot');
 goog.require('r5js.js.Environment');
 goog.require('r5js.test.SchemeSources');
@@ -17,11 +16,11 @@ r5js.repl.jqconsole.main = function(jqconsole) {
   var jsEnv = r5js.js.Environment.get();
   r5js.test.SchemeSources.get(jsEnv.fetchUrl.bind(jsEnv)).
       then(function(sources) {
-        var buffer = [];
-        var stdin = new r5js.InMemoryInputPort(buffer);
-        var stdout = new r5js.InMemoryOutputPort(buffer);
+        var stdout = new r5js.R5RSCompliantOutputPort(function(str) {
+          jqconsole.Write(str, 'jqconsole-output');
+        });
         var evaluator = r5js.boot(
-            sources.syntax, sources.procedures, stdin, stdout);
+            sources.syntax, sources.procedures, r5js.InputPort.NULL, stdout);
         var ugh = function() {
           jqconsole.Prompt(true, function(input) {
             jqconsole.Write(
