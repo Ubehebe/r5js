644d4e8a40cd87ab9b7e5f602fb991ebdaffc04f
Brendan Linn <brendan.linn@gmail.com>
2015-03-02 07:01:55 -0800

    test: NodeTest: no need to test for node before running tests.
    If node isn't present, the test will fail anyway. command -v was causing
    problems because it is a shell builtin on some platforms.

--- test/java/r5js/NodeTest.java
+++ test/java/r5js/NodeTest.java
@@ -15,20 +15,13 @@ public final class NodeTest {
         File tmp = File.createTempFile("test-all", ".js");
         Files.write(tmp.toPath(), bytes);
 
-        Runtime runtime = Runtime.getRuntime();
-
-        int statusCode = runtime.exec(new String[]{"command", "-v", "node"}).waitFor();
-        ASSERT.withFailureMessage("node is required for testing")
-                .that(statusCode)
-                .isEqualTo(0);
-
         String outfilePathForNode = tmp.getAbsolutePath();
         outfilePathForNode = outfilePathForNode.substring(0, outfilePathForNode.length() - 3);
 
         String nodeCommand = String.format(
                 "require('%s').r5js.test.main(process.argv, process.env);", outfilePathForNode);
 
-        statusCode = new ProcessBuilder("node", "-e", nodeCommand, "type=unit", "verbose")
+        int statusCode = new ProcessBuilder("node", "-e", nodeCommand, "type=unit", "verbose")
                 .inheritIO()
                 .start()
                 .waitFor();
