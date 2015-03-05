754c4006827a706dc3e097171775b174b9e04f76
Brendan Linn <brendan.linn@gmail.com>
2015-03-05 07:32:52 -0800

    java: Platform: make things private where possible.

--- src/java/r5js/Platform.java
+++ src/java/r5js/Platform.java
@@ -21,27 +21,12 @@ import java.util.zip.ZipFile;
 
 final class Platform {
 
-    final String name;
-    final ImmutableList<CompilationUnit> inputs;
+    private final String name;
+    private final ImmutableList<CompilationUnit> inputs;
 
     private Platform(String name, ImmutableList<CompilationUnit> inputs) {
         this.name = name;
         this.inputs = inputs;
-    }
-
-    boolean relevant(Path path) {
-        return path.getFileName().toString().endsWith(".js")
-                && (path.startsWith("closure-library")
-                || (path.startsWith("src/js") && isRelevantSourcePath(path)));
-    }
-
-    private boolean isRelevantSourcePath(Path path) {
-        if (!path.startsWith("src/js/platform")) {
-            return true;
-        }
-
-        Path parent = path.getParent();
-        return parent.endsWith("platform") || parent.endsWith(name);
     }
 
     /**
@@ -55,6 +40,21 @@ final class Platform {
             outputs.add(build(input));
         }
         return outputs.build();
+    }
+
+    private boolean relevant(Path path) {
+        return path.getFileName().toString().endsWith(".js")
+                && (path.startsWith("closure-library")
+                || (path.startsWith("src/js") && isRelevantSourcePath(path)));
+    }
+
+    private boolean isRelevantSourcePath(Path path) {
+        if (!path.startsWith("src/js/platform")) {
+            return true;
+        }
+
+        Path parent = path.getParent();
+        return parent.endsWith("platform") || parent.endsWith(name);
     }
 
     private CompilationUnit.Output build(CompilationUnit input) throws IOException {
