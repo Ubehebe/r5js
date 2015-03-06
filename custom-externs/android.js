279b3aa6d605fa908a41c46512408a9a762f0b4e
Brendan Linn <brendan.linn@gmail.com>
2015-03-05 19:31:49 -0800

    externs: remove obsolete android externs.

--- custom-externs/android.js
+++ custom-externs/android.js
@@ -8,45 +8,5 @@
 var AndroidSchemePlatform;
 
 
-/** @return {string} */
-AndroidSchemePlatform.getR5RSSyntax = function() {};
-
-
-/** @return {string} */
-AndroidSchemePlatform.getR5RSProcedures = function() {};
-
-
-/** @return {string} */
-AndroidSchemePlatform.getTestFramework = function() {};
-
-
-/** @return {string} */
-AndroidSchemePlatform.getTestFrameworkTests = function() {};
-
-
-/** @return {string} */
-AndroidSchemePlatform.getR5RSTests = function() {};
-
-
-/** @return {string} */
-AndroidSchemePlatform.getNegativeTests = function() {};
-
-
-/** @return {string} */
-AndroidSchemePlatform.getOtherTests = function() {};
-
-
-/** @param {string} s */
-AndroidSchemePlatform.print = function(s) {};
-
-
-/** @param {string} s */
-AndroidSchemePlatform.returnValue = function(s) {};
-
-
-/** @param {string} s */
-AndroidSchemePlatform.error = function(s) {};
-
-
 /** @param {number} statusCode */
 AndroidSchemePlatform.exit = function(statusCode) {};
