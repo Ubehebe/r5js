0badf6c63a3464768dad69d462f02c82edb3d354
Brendan Linn <brendan.linn@gmail.com>
2015-03-07 09:18:22 -0800

    java: finally get tests running in nashorn.
    It's really slow and is broken by a JDK bug prior to 1.8.0_40.

--- src/java/r5js/platform/nashorn/Window.java
+++
@@ -1,7 +0,0 @@
-package r5js.platform.nashorn;
-
-public final class Window {
-    public void setTimeout(long callback, Object delay) {
-        System.out.printf("%d %s", callback, delay);
-    }
-}
--- test/java/r5js/NashornTest.java
+++ test/java/r5js/NashornTest.java
@@ -1,55 +1,12 @@
 package r5js;
 
-import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
-import org.junit.Before;
-import org.junit.Test;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
-
-import static com.google.common.truth.Truth.assertThat;
-
 public final class NashornTest {
 
-    private static final NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
-
-    private ScriptEngine scriptEngine;
-
-    @Before
-    public void setUp() {
-        scriptEngine = factory.getScriptEngine();
-    }
-
-//    @Test
-//    public void testPassthrough() throws Exception {
-//        byte[] bytes = Platforms.NASHORN.build().outputs.get(0).getBytes();
-//        scriptEngine.put("console", new Console());
-//        scriptEngine.put("setTimeout", new BiFunction<ScriptObjectMirror, ScriptObjectMirror, ScriptObjectMirror>() {
-//            @Override
-//            public ScriptObjectMirror apply(ScriptObjectMirror callback, ScriptObjectMirror delay) {
-//                callback.call()
-//                System.out.println("!!!");
-//                return delay;
-//            }
-//        });
-//        scriptEngine.eval(new String(bytes, StandardCharsets.UTF_8));
-//        Object hmm = scriptEngine.eval("r5js.test.main();");
-//        System.out.println("");
-//    }
-
-    @Test
-    public void installBindings() throws ScriptException {
-        scriptEngine.put("foo", 42);
-        Object blah = scriptEngine.eval("foo;");
-        assertThat(blah).isEqualTo(42);
-        scriptEngine.put("repeater", new Repeater());
-        Object blah2 = scriptEngine.eval("repeater.repeat('42');");
-        assertThat(blah2).isEqualTo("4242");
-    }
-
-    public static final class Repeater {
-        public String repeat(String input) {
-            return input + input;
-        }
-    }
+    /**
+     * This is disabled because it doesn't work prior to java -version 1.8.0_40
+     * When it does work, it's unbearably slow, much slower than shelling out to Node.
+     @Test public void testPassthrough() {
+     Application.launch(R5JSTests.class);
+     }
+     */
 }
