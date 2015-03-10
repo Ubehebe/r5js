0b7ab83038b633f49aa31963d0c2a4493d69b18d
Brendan Linn <brendan.linn@gmail.com>
2015-03-09 21:38:46 -0700

    test: write out the js binaries to target/.

--- test/java/r5js/CompilationTest.java
+++ test/java/r5js/CompilationTest.java
@@ -1,36 +1,44 @@
 package r5js;
 
+import com.google.common.base.Throwables;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class CompilationTest {
 
     @Test public void androidRepl() throws IOException {
-        Targets.ANDROID_REPL.build().outputs.forEach(CompilationTest::print);
+        Targets.ANDROID_REPL.build().outputs.forEach(CompilationTest::writeOut);
     }
 
     @Test public void androidTests() throws IOException{
-        Targets.ANDROID_TESTS.build().outputs.forEach(CompilationTest::print);
+        Targets.ANDROID_TESTS.build().outputs.forEach(CompilationTest::writeOut);
     }
 
     @Test public void html5Tests() throws IOException {
-        Targets.HTML5_TESTS.build().outputs.forEach(CompilationTest::print);
+        Targets.HTML5_TESTS.build().outputs.forEach(CompilationTest::writeOut);
     }
 
     @Test public void nashornTests() throws IOException {
-        Targets.NASHORN_TESTS.build().outputs.forEach(CompilationTest::print);
+        Targets.NASHORN_TESTS.build().outputs.forEach(CompilationTest::writeOut);
     }
 
     @Test public void nodeRepl() throws IOException {
-        Targets.NODE_REPL.build().outputs.forEach(CompilationTest::print);
+        Targets.NODE_REPL.build().outputs.forEach(CompilationTest::writeOut);
     }
 
     @Test public void nodeTests() throws IOException {
-        Targets.NODE_TESTS.build().outputs.forEach(CompilationTest::print);
+        Targets.NODE_TESTS.build().outputs.forEach(CompilationTest::writeOut);
     }
 
-    private static void print(CompilationUnitOutput output) {
+    private static void writeOut(CompilationUnitOutput output) {
         System.out.printf("%s\t%d%n", output.getBuildArtifactName(), output.getBytes().length);
+        try {
+            Files.write(Paths.get("target", output.getBuildArtifactName()), output.getBytes());
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
     }
 }
