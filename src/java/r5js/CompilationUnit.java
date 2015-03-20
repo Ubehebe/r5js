10f3d045dc5c02bcf4c2a2afdc5b98604e3d9c06
Brendan Linn <brendan.linn@gmail.com>
2015-03-19 19:56:27 -0700

    java: progress on investigating repl breakage.

--- src/java/r5js/CompilationUnit.java
+++ src/java/r5js/CompilationUnit.java
@@ -7,8 +7,10 @@ import com.google.javascript.jscomp.CompilationLevel;
 import com.google.javascript.jscomp.Compiler;
 import com.google.javascript.jscomp.CompilerOptions;
 import com.google.javascript.jscomp.DependencyOptions;
+import com.google.javascript.jscomp.PropertyRenamingPolicy;
 import com.google.javascript.jscomp.Result;
 import com.google.javascript.jscomp.SourceFile;
+import com.google.javascript.jscomp.VariableRenamingPolicy;
 import com.google.javascript.jscomp.WarningLevel;
 
 import java.io.IOException;
@@ -125,9 +127,11 @@ final class CompilationUnit {
         options.setInferTypes(true);
         CompilationLevel.ADVANCED_OPTIMIZATIONS.setOptionsForCompilationLevel(options);
 
-        // TODO bl: collapseProperties breaks the HTML5 and node REPLs. Investigate.
-        options.setCollapseProperties(false);
-
+        // TODO bl: ADVANCED_OPTIMIZATIONS renames all unquoted variables by default
+        // and also collapses properties. But these two options interact to crash
+        // the html5 and node repls. Setting to LOCAL for now to minimize code size.
+        options.setRenamingPolicy(
+                VariableRenamingPolicy.LOCAL, PropertyRenamingPolicy.ALL_UNQUOTED);
         WarningLevel.VERBOSE.setOptionsForWarningLevel(options);
         return options;
     }
