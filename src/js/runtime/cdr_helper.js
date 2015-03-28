afb4ba29ddf083e3583cdddeccc8083d314bc896
Brendan Linn <brendan.linn@gmail.com>
2015-03-27 19:15:01 -0700

    [es6] runtime: CdrHelper.

--- src/js/runtime/cdr_helper.js
+++ src/js/runtime/cdr_helper.js
@@ -30,68 +30,70 @@ goog.require('r5js.error');
  * @struct
  * @constructor
  */
-r5js.CdrHelper = function(head, startOfCdr) {
-  /** @const @private */ this.head_ = head;
+r5js.CdrHelper = class {
+    /**
+     * @param {!r5js.ast.CompoundDatum} head
+     * @param {!r5js.Datum} startOfCdr
+     */
+    constructor(head, startOfCdr) {
+        /** @const @private */ this.head_ = head;
 
-  /** @const @private {!r5js.Datum} */
-  this.startOfCdr_ = startOfCdr;
-};
+        /** @const @private {!r5js.Datum} */
+        this.startOfCdr_ = startOfCdr;
+    }
 
+    /**
+     * Basically, call set-car! on the master list.
+     * @param {!r5js.Datum} car TODO bl.
+     */
+    setCar(car) {
+        if (this.head_.isImmutable()) {
+            throw r5js.error.immutable(this.head_.toString());
+        }
+        this.head_.getFirstChild().setNextSibling(car);
+    }
 
-/**
- * Basically, call set-car! on the master list.
- * @param {!r5js.Datum} car TODO bl.
- */
-r5js.CdrHelper.prototype.setCar = function(car) {
-  if (this.head_.isImmutable()) {
-    throw r5js.error.immutable(this.head_.toString());
-  }
-  this.head_.getFirstChild().setNextSibling(car);
-};
+    /**
+     * Basically, call set-cdr! on the master list.
+     * @param {!r5js.Datum} cdr TODO bl.
+     */
+    setCdr(cdr) {
+        if (this.head_.isImmutable()) {
+            throw r5js.error.immutable(this.head_.toString());
+        }
+        this.startOfCdr_.setNextSibling(cdr);
+        if (!(cdr instanceof r5js.ast.List)) {
+            var cur = this;
+            do {
+                if (cur.head_ instanceof r5js.ast.List) {
+                    cur.head_.markDotted();
+                }
+            } while (cur = cur.head_.getCdrHelper());
+        }
+    }
 
+    /**
+     * @param {!r5js.CdrHelper} cdrHelper Helper to compare against.
+     * @return {boolean} True iff the two CdrHelpers point to the same list
+     * and have the same offset.
+     */
+    equals(cdrHelper) {
+        return this.head_ === cdrHelper.head_ &&
+            this.startOfCdr_ === cdrHelper.startOfCdr_;
+    }
 
-/**
- * Basically, call set-cdr! on the master list.
- * @param {!r5js.Datum} cdr TODO bl.
- */
-r5js.CdrHelper.prototype.setCdr = function(cdr) {
-  if (this.head_.isImmutable()) {
-    throw r5js.error.immutable(this.head_.toString());
-  }
-  this.startOfCdr_.setNextSibling(cdr);
-  if (!(cdr instanceof r5js.ast.List)) {
-    var cur = this;
-    do {
-      if (cur.head_ instanceof r5js.ast.List) {
-        cur.head_.markDotted();
-      }
-    } while (cur = cur.head_.getCdrHelper());
-  }
-};
-
-
-/**
- * @param {!r5js.CdrHelper} cdrHelper Helper to compare against.
- * @return {boolean} True iff the two CdrHelpers point to the same list
- * and have the same offset.
- */
-r5js.CdrHelper.prototype.equals = function(cdrHelper) {
-  return this.head_ === cdrHelper.head_ &&
-      this.startOfCdr_ === cdrHelper.startOfCdr_;
-};
-
-
-/**
- * @param {r5js.ast.CompoundDatum} datum The datum to test against.
- * @return {boolean} True iff the CdrHelper points to the given list datum
- * and its offset is that list's first child.
- */
-r5js.CdrHelper.prototype.resolvesTo = function(datum) {
-  if (!datum) {
-    return false;
-  } else if (this.head_ === datum) {
-    return this.startOfCdr_ === datum.getFirstChild();
-  } else {
-    return false;
-  }
+    /**
+     * @param {r5js.ast.CompoundDatum} datum The datum to test against.
+     * @return {boolean} True iff the CdrHelper points to the given list datum
+     * and its offset is that list's first child.
+     */
+    resolvesTo(datum) {
+        if (!datum) {
+            return false;
+        } else if (this.head_ === datum) {
+            return this.startOfCdr_ === datum.getFirstChild();
+        } else {
+            return false;
+        }
+    }
 };
