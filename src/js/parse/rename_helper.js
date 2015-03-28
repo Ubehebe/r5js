d2ce0cc86a1797c615ea0315e62a16d46b1f37a1
Brendan Linn <brendan.linn@gmail.com>
2015-03-27 19:06:41 -0700

    [es6] parse: RenameHelper.

--- src/js/parse/rename_helper.js
+++ src/js/parse/rename_helper.js
@@ -19,48 +19,50 @@ goog.provide('r5js.RenameHelper');
 
 
 /**
- * @param {r5js.RenameHelper} parent The parent helper, if any.
  * @struct
  * @constructor
  */
-r5js.RenameHelper = function(parent) {
-  /** @const @private {!Object<string, string>} */ this.bindings_ = {};
-  /** @const @private */ this.parent_ = parent;
-};
+r5js.RenameHelper = class {
 
+    /**
+     * @param {r5js.RenameHelper} parent The parent helper, if any.
+     */
+    constructor(parent) {
+        /** @const @private {!Object<string, string>} */ this.bindings_ = {};
+        /** @const @private */ this.parent_ = parent;
+    }
 
-/**
- * @param {string} from Name to add a renaming for.
- * @return {string} A new name for the given name.
- */
-r5js.RenameHelper.prototype.addRenameBinding = function(from) {
-  var to = newCpsName();
-  this.bindings_[from] = to;
-  return to;
-};
+    /**
+     * @param {string} from Name to add a renaming for.
+     * @return {string} A new name for the given name.
+     */
+    addRenameBinding(from) {
+        var to = newCpsName();
+        this.bindings_[from] = to;
+        return to;
+    }
 
+    /**
+     * @param {string} name Name to look up rename binding for.
+     * @return {?string} The renaming of this name, or null if this object
+     * has no such binding.
+     */
+    getRenameBinding(name) {
+        var maybe = this.bindings_[name];
+        if (maybe) {
+            return maybe;
+        } else if (this.parent_) {
+            return this.parent_.getRenameBinding(name);
+        } else {
+            return null;
+        }
+    }
 
-/**
- * @param {string} name Name to look up rename binding for.
- * @return {?string} The renaming of this name, or null if this object
- * has no such binding.
- */
-r5js.RenameHelper.prototype.getRenameBinding = function(name) {
-  var maybe = this.bindings_[name];
-  if (maybe) {
-    return maybe;
-  } else if (this.parent_) {
-    return this.parent_.getRenameBinding(name);
-  } else {
-    return null;
-  }
-};
-
-
-/** @return {boolean} True iff the helper was used. */
-r5js.RenameHelper.prototype.wasUsed = function() {
-  for (var name in this.bindings_) {
-    return true;
-  }
-  return false;
+    /** @return {boolean} True iff the helper was used. */
+    wasUsed() {
+        for (var name in this.bindings_) {
+            return true;
+        }
+        return false;
+    }
 };
