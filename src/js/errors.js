6786be13840e016a149f72df7d27d1d9d3f1cdd6
Brendan Linn <brendan.linn@gmail.com>
2015-04-02 06:26:59 -0700

    [es6]: Error.

--- src/js/errors.js
+++ src/js/errors.js
@@ -1,19 +1,3 @@
-/* Copyright 2011-2014 Brendan Linn
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-
 goog.provide('r5js.Error');
 goog.provide('r5js.error');
 
@@ -22,16 +6,16 @@ goog.require('r5js.Token');
 
 
 
-/**
- * @param {!r5js.Error.Type} type
- * @param {string} msg Human-readable error message.
- * @param {...*} var_args
- * @struct
- * @constructor
- */
-r5js.Error = function(type, msg, var_args) {
-  /** @const */ this.type = type;
-  /** @const */ this.msg = msg;
+r5js.Error = class {
+    /**
+     * @param {!r5js.Error.Type} type
+     * @param {string} msg Human-readable error message.
+     * @param {...*} var_args
+     */
+    constructor(type, msg, var_args) {
+        /** @const */ this.type = type;
+        /** @const */ this.msg = msg;
+    }
 };
 
 
