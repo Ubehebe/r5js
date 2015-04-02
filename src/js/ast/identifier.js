d9cfd1ce7eac15290e078963221665a7d80c2692
Brendan Linn <brendan.linn@gmail.com>
2015-04-01 20:27:30 -0700

    [es6] ast: Identifier.

--- src/js/ast/identifier.js
+++ src/js/ast/identifier.js
@@ -22,44 +22,37 @@ goog.require('r5js.parse.Terminals');
 
 
 
-/**
- * @param {string} name
- * @extends {r5js.ast.SimpleDatum<string>}
- * @struct
- * @constructor
- */
-r5js.ast.Identifier = function(name) {
-  r5js.ast.Identifier.base(this, 'constructor', name);
-};
-goog.inherits(r5js.ast.Identifier, r5js.ast.SimpleDatum);
-
-
-/**
- * @return {boolean} True iff this Datum is in a quasiquotation and should be
- * unquoted (i.e. starts with a ,).
- */
-r5js.ast.Identifier.prototype.shouldUnquote = function() {
-  return this.payload.charAt(0) === r5js.parse.Terminals.COMMA;
-};
-
-
-/**
- * This is a subcase of shouldUnquote, because unquotes and unquote-splicings
- * have pretty much the same logic.
- * @return {boolean} TODO bl.
- * @suppress {accessControls} for r5js.Datum.CPS_PREFIX_
- */
-r5js.ast.Identifier.prototype.shouldUnquoteSplice = function() {
-  return this.payload.charAt(1) === r5js.Datum.CPS_PREFIX_;
-};
-
-
-/** @override */
-r5js.ast.Identifier.prototype.fixParserSensitiveIds = function(helper) {
-  if (isParserSensitiveId(this.payload)) {
-    var renamedAs = helper.getRenameBinding(this.payload);
-    if (renamedAs) {
-      this.setPayload(renamedAs);
+r5js.ast.Identifier = /** @extends {r5js.ast.SimpleDatum<string>} */ class extends r5js.ast.SimpleDatum {
+    /** @param {string} name */
+    constructor(name) {
+        super(name);
     }
-  }
+
+    /**
+     * @return {boolean} True iff this Datum is in a quasiquotation and should be
+     * unquoted (i.e. starts with a ,).
+     */
+    shouldUnquote() {
+        return this.payload.charAt(0) === r5js.parse.Terminals.COMMA;
+    }
+
+    /**
+     * This is a subcase of shouldUnquote, because unquotes and unquote-splicings
+     * have pretty much the same logic.
+     * @return {boolean} TODO bl.
+     * @suppress {accessControls} for r5js.Datum.CPS_PREFIX_
+     */
+    shouldUnquoteSplice() {
+        return this.payload.charAt(1) === r5js.Datum.CPS_PREFIX_;
+    }
+
+    /** @override */
+    fixParserSensitiveIds(helper) {
+        if (isParserSensitiveId(this.payload)) {
+            var renamedAs = helper.getRenameBinding(this.payload);
+            if (renamedAs) {
+                this.setPayload(renamedAs);
+            }
+        }
+    }
 };
