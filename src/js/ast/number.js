7e8c16f105406cb91f5ed485a5de1ff8d8155f25
Brendan Linn <brendan.linn@gmail.com>
2015-04-01 20:39:39 -0700

    [es6] ast: Number.

--- src/js/ast/number.js
+++ src/js/ast/number.js
@@ -1,18 +1,3 @@
-/* Copyright 2011-2014 Brendan Linn
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
 goog.provide('r5js.ast.Number');
 
 
@@ -20,13 +5,9 @@ goog.require('r5js.ast.SimpleDatum');
 
 
 
-/**
- * @param {number} x
- * @extends {r5js.ast.SimpleDatum<number>}
- * @struct
- * @constructor
- */
-r5js.ast.Number = function(x) {
-  r5js.ast.Number.base(this, 'constructor', x);
+r5js.ast.Number = /** @extends {r5js.ast.SimpleDatum<number>} */ class extends r5js.ast.SimpleDatum {
+ /** @param {number} x */
+  constructor(x) {
+   super(x);
+  }
 };
-goog.inherits(r5js.ast.Number, r5js.ast.SimpleDatum);
