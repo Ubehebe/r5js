4284de18eaef8792bbfdd10ce352a111da7f33d0
Brendan Linn <brendan.linn@gmail.com>
2015-04-03 06:17:04 -0700

    [es6] platform: SchemeSources.

--- src/js/platform/scheme_sources.js
+++ src/js/platform/scheme_sources.js
@@ -1,39 +1,21 @@
-/* Copyright 2011-2014 Brendan Linn
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
 goog.provide('r5js.SchemeSources');
 
 goog.require('PROCEDURES');
 goog.require('SYNTAX');
 goog.require('goog.Promise');
 
+r5js.SchemeSources = class {
+    /**
+     * @param {string} syntax
+     * @param {string} procedures
+     */
+    constructor(syntax, procedures) {
+        /** @const */ this.syntax = syntax;
+        /** @const */ this.procedures = procedures;
+    }
 
-
-/**
- * @param {string} syntax
- * @param {string} procedures
- * @struct
- * @constructor
- */
-r5js.SchemeSources = function(syntax, procedures) {
-  /** @const */ this.syntax = syntax;
-  /** @const */ this.procedures = procedures;
-};
-
-
-/** @return {!goog.Promise<!r5js.SchemeSources>} */
-r5js.SchemeSources.get = function() {
-  return goog.Promise.resolve(new r5js.SchemeSources(SYNTAX, PROCEDURES));
+    /** @return {!goog.Promise<!r5js.SchemeSources>} */
+    static get() {
+        return goog.Promise.resolve(new r5js.SchemeSources(SYNTAX, PROCEDURES));
+    }
 };
