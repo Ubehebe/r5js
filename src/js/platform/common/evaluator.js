1ad0b40b6d94ae2cb7aea3bbcca353b7530194d3
Brendan Linn <brendan.linn@gmail.com>
2015-04-03 06:11:35 -0700

    [es6] platform/common: Evaluator.

--- src/js/platform/common/evaluator.js
+++ src/js/platform/common/evaluator.js
@@ -1,27 +1,9 @@
-/* Copyright 2011-2015 Brendan Linn
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-
 goog.provide('r5js.platform.common.newEvaluator');
-
 
 goog.require('goog.Promise');
 goog.require('r5js.Evaluator');
 goog.require('r5js.SchemeSources');
 goog.require('r5js.boot');
-
 
 /**
  * @param {!r5js.InputPort=} opt_inputPort
@@ -41,28 +23,23 @@ r5js.platform.common.newEvaluator =
   });
 };
 
-
-
 /**
  * Evaluator implementation that simply wraps a synchronous evaluator
  * in promises. This is appropriate for most non-web platforms, since these
  * typically can run JavaScript synchronously off the main thread.
- * @param {!r5js.sync.Evaluator} evaluator
- * @implements {r5js.Evaluator}
- * @struct
- * @constructor
- * @private
  */
-r5js.platform.common.Evaluator_ = function(evaluator) {
-  /** @const @private */ this.evaluator_ = evaluator;
-};
+r5js.platform.common.Evaluator_ = /** @private @implements {r5js.Evaluator} */ class {
+    /** @param {!r5js.sync.Evaluator} evaluator */
+    constructor(evaluator) {
+        /** @const @private */ this.evaluator_ = evaluator;
+    }
 
-
-/** @override */
-r5js.platform.common.Evaluator_.prototype.evaluate = function(input) {
-  try {
-    return goog.Promise.resolve(this.evaluator_.evaluate(input));
-  } catch (e) {
-    return goog.Promise.reject(e);
-  }
+    /** @override */
+    evaluate(input) {
+        try {
+            return goog.Promise.resolve(this.evaluator_.evaluate(input));
+        } catch (e) {
+            return goog.Promise.reject(e);
+        }
+    }
 };
