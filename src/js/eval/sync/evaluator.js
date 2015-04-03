5c963dc877842a0868593fe2b66a6b8e257e7b29
Brendan Linn <brendan.linn@gmail.com>
2015-04-03 06:30:35 -0700

    [es6 module] eval/sync: Evaluator.

--- src/js/eval/sync/evaluator.js
+++ src/js/eval/sync/evaluator.js
@@ -1,26 +1,13 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.sync.Evaluator');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.sync.Evaluator');
-
-
-r5js.sync.Evaluator = /** @interface */ class {
+/** @interface */
+class Evaluator {
  /**
   * @param {string} input
   * @return {string}
   * @throws {!r5js.Error}
   */
  evaluate(input) {}
-};
+}
+
+exports = Evaluator;
