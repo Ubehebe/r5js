8f2c44db73ef07bd6608e9903582119407cc6df9
Brendan Linn <brendan.linn@gmail.com>
2015-04-03 19:03:26 -0700

    [es6 module] io: PortManager.

--- src/js/io/port_manager.js
+++ src/js/io/port_manager.js
@@ -1,57 +1,38 @@
-/* Copyright 2011-2015 Brendan Linn
+goog.module('r5js.PortManager');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const InputPort = goog.require('r5js.InputPort');
+const OutputPort = goog.require('r5js.OutputPort');
+const InMemoryInputPort = goog.require('r5js.InMemoryInputPort');
+const InMemoryOutputPort = goog.require('r5js.InMemoryOutputPort');
+const InMemoryPortBuffer = goog.require('r5js.InMemoryPortBuffer');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-
-goog.provide('r5js.PortManager');
-
-
-goog.require('r5js.InMemoryInputPort');
-goog.require('r5js.InMemoryOutputPort');
-goog.require('r5js.InMemoryPortBuffer');
-
-
-
-/**
- * @struct
- * @constructor
- */
-r5js.PortManager = class {
+class PortManager {
     constructor() {
-        /** @const @private {!Object<string, !r5js.InMemoryPortBuffer>} */
+        /** @const @private {!Object<string, !InMemoryPortBuffer>} */
         this.buffers_ = {};
     }
 
     /**
      * @param {string} name
-     * @return {!r5js.InputPort}
+     * @return {!InputPort}
      */
     newInputPort(name) {
         if (!(name in this.buffers_)) {
-            this.buffers_[name] = new r5js.InMemoryPortBuffer();
+            this.buffers_[name] = new InMemoryPortBuffer();
         }
-        return new r5js.InMemoryInputPort(this.buffers_[name]);
+        return new InMemoryInputPort(this.buffers_[name]);
     }
 
     /**
      * @param {string} name
-     * @return {!r5js.OutputPort}
+     * @return {!OutputPort}
      */
     newOutputPort(name) {
         if (!(name in this.buffers_)) {
-            this.buffers_[name] = new r5js.InMemoryPortBuffer();
+            this.buffers_[name] = new InMemoryPortBuffer();
         }
-        return new r5js.InMemoryOutputPort(this.buffers_[name]);
+        return new InMemoryOutputPort(this.buffers_[name]);
     }
-};
+}
+
+exports = PortManager;
