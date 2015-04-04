dda04205b02e7b8eaee7213be4f3382e381a0ade
Brendan Linn <brendan.linn@gmail.com>
2015-04-03 19:08:17 -0700

    [es6 module] eval: Evaluator.

--- src/js/eval/evaluator.js
+++ src/js/eval/evaluator.js
@@ -1,36 +1,24 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.Evaluator');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.Evaluator');
-
-
+const Promise = goog.require('goog.Promise');
 
 /**
  * This is the most important interface in the whole codebase,
  * the main abstraction used by clients to evaluate Scheme source code.
  * Its methods are asynchronous, returning Promises, because
  * evaluation should not block the main thread (for example, the browser UI).
+ * @interface
  */
-r5js.Evaluator = /** @interface */ class {
+class Evaluator {
  /**
   * @param {string} input Input to evaluate.
-  * @return {!goog.Promise<string>}  If evaluation succeeds,
+  * @return {!Promise<string>}  If evaluation succeeds,
   * this promise will be resolved with a string representation of the Scheme
   * value (as if it was serialized with the {@code write} procedure).
   * If evaluation fails, the promise will be rejected with an {@link r5js.Error}
   * explaining what went wrong.
   */
  evaluate(input) {}
-};
+}
+
+exports = Evaluator;
