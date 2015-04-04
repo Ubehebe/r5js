8d8d1adf8a1a881623dfbb7a645750a44d5ca859
Brendan Linn <brendan.linn@gmail.com>
2015-04-03 18:37:03 -0700

    [es6] io: CallbackBackedPort.

--- src/js/io/callback_backed_port.js
+++ src/js/io/callback_backed_port.js
@@ -1,44 +1,25 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.CallbackBackedPort');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const OutputPort = goog.require('r5js.OutputPort');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-
-goog.provide('r5js.CallbackBackedPort');
-
-
-goog.require('r5js.OutputPort');
-
-
-
-/**
- * @param {function(string)} onOutput Callback that will be called
- * whenever output is available.
- * @implements {r5js.OutputPort}
- * @struct
- * @constructor
- */
-r5js.CallbackBackedPort = function(onOutput) {
+/** @implements {OutputPort} */
+class CallbackBackedPort {
+ /**
+  * @param {function(string)} onOutput Callback that will be called
+  * whenever output is available.
+  */
+ constructor(onOutput) {
   /** @const @private */ this.onOutput_ = onOutput;
-};
-r5js.OutputPort.addImplementation(r5js.CallbackBackedPort);
+ }
 
+ /** @override */
+ close() {}
 
-/** @override */
-r5js.CallbackBackedPort.prototype.close = goog.nullFunction;
-
-
-/** @override */
-r5js.CallbackBackedPort.prototype.write = function(str) {
+ /** @override */
+ write(str) {
   this.onOutput_(str);
-};
+ }
+}
+
+OutputPort.addImplementation(CallbackBackedPort);
+exports = CallbackBackedPort;
