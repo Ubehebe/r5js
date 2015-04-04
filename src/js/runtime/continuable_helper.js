c166bd5894546dfe543083c8efb7df0fdb404d53
Brendan Linn <brendan.linn@gmail.com>
2015-04-04 15:53:57 -0700

    [es6 module] runtime: ContinuableHelper.

--- src/js/runtime/continuable_helper.js
+++ src/js/runtime/continuable_helper.js
@@ -1,18 +1,18 @@
-goog.provide('r5js.ContinuableHelper');
+goog.module('r5js.ContinuableHelper');
 
-goog.require('r5js.ProcCallLike');
+const ProcCallLike = goog.require('r5js.ProcCallLike');
 
 /**
  * A buffer to accumulate a Continuable-Continuation chain
  * without the caller having to do the pointer arithmetic.
  */
-r5js.ContinuableHelper = class {
+class ContinuableHelper {
     constructor() {
-        /** @private {r5js.ProcCallLike} */ this.firstProcCallLike_ = null;
-        /** @private {r5js.ProcCallLike} */ this.lastProcCallLike_ = null;
+        /** @private {ProcCallLike} */ this.firstProcCallLike_ = null;
+        /** @private {ProcCallLike} */ this.lastProcCallLike_ = null;
     }
 
-    /** @param {!r5js.ProcCallLike} procCallLike A continuable object. */
+    /** @param {!ProcCallLike} procCallLike A continuable object. */
     appendProcCallLike(procCallLike) {
         if (!this.firstProcCallLike_) {
             this.firstProcCallLike_ = procCallLike;
@@ -22,8 +22,10 @@ r5js.ContinuableHelper = class {
         this.lastProcCallLike_ = r5js.ProcCallLike.getLast(procCallLike);
     }
 
-    /** @return {r5js.ProcCallLike} */
+    /** @return {ProcCallLike} */
     toContinuable() {
         return this.firstProcCallLike_;
     }
-};
+}
+
+exports = ContinuableHelper;
