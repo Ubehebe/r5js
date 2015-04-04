e646718264b34f1c83d0ea7d61daaf4ad08349a0
Brendan Linn <brendan.linn@gmail.com>
2015-04-04 16:20:22 -0700

    [es6 module] runtime: VarargsUserDefinedProcedure.

--- src/js/runtime/varargs_user_defined_procedure.js
+++ src/js/runtime/varargs_user_defined_procedure.js
@@ -1,73 +1,52 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.VarargsUserDefinedProcedure');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const Datum = goog.require('r5js.Datum');
+const IEnvironment = goog.require('r5js.IEnvironment');
+const SiblingBuffer = goog.require('r5js.SiblingBuffer');
+const UserDefinedProcedure = goog.require('r5js.UserDefinedProcedure');
+const List = goog.require('r5js.ast.List');
+const datumutil = goog.require('r5js.datumutil');
+const error = goog.require('r5js.error');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.VarargsUserDefinedProcedure');
-
-
-goog.require('r5js.SiblingBuffer');
-goog.require('r5js.UserDefinedProcedure');
-goog.require('r5js.ast.List');
-goog.require('r5js.datumutil');
-goog.require('r5js.error');
-
-
-
-/**
- * @param {!Array<string>} formalsArray The procedure's formal parameters,
- * in order.
- * @param {r5js.Datum} bodyStart
- * @param {!r5js.IEnvironment} env
- * @param {string=} opt_name The procedure's name, for pretty-printing and
- * error messages. If not given, one will be created.
- * @extends {r5js.UserDefinedProcedure}
- * @struct
- * @constructor
- */
-r5js.VarargsUserDefinedProcedure = function(
-    formalsArray, bodyStart, env, opt_name) {
-  r5js.VarargsUserDefinedProcedure.base(
-      this, 'constructor', formalsArray, bodyStart, env, opt_name);
-};
-goog.inherits(r5js.VarargsUserDefinedProcedure, r5js.UserDefinedProcedure);
-
-
-/** @override */
-r5js.VarargsUserDefinedProcedure.prototype.checkNumArgs = function(numActuals) {
-  const minNumArgs = this.formalsArray.length - 1;
-  if (numActuals < minNumArgs) {
-    throw r5js.error.tooFewVarargs(this.toString(), minNumArgs, numActuals);
-  }
-};
-
-
-/** @override */
-r5js.VarargsUserDefinedProcedure.prototype.bindArgs = function(args, env) {
-  let name, i;
-
-  for (i = 0; i < this.formalsArray.length - 1; ++i) {
-    name = this.formalsArray[i];
-    env.addBinding(name, args[i]);
-  }
-
-  if (this.formalsArray.length > 0) {
-    name = this.formalsArray[i];
-    // Roll up the remaining arguments into a list
-    let siblingBuffer = new r5js.SiblingBuffer();
-    for (let j = i; j < args.length; ++j) {
-      siblingBuffer.appendSibling(r5js.datumutil.wrapValue(args[j]));
+class VarargsUserDefinedProcedure extends UserDefinedProcedure {
+    /**
+     * @param {!Array<string>} formalsArray The procedure's formal parameters, in order.
+     * @param {Datum} bodyStart
+     * @param {!IEnvironment} env
+     * @param {string=} opt_name The procedure's name, for pretty-printing and
+     * error messages. If not given, one will be created.
+     */
+    constructor(formalsArray, bodyStart, env, opt_name) {
+        super(formalsArray, bodyStart, env, opt_name);
     }
-    env.addBinding(name, siblingBuffer.toList(r5js.ast.List));
-  }
-};
+
+    /** @override */
+    checkNumArgs(numActuals) {
+        const minNumArgs = this.formalsArray.length - 1;
+        if (numActuals < minNumArgs) {
+            throw error.tooFewVarargs(this.toString(), minNumArgs, numActuals);
+        }
+    }
+
+    /** @override */
+    bindArgs(args, env) {
+        let name, i;
+
+        for (i = 0; i < this.formalsArray.length - 1; ++i) {
+            name = this.formalsArray[i];
+            env.addBinding(name, args[i]);
+        }
+
+        if (this.formalsArray.length > 0) {
+            name = this.formalsArray[i];
+            // Roll up the remaining arguments into a list
+            let siblingBuffer = new r5js.SiblingBuffer();
+            for (let j = i; j < args.length; ++j) {
+                siblingBuffer.appendSibling(datumutil.wrapValue(args[j]));
+            }
+            env.addBinding(name, siblingBuffer.toList(List));
+        }
+    }
+}
+
+exports = VarargsUserDefinedProcedure;
