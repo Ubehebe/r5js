04a39215f75e69da860c55735af69f4a57072654
Brendan Linn <brendan.linn@gmail.com>
2015-04-04 15:37:53 -0700

    [es6 module] runtime: CdrHelper.

--- src/js/runtime/cdr_helper.js
+++ src/js/runtime/cdr_helper.js
@@ -1,71 +1,50 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.CdrHelper');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-
-goog.provide('r5js.CdrHelper');
-
-
-goog.require('r5js.ast.List');
-goog.require('r5js.error');
-
-
+const CompoundDatum = goog.require('r5js.ast.CompoundDatum');
+const Datum = goog.require('r5js.Datum');
+const List = goog.require('r5js.ast.List');
+const error = goog.require('r5js.error');
 
 /**
  * See the comment to {@link r5js.Datum.siblingsToList}
  * for an explanation of what this class does.
- * @param {!r5js.ast.CompoundDatum} head
- * @param {!r5js.Datum} startOfCdr
- * @struct
- * @constructor
  */
-r5js.CdrHelper = class {
+class CdrHelper {
     /**
-     * @param {!r5js.ast.CompoundDatum} head
-     * @param {!r5js.Datum} startOfCdr
+     * @param {!CompoundDatum} head
+     * @param {!Datum} startOfCdr
      */
     constructor(head, startOfCdr) {
         /** @const @private */ this.head_ = head;
 
-        /** @const @private {!r5js.Datum} */
+        /** @const @private {!Datum} */
         this.startOfCdr_ = startOfCdr;
     }
 
     /**
      * Basically, call set-car! on the master list.
-     * @param {!r5js.Datum} car TODO bl.
+     * @param {!Datum} car TODO bl.
      */
     setCar(car) {
         if (this.head_.isImmutable()) {
-            throw r5js.error.immutable(this.head_.toString());
+            throw error.immutable(this.head_.toString());
         }
         this.head_.getFirstChild().setNextSibling(car);
     }
 
     /**
      * Basically, call set-cdr! on the master list.
-     * @param {!r5js.Datum} cdr TODO bl.
+     * @param {!Datum} cdr TODO bl.
      */
     setCdr(cdr) {
         if (this.head_.isImmutable()) {
-            throw r5js.error.immutable(this.head_.toString());
+            throw error.immutable(this.head_.toString());
         }
         this.startOfCdr_.setNextSibling(cdr);
-        if (!(cdr instanceof r5js.ast.List)) {
+        if (!(cdr instanceof List)) {
             let cur = this;
             do {
-                if (cur.head_ instanceof r5js.ast.List) {
+                if (cur.head_ instanceof List) {
                     cur.head_.markDotted();
                 }
             } while (cur = cur.head_.getCdrHelper());
@@ -73,7 +52,7 @@ r5js.CdrHelper = class {
     }
 
     /**
-     * @param {!r5js.CdrHelper} cdrHelper Helper to compare against.
+     * @param {!CdrHelper} cdrHelper Helper to compare against.
      * @return {boolean} True iff the two CdrHelpers point to the same list
      * and have the same offset.
      */
@@ -83,7 +62,7 @@ r5js.CdrHelper = class {
     }
 
     /**
-     * @param {r5js.ast.CompoundDatum} datum The datum to test against.
+     * @param {!CompoundDatum} datum The datum to test against.
      * @return {boolean} True iff the CdrHelper points to the given list datum
      * and its offset is that list's first child.
      */
@@ -96,4 +75,6 @@ r5js.CdrHelper = class {
             return false;
         }
     }
-};
+}
+
+exports = CdrHelper;
