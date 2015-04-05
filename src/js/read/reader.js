b9fb98b846c13cd78fa4ad7cdfade2f490c3bc4a
Brendan Linn <brendan.linn@gmail.com>
2015-04-04 22:48:26 -0700

    [es6 module] read: Reader.

--- src/js/read/reader.js
+++ src/js/read/reader.js
@@ -1,28 +1,17 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.Reader');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const Datum = goog.require('r5js.Datum');
+const Error = goog.require('r5js.Error');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.Reader');
-
-
-
-r5js.Reader = /** @interface */ class {
+/** @interface */
+class Reader {
  /**
-  * @return {!r5js.Datum} The root of the datum tree.
+  * @return {!Datum} The root of the datum tree.
   * If reading the tokens into datums was unsuccessful, a {@link r5js.ReadError}
   * is thrown.
-  * @throws {r5js.Error}
+  * @throws {Error}
   */
  read() {}
-};
+}
+
+exports = Reader;
