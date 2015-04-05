861f69167dda0f92d59fcea944b9ddd7d7b1da7f
Brendan Linn <brendan.linn@gmail.com>
2015-04-05 08:42:33 -0700

    [es6 module] eval: valutil.

--- src/js/eval/valutil.js
+++ src/js/eval/valutil.js
@@ -1,29 +1,22 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.valutil');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.valutil');
-
-
-
-goog.require('r5js.Environment');
-goog.require('r5js.InputPort');
-goog.require('r5js.OutputPort');
-goog.require('r5js.UserDefinedProcedure');
-goog.require('r5js.ast.Quote');
-goog.require('r5js.parse.Terminals');
-goog.require('r5js.runtime.UNSPECIFIED_VALUE');
+const Character = goog.require('r5js.ast.Character');
+const Datum = goog.require('r5js.Datum');
+const Environment = goog.require('r5js.Environment');
+const EOF = goog.require('r5js.runtime.EOF');
+const InputPort = goog.require('r5js.InputPort');
+const DottedList = goog.require('r5js.ast.DottedList');
+const List = goog.require('r5js.ast.List');
+const OutputPort = goog.require('r5js.OutputPort');
+const procspec = goog.require('r5js.procspec');
+const Quote = goog.require('r5js.ast.Quote');
+const Ref = goog.require('r5js.Ref');
+const String = goog.require('r5js.ast.String');
+const Terminals = goog.require('r5js.parse.Terminals');
+const UNSPECIFIED_VALUE = goog.require('r5js.runtime.UNSPECIFIED_VALUE');
+const UserDefinedProcedure = goog.require('r5js.UserDefinedProcedure');
+const Value = goog.require('r5js.runtime.Value');
+const Vector = goog.require('r5js.ast.Vector');
 
 /*
  * Implementation note: a richer representation of Scheme values would
@@ -49,32 +42,29 @@ goog.require('r5js.runtime.UNSPECIFIED_VALUE');
  * satisfactory.
  */
 
-
 /**
- * @param {!r5js.runtime.Value} value
+ * @param {!Value} value
  * @return {string}
  */
-r5js.valutil.toDisplayString = function(value) {
-  return r5js.valutil.toString_(false /* includeSigils */, value);
-};
-
+function toDisplayString(value) {
+  return toString_(false /* includeSigils */, value);
+}
 
 /**
- * @param {!r5js.runtime.Value} value
+ * @param {!Value} value
  * @return {string}
  */
-r5js.valutil.toWriteString = function(value) {
-  return r5js.valutil.toString_(true /* includeSigils */, value);
-};
-
+function toWriteString(value) {
+  return toString_(true /* includeSigils */, value);
+}
 
 /**
  * @param {boolean} includeSigils
- * @param {!r5js.runtime.Value} value
+ * @param {!Value} value
  * @return {string}
  * @private
  */
-r5js.valutil.toString_ = function(includeSigils, value) {
+function toString_(includeSigils, value) {
   switch (typeof value) {
     case 'number':
       return value + '';
@@ -83,34 +73,33 @@ r5js.valutil.toString_ = function(includeSigils, value) {
     case 'string':
       return value;
     case 'object':
-      if (value === r5js.runtime.UNSPECIFIED_VALUE) {
+      if (value === UNSPECIFIED_VALUE) {
         return '';
-      } else if (value === r5js.runtime.EOF) {
+      } else if (value === EOF) {
         return '<eof>';
-      } else if (value instanceof r5js.Ref) {
-        return r5js.valutil.toString_(includeSigils, value.deref());
-      } else if (value instanceof r5js.ast.List ||
-          value instanceof r5js.ast.DottedList) {
+      } else if (value instanceof Ref) {
+        return toString_(includeSigils, value.deref());
+      } else if (value instanceof List || value instanceof DottedList) {
         const children = value.mapChildren(
-            goog.partial(r5js.valutil.toString_, includeSigils));
-        if ((value instanceof r5js.ast.List && value.isImproperList()) ||
-            value instanceof r5js.ast.DottedList) {
-          children.splice(children.length - 1, 0, r5js.parse.Terminals.DOT);
+            goog.partial(toString_, includeSigils));
+        if ((value instanceof List && value.isImproperList())
+            || value instanceof DottedList) {
+          children.splice(children.length - 1, 0, Terminals.DOT);
         }
-        return r5js.parse.Terminals.LPAREN +
+        return Terminals.LPAREN +
             children.join(' ') +
-            r5js.parse.Terminals.RPAREN;
-      } else if (value instanceof r5js.ast.Vector) {
+            Terminals.RPAREN;
+      } else if (value instanceof Vector) {
         const childStrings = value.mapChildren(
-            goog.partial(r5js.valutil.toString_, includeSigils)).join(' ');
-        return r5js.parse.Terminals.LPAREN_VECTOR +
+            goog.partial(toString_, includeSigils)).join(' ');
+        return Terminals.LPAREN_VECTOR +
             childStrings +
-            r5js.parse.Terminals.RPAREN;
-      } else if (value instanceof r5js.ast.String) {
+            Terminals.RPAREN;
+      } else if (value instanceof String) {
         return includeSigils ?
             '"' + value.getPayload() + '"' : // TODO bl escape
             value.getPayload();
-      } else if (value instanceof r5js.ast.Character) {
+      } else if (value instanceof Character) {
         if (includeSigils) {
           // Special cases for space and newline: R5RS 6.3.4
           const payload = value.getPayload();
@@ -124,26 +113,31 @@ r5js.valutil.toString_ = function(includeSigils, value) {
         } else {
           return value.getPayload();
         }
-      } else if (value instanceof r5js.ast.Quote) {
-        return r5js.parse.Terminals.TICK + r5js.valutil.toString_(
+      } else if (value instanceof Quote) {
+        return Terminals.TICK + toString_(
             includeSigils,
-            /** @type {!r5js.runtime.Value} */ (value.getFirstChild()));
-      } else if (value instanceof r5js.UserDefinedProcedure) {
+            /** @type {!Value} */ (value.getFirstChild()));
+      } else if (value instanceof UserDefinedProcedure) {
         return '<proc:' + value.getName() + '>';
-      } else if (value instanceof r5js.procspec.PrimitiveProcedure_) {
+      } else if (value instanceof procspec.PrimitiveProcedure_) {
         return '<proc:' + value.getDebugName() + '>';
-      } else if (r5js.InputPort.isImplementedBy(value)) {
+      } else if (InputPort.isImplementedBy(value)) {
         return '<input-port>';
-      } else if (r5js.OutputPort.isImplementedBy(value)) {
+      } else if (OutputPort.isImplementedBy(value)) {
         return '<output-port>';
-      } else if (value instanceof r5js.Datum) {
-        return r5js.valutil.toString_(includeSigils, value.unwrap());
-      } else if (value instanceof r5js.Environment) {
+      } else if (value instanceof Datum) {
+        return toString_(includeSigils, value.unwrap());
+      } else if (value instanceof Environment) {
         return '<environment-specifier>';
       }
     default:
       return '';
   }
+}
+
+exports = {
+    toDisplayString: toDisplayString,
+    toWriteString: toWriteString
 };
 
 
