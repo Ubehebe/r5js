fc8250a792b8d5424e7f6f20d2b752290f99a1bb
Brendan Linn <brendan.linn@gmail.com>
2015-04-05 09:23:18 -0700

    [es6 module] boot.

--- src/js/boot.js
+++ src/js/boot.js
@@ -1,33 +1,19 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.boot');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-
-goog.provide('r5js.boot');
-
-
-goog.require('r5js.Environment');
-goog.require('r5js.InputPort');
-goog.require('r5js.OutputPort');
-goog.require('r5js.ParserImpl');
-goog.require('r5js.PipelineImpl');
-goog.require('r5js.PrimitiveProcedures');
-goog.require('r5js.ReaderImpl');
-goog.require('r5js.Scanner');
-goog.require('r5js.sync.EvaluatorImpl');
-goog.require('r5js.trampoline');
-
+const Datum = goog.require('r5js.Datum');
+const Environment = goog.require('r5js.Environment');
+const IEnvironment = goog.require('r5js.IEnvironment');
+const InputPort = goog.require('r5js.InputPort');
+const OutputPort = goog.require('r5js.OutputPort');
+const ParserImpl = goog.require('r5js.ParserImpl');
+const PipelineImpl = goog.require('r5js.PipelineImpl');
+const PrimitiveProcedures = goog.require('r5js.PrimitiveProcedures');
+const ProcCallLike = goog.require('r5js.ProcCallLike');
+const ReaderImpl = goog.require('r5js.ReaderImpl');
+const Scanner = goog.require('r5js.Scanner');
+const Evaluator = goog.require('r5js.sync.Evaluator');
+const EvaluatorImpl = goog.require('r5js.sync.EvaluatorImpl');
+const trampoline = goog.require('r5js.trampoline');
 
 /**
  * The main bootstrap function. Given Scheme source code for R5RS syntax and
@@ -35,18 +21,17 @@ goog.require('r5js.trampoline');
  * and is connected to the given ports.
  * @param {string} syntaxLib Scheme source code for the R5RS syntax library.
  * @param {string} procLib Scheme source code for the R5RS procedure library.
- * @param {!r5js.InputPort=} opt_inputPort Optional input port that the new
+ * @param {!InputPort=} opt_inputPort Optional input port that the new
  * evaluator will be connected to. If not given, defaults to
  * {@link r5js.InputPort.NULL}.
- * @param {!r5js.OutputPort=} opt_outputPort Optional output port that the new
+ * @param {!OutputPort=} opt_outputPort Optional output port that the new
  * evaluator will be connected to. If not given, defaults to
  * {@link r5js.OutputPort.NULL}.
- * @return {!r5js.sync.Evaluator}
+ * @return {!Evaluator}
  */
-r5js.boot = function(
-    syntaxLib, procLib, opt_inputPort, opt_outputPort) {
-  const nullEnv = new r5js.Environment(null /* enclosingEnv */);
-  r5js.boot.installSchemeSource_(syntaxLib, nullEnv);
+function boot(syntaxLib, procLib, opt_inputPort, opt_outputPort) {
+  const nullEnv = new Environment(null /* enclosingEnv */);
+  installSchemeSource(syntaxLib, nullEnv);
   nullEnv.seal();
 
   /* r5RSEnv is the normal "root" environment. But we also have to
@@ -72,25 +57,25 @@ r5js.boot = function(
      Ugh. */
 
   const r5RSEnv = nullEnv.clone();
-  r5js.PrimitiveProcedures.install(nullEnv, r5RSEnv);
-  r5js.boot.installSchemeSource_(procLib, r5RSEnv);
+  PrimitiveProcedures.install(nullEnv, r5RSEnv);
+  installSchemeSource(procLib, r5RSEnv);
   r5RSEnv.seal();
-  return new r5js.sync.EvaluatorImpl(
-      new r5js.PipelineImpl(r5RSEnv),
-      opt_inputPort || r5js.InputPort.NULL,
-      opt_outputPort || r5js.OutputPort.NULL);
-};
+  return new EvaluatorImpl(
+      new PipelineImpl(r5RSEnv),
+      opt_inputPort || InputPort.NULL,
+      opt_outputPort || OutputPort.NULL);
+}
 
 
 /**
  * @param {string} lib Scheme source code.
- * @param {!r5js.IEnvironment} env Environment to install the source code's
- * definitions into.
- * @private
+ * @param {!IEnvironment} env Environment to install the source code's definitions into.
  */
-r5js.boot.installSchemeSource_ = function(lib, env) {
-  const continuable = /** @type {!r5js.ProcCallLike} */ (new r5js.ParserImpl(
-      /** @type {!r5js.Datum} */ (new r5js.ReaderImpl(
-      new r5js.Scanner(lib)).read())).parse().desugar(env));
-  r5js.trampoline(continuable, env, r5js.InputPort.NULL, r5js.OutputPort.NULL);
-};
+function installSchemeSource(lib, env) {
+    const continuable = /** @type {!ProcCallLike} */ (new ParserImpl(
+        /** @type {!Datum} */ (new ReaderImpl(
+            new Scanner(lib)).read())).parse().desugar(env));
+    trampoline(continuable, env, InputPort.NULL, OutputPort.NULL);
+}
+
+exports = boot;
