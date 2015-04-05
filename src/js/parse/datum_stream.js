76aa7ee482eb842235287b8750e26a1ddf744509
Brendan Linn <brendan.linn@gmail.com>
2015-04-05 09:08:40 -0700

    [es6 module] parse: DatumStream.

--- src/js/parse/datum_stream.js
+++ src/js/parse/datum_stream.js
@@ -1,27 +1,13 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.DatumStream');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const Datum = goog.require('r5js.Datum');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.DatumStream');
-
-
-
-r5js.DatumStream = /** @interface */ class {
- /** @return {r5js.Datum} The next datum, or null if there is none. */
+/** @interface */
+class DatumStream {
+ /** @return {Datum} The next datum, or null if there is none. */
  getNextDatum() {}
 
- /** @param {!r5js.Datum} next */
+ /** @param {!Datum} next */
  advanceTo(next) {}
 
  /** Advances to child. */
@@ -32,4 +18,6 @@ r5js.DatumStream = /** @interface */ class {
 
  /** @return {boolean} True iff the advance worked. */
  maybeAdvanceToNextSiblingOfParent() {}
-};
+}
+
+exports = DatumStream;
