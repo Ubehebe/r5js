0f6b5b6eefcdfb3cab756c6f2eb303a90bc9b649
Brendan Linn <brendan.linn@gmail.com>
2015-04-05 08:07:15 -0700

    [es6 module] ast: Character.

--- src/js/ast/character.js
+++ src/js/ast/character.js
@@ -1,46 +1,27 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.ast.Character');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const SimpleDatum = goog.require('r5js.ast.SimpleDatum');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+/** @extends {SimpleDatum<string>} */
+class Character extends SimpleDatum {
 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+ /** @param {string} c */
+ constructor(c) {
+  super(c);
+ }
 
-goog.provide('r5js.ast.Character');
-
-
-goog.require('r5js.ast.SimpleDatum');
-
-
-
-/**
- * @param {string} c
- * @extends {r5js.ast.SimpleDatum<string>}
- * @struct
- * @constructor
- */
-r5js.ast.Character = function(c) {
-  r5js.ast.Character.base(this, 'constructor', c);
-};
-goog.inherits(r5js.ast.Character, r5js.ast.SimpleDatum);
-
-
-/**
- * Datums representing characters have payloads of type string.
- * If they unwrapped as JavaScript strings, it would be impossible
- * to re-wrap them correctly (noninjective mapping). We choose to store
- * identifiers unwrapped because they're expected to be more common than
- * the other two.
- *
- * @override
- */
-r5js.ast.Character.prototype.unwrap = function() {
+ /**
+  * Datums representing characters have payloads of type string.
+  * If they unwrapped as JavaScript strings, it would be impossible
+  * to re-wrap them correctly (noninjective mapping). We choose to store
+  * identifiers unwrapped because they're expected to be more common than
+  * the other two.
+  *
+  * @override
+  */
+ unwrap() {
   return this;
-};
+ }
+}
+
+exports = Character;
