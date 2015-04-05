1bd66228a6d9cc81d604f66282cec2226c5a6078
Brendan Linn <brendan.linn@gmail.com>
2015-04-05 09:07:00 -0700

    [es6 module] parse: RenameHelper.

--- src/js/parse/rename_helper.js
+++ src/js/parse/rename_helper.js
@@ -1,32 +1,7 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.RenameHelper');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-
-goog.provide('r5js.RenameHelper');
-
-
-
-/**
- * @struct
- * @constructor
- */
-r5js.RenameHelper = class {
-
-    /**
-     * @param {r5js.RenameHelper} parent The parent helper, if any.
-     */
+class RenameHelper {
+    /** @param {RenameHelper} parent The parent helper, if any. */
     constructor(parent) {
         /** @const @private {!Object<string, string>} */ this.bindings_ = {};
         /** @const @private */ this.parent_ = parent;
@@ -65,4 +40,6 @@ r5js.RenameHelper = class {
         }
         return false;
     }
-};
+}
+
+exports = RenameHelper;
