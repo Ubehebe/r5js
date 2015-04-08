d5e0062cf463a34d252d860f0eed9324c561ea54
Brendan Linn <brendan.linn@gmail.com>
2015-04-07 20:12:49 -0700

    [es6 module] ast: Lambda.

--- src/js/ast/lambda.js
+++ src/js/ast/lambda.js
@@ -1,28 +1,13 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.ast.Lambda');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const Procedure = goog.require('r5js.Procedure');
+const SimpleDatum = goog.require('r5js.ast.SimpleDatum');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.ast.Lambda');
-
-
-goog.require('r5js.ast.SimpleDatum');
-
-
-r5js.ast.Lambda = /** @extends {r5js.ast.SimpleDatum<!r5js.Procedure>} */ class extends r5js.ast.SimpleDatum {
+/** @extends {SimpleDatum<!Procedure>} */
+class Lambda extends SimpleDatum {
     /**
      * @param {string} name Name of the procedure.
-     * @param {!r5js.Procedure} procedure TODO bl.
+     * @param {!Procedure} procedure TODO bl.
      */
     constructor(name, procedure) {
         super(procedure);
@@ -33,4 +18,6 @@ r5js.ast.Lambda = /** @extends {r5js.ast.SimpleDatum<!r5js.Procedure>} */ class 
     getName() {
         return this.name_;
     }
-};
+}
+
+exports = Lambda;
