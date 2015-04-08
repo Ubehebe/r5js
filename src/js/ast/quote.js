5f68e1f240a94d415a06cbc610d696fe037064b3
Brendan Linn <brendan.linn@gmail.com>
2015-04-08 07:31:47 -0700

    [es6 module] ast: Quote.

--- src/js/ast/quote.js
+++ src/js/ast/quote.js
@@ -1,123 +1,119 @@
-goog.provide('r5js.ast.Quote');
+goog.module('r5js.ast.Quote');
 
-goog.require('r5js.IPair');
-goog.require('r5js.ast.CompoundDatum');
-goog.require('r5js.ast.Identifier');
-goog.require('r5js.ast.List');
-goog.require('r5js.parse.Terminals');
-goog.require('r5js.ProcCallLike');
+const CompoundDatum = goog.require('r5js.ast.CompoundDatum');
+const Datum = goog.require('r5js.Datum');
+const datumutil = goog.require('r5js.datumutil');
+const error = goog.require('r5js.error');
+const Identifier = goog.require('r5js.ast.Identifier');
+const IPair = goog.require('r5js.IPair');
+const List = goog.require('r5js.ast.List');
+const ProcCallLike = goog.require('r5js.ProcCallLike');
+const Ref = goog.require('r5js.Ref');
+const Terminals = goog.require('r5js.parse.Terminals');
+const UNSPECIFIED_VALUE = goog.require('r5js.runtime.UNSPECIFIED_VALUE');
+const Value = goog.require('r5js.runtime.Value');
 
-/**
- * @param {r5js.Datum} firstChild
- * @implements {r5js.IPair}
- * @extends {r5js.ast.CompoundDatum}
- * @struct
- * @constructor
- */
-r5js.ast.Quote = function(firstChild) {
-  r5js.ast.Quote.base(this, 'constructor');
-  if (firstChild) {
-    this.setFirstChild(firstChild.setImmutable());
-  }
-};
-goog.inherits(r5js.ast.Quote, r5js.ast.CompoundDatum);
-r5js.IPair.addImplementation(r5js.ast.Quote);
-
-
-/** @const @private {!r5js.runtime.Value} */
-r5js.ast.Quote.CAR_ = new r5js.ast.Identifier(r5js.parse.Terminals.QUOTE);
-
-
-/** @override */
-r5js.ast.Quote.prototype.car = function() {
-  return r5js.ast.Quote.CAR_;
-};
-
-
-/** @override */
-r5js.ast.Quote.prototype.cdr = function() {
-  return new r5js.ast.List(this.getFirstChild());
-};
-
-
-/** @override */
-r5js.ast.Quote.prototype.fixParserSensitiveIds = goog.nullFunction;
-
-/** @override */
-r5js.ast.Quote.prototype.toProcCallLike = function() {
-    return new r5js.QuoteShim_(this);
-};
-
-/**
- * TODO bl the purpose of this class is unclear.
- * @param {!r5js.ast.Quote} payload
- * @param {string=} opt_continuationName Optional name of the continuation.
- * @extends {r5js.ProcCallLike}
- * @struct
- * @constructor
- * @private
- */
-r5js.QuoteShim_ = function(payload, opt_continuationName) {
-    r5js.QuoteShim_.base(this, 'constructor', opt_continuationName);
-    /** @const @private */ this.firstOperand_ = payload;
-};
-goog.inherits(r5js.QuoteShim_, r5js.ProcCallLike);
-
-/** @override */
-r5js.QuoteShim_.prototype.evalAndAdvance = function(resultStruct, env, parserProvider) {
-    const ans = this.tryQuote_(this.firstOperand_);
-    if (ans !== null) {
-        this.bindResult(ans);
-        resultStruct.setValue(ans);
+/** @implements {r5js.IPair} */
+class Quote extends CompoundDatum {
+    /** @param {Datum} firstChild */
+    constructor(firstChild) {
+        super();
+        if (firstChild) {
+            this.setFirstChild(firstChild.setImmutable());
+        }
     }
 
-    const nextContinuable = this.getNext();
-
-    if (nextContinuable) {
-        resultStruct.setNext(nextContinuable);
+    /** @override */
+    car() {
+        return CAR_;
     }
-};
 
-/**
- * @param {!r5js.ast.Quote} quote
- * @return {?r5js.runtime.Value}
- * @private
- */
-r5js.QuoteShim_.prototype.tryQuote_ = function(quote) {
-    const env = this.getEnv();
-    // Do the appropriate substitutions.
-    const ans = quote.replaceChildren(
-        function(node) {
-            return node instanceof r5js.ast.Identifier && node.shouldUnquote();
-        },
-        function(node) {
-            const result = env.get(
-                /** @type {string} */ ((
-                /** @type {!r5js.ast.Identifier} */ (node)).
-                    getPayload()));
-            let ans = result === null ?
-                r5js.runtime.UNSPECIFIED_VALUE :
-                r5js.datumutil.wrapValue(result);
-            // TODO bl document why we're doing this
-            if (ans instanceof r5js.Ref) {
-                ans = ans.deref();
-            }
-            if (node instanceof r5js.ast.Identifier &&
-                node.shouldUnquoteSplice()) {
-                if (ans instanceof r5js.ast.List) {
-                    if (ans.getFirstChild()) { // `(1 ,@(list 2 3) 4) => (1 2 3 4)
-                        ans = ans.getFirstChild();
-                    } else { // `(1 ,@(list) 2) => (1 2)
-                        ans = null;
-                    }
-                } else throw r5js.error.quasiquote(ans + ' is not a list');
-            }
-            return /** @type {r5js.Datum} */ (ans);
-        });
-    // Now strip away the quote mark.
-    // the newIdOrLiteral part is for (quote quote)
-    return (ans instanceof r5js.ast.CompoundDatum &&
-    ans.getFirstChild()) ?
-        ans.getFirstChild() :
-        new r5js.ast.Identifier(r5js.parse.Terminals.QUOTE);
-};
+    /** @override */
+    cdr() {
+        return new List(this.getFirstChild());
+    }
+
+    /** @override */
+    fixParserSensitiveIds() {
+    }
+
+    /** @override */
+    toProcCallLike() {
+        return new QuoteShim(this);
+    }
+}
+
+IPair.addImplementation(Quote);
+
+/** @const @private {!Value} */
+const CAR_ = new Identifier(Terminals.QUOTE);
+
+
+/** TODO bl the purpose of this class is unclear. */
+class QuoteShim extends ProcCallLike {
+    /**
+     * @param {!Quote} payload
+     * @param {string=} opt_continuationName Optional name of the continuation.
+     */
+    constructor(payload, opt_continuationName) {
+        super(opt_continuationName);
+        /** @const @private */ this.firstOperand_ = payload;
+    }
+
+    /** @override */
+    evalAndAdvance(resultStruct, env, parserProvider) {
+        const ans = this.tryQuote_(this.firstOperand_);
+        if (ans !== null) {
+            this.bindResult(ans);
+            resultStruct.setValue(ans);
+        }
+
+        const nextContinuable = this.getNext();
+
+        if (nextContinuable) {
+            resultStruct.setNext(nextContinuable);
+        }
+    }
+
+    /**
+     * @param {!Quote} quote
+     * @return {?Value}
+     * @private
+     */
+    tryQuote_(quote) {
+        const env = this.getEnv();
+        // Do the appropriate substitutions.
+        const ans = quote.replaceChildren(
+            function (node) {
+                return node instanceof Identifier && node.shouldUnquote();
+            },
+            function (node) {
+                const result = env.get(/** @type {string} */ ((/** @type {!Identifier} */ (node))
+                    .getPayload()));
+                let ans = result === null
+                    ? UNSPECIFIED_VALUE
+                    : datumutil.wrapValue(result);
+                // TODO bl document why we're doing this
+                if (ans instanceof Ref) {
+                    ans = ans.deref();
+                }
+                if (node instanceof Identifier && node.shouldUnquoteSplice()) {
+                    if (ans instanceof List) {
+                        if (ans.getFirstChild()) { // `(1 ,@(list 2 3) 4) => (1 2 3 4)
+                            ans = ans.getFirstChild();
+                        } else { // `(1 ,@(list) 2) => (1 2)
+                            ans = null;
+                        }
+                    } else throw error.quasiquote(ans + ' is not a list');
+                }
+                return /** @type {Datum} */ (ans);
+            });
+        // Now strip away the quote mark.
+        // the newIdOrLiteral part is for (quote quote)
+        return (ans instanceof CompoundDatum && ans.getFirstChild())
+            ? ans.getFirstChild()
+            : new Identifier(Terminals.QUOTE);
+    }
+}
+
+exports = Quote;
