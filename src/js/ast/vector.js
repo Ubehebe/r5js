92cf6257407db6635669cdae31aa5b9006082edf
Brendan Linn <brendan.linn@gmail.com>
2015-04-08 07:05:26 -0700

    [es6 module] ast: Vector.

--- src/js/ast/vector.js
+++ src/js/ast/vector.js
@@ -1,31 +1,9 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.ast.Vector');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.ast.Vector');
-
-
-goog.require('r5js.Datum');
-goog.require('r5js.ast.CompoundDatum');
-
-
+const Datum = goog.require('r5js.Datum');
+const CompoundDatum = goog.require('r5js.ast.CompoundDatum');
 
 /**
- * @param {!r5js.Datum|!Array<!r5js.Datum>} firstChildOrArray
- * @extends {r5js.ast.CompoundDatum}
- * @struct
- * @constructor
  * TODO bl: this class illustrates the interpreter's confusion of abstract
  * syntax, intermediate representation, and runtime representation.
  * Having Scheme vectors be backed by JavaScript arrays is a reasonable
@@ -34,70 +12,69 @@ goog.require('r5js.ast.CompoundDatum');
  * on its argument; that changes the structure of the syntax tree,
  * which affects parsing. Only the runtime primitive procedures can do this.
  */
-r5js.ast.Vector = function(firstChildOrArray) {
-  r5js.ast.Vector.base(this, 'constructor');
+class Vector extends CompoundDatum {
+    /** @param {!Datum|!Array<!Datum>} firstChildOrArray */
+    constructor(firstChildOrArray) {
+        super();
 
-  /** @private */ this.arrayBacked_ = goog.isArray(firstChildOrArray);
+        /** @private */ this.arrayBacked_ = goog.isArray(firstChildOrArray);
 
-  /** @const @private {!Array<!r5js.Datum>} */ this.array_ =
-      this.arrayBacked_ ?
-      /** @type {!Array<!r5js.Datum>} */ (firstChildOrArray) :
-      [];
+        /** @const @private {!Array<!Datum>} */ this.array_ = this.arrayBacked_
+            ? /** @type {!Array<!Datum>} */ (firstChildOrArray)
+            : [];
 
-  if (firstChildOrArray instanceof r5js.Datum) {
-    this.setFirstChild(firstChildOrArray);
-  }
-};
-goog.inherits(r5js.ast.Vector, r5js.ast.CompoundDatum);
+        if (firstChildOrArray instanceof Datum) {
+            this.setFirstChild(firstChildOrArray);
+        }
+    }
 
+    /** @return {number} */
+    vectorLength() {
+        if (!this.arrayBacked_) {
+            this.convertVectorToArrayBacked_();
+        }
+        return this.array_.length;
+    }
 
-/** @return {number} */
-r5js.ast.Vector.prototype.vectorLength = function() {
-  if (!this.arrayBacked_) {
-    this.convertVectorToArrayBacked_();
-  }
-  return this.array_.length;
-};
+    /**
+     * @param {number} index
+     * @return {!Datum}
+     */
+    vectorRef(index) {
+        if (!this.arrayBacked_) {
+            this.convertVectorToArrayBacked_();
+        }
+        return this.array_[index];
+    }
 
+    /**
+     * @param {number} index
+     * @param {!Datum} val
+     */
+    vectorSet(index, val) {
+        if (!this.arrayBacked_) {
+            this.convertVectorToArrayBacked_();
+        }
+        this.array_[index] = val;
+    }
 
-/**
- * @param {number} index
- * @return {!r5js.Datum}
- */
-r5js.ast.Vector.prototype.vectorRef = function(index) {
-  if (!this.arrayBacked_) {
-    this.convertVectorToArrayBacked_();
-  }
-  return this.array_[index];
-};
+    /**
+     * Vector literals are constructed by the reader as linked lists
+     * with no random access, while vectors created programmatically
+     * via make-vector can just use JavaScript arrays. Instead of building
+     * logic into the reader to convert its inefficient vectors to array-backed
+     * ones, we check in every primitive vector procedure if the vector
+     * is array-backed, and mutate it in place if it isn't. There may
+     * be bugs involving the lost child/sibling pointers.
+     * @private
+     */
+    convertVectorToArrayBacked_() {
+        this.forEachChild(function (child) {
+            this.array_.push(child);
+        }, this);
+        this.clearFirstChild();
+        this.arrayBacked_ = true;
+    }
+}
 
-
-/**
- * @param {number} index
- * @param {!r5js.Datum} val
- */
-r5js.ast.Vector.prototype.vectorSet = function(index, val) {
-  if (!this.arrayBacked_) {
-    this.convertVectorToArrayBacked_();
-  }
-  this.array_[index] = val;
-};
-
-
-/**
- * Vector literals are constructed by the reader as linked lists
- * with no random access, while vectors created programmatically
- * via make-vector can just use JavaScript arrays. Instead of building
- * logic into the reader to convert its inefficient vectors to array-backed
- * ones, we check in every primitive vector procedure if the vector
- * is array-backed, and mutate it in place if it isn't. There may
- * be bugs involving the lost child/sibling pointers.
- * @private
- */
-r5js.ast.Vector.prototype.convertVectorToArrayBacked_ = function() {
-  this.forEachChild(function(child) {
-    this.array_.push(child);
-  }, this);
-  this.clearFirstChild();
-  this.arrayBacked_ = true;
-};
+exports = Vector;
