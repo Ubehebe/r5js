2e175d4752b483341d4e5e1e4f57cfa26ce4ce64
Brendan Linn <brendan.linn@gmail.com>
2015-04-08 06:32:05 -0700

    [es6 module] ast: Identifier.

--- src/js/ast/identifier.js
+++ src/js/ast/identifier.js
@@ -1,29 +1,13 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.ast.Identifier');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const Datum = goog.require('r5js.Datum');
+const SimpleDatum = goog.require('r5js.ast.SimpleDatum');
+const Terminals = goog.require('r5js.parse.Terminals');
+const ProcCallLike = goog.require('r5js.ProcCallLike');
+const Value = goog.require('r5js.runtime.Value');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.ast.Identifier');
-
-
-goog.require('r5js.Datum');
-goog.require('r5js.ast.SimpleDatum');
-goog.require('r5js.parse.Terminals');
-goog.require('r5js.ProcCallLike');
-
-
-
-r5js.ast.Identifier = /** @extends {r5js.ast.SimpleDatum<string>} */ class extends r5js.ast.SimpleDatum {
+/** @extends {SimpleDatum<string>} */
+class Identifier extends SimpleDatum {
     /** @param {string} name */
     constructor(name) {
         super(name);
@@ -34,7 +18,7 @@ r5js.ast.Identifier = /** @extends {r5js.ast.SimpleDatum<string>} */ class exten
      * unquoted (i.e. starts with a ,).
      */
     shouldUnquote() {
-        return this.payload.charAt(0) === r5js.parse.Terminals.COMMA;
+        return this.payload.charAt(0) === Terminals.COMMA;
     }
 
     /**
@@ -44,7 +28,7 @@ r5js.ast.Identifier = /** @extends {r5js.ast.SimpleDatum<string>} */ class exten
      * @suppress {accessControls} for r5js.Datum.CPS_PREFIX_
      */
     shouldUnquoteSplice() {
-        return this.payload.charAt(1) === r5js.Datum.CPS_PREFIX_;
+        return this.payload.charAt(1) === Datum.CPS_PREFIX_;
     }
 
     /** @override */
@@ -59,9 +43,9 @@ r5js.ast.Identifier = /** @extends {r5js.ast.SimpleDatum<string>} */ class exten
 
     /** @override */
     toProcCallLike() {
-        return new r5js.IdShim_(this);
+        return new IdShim(this);
     }
-};
+}
 
 /**
  * If a nonterminal in the grammar has no associated desugar function,
@@ -72,43 +56,40 @@ r5js.ast.Identifier = /** @extends {r5js.ast.SimpleDatum<string>} */ class exten
  *
  * We represent these id shims as ProcCalls whose operatorNames are null
  * and whose firstOperand is the payload.
- * @param {!r5js.ast.Identifier} payload
- * @param {string=} opt_continuationName Optional name of the continuation.
- * @extends {r5js.ProcCallLike}
- * @struct
- * @constructor
- * @private
  */
-r5js.IdShim_ = function(payload, opt_continuationName) {
-    r5js.IdShim_.base(this, 'constructor', opt_continuationName);
-    /** @const @private */ this.firstOperand_ = payload;
-};
-goog.inherits(r5js.IdShim_, r5js.ProcCallLike);
-
-
-/** @override */
-r5js.IdShim_.prototype.evalAndAdvance = function(
-    resultStruct, env, parserProvider) {
-    const ans = this.tryIdentifier_(this.firstOperand_);
-
-    if (ans !== null) {
-        this.bindResult(ans);
-        resultStruct.setValue(ans);
+class IdShim extends ProcCallLike {
+    /**
+     * @param {!Identifier} payload
+     * @param {string=} opt_continuationName Optional name of the continuation.
+     */
+    constructor(payload, opt_continuationName) {
+        super(opt_continuationName);
+        /** @const @private */ this.firstOperand_ = payload;
     }
 
-    const nextContinuable = this.getNext();
+    /** @override */
+    evalAndAdvance(resultStruct, env, parserProvider) {
+        const ans = this.tryIdentifier_(this.firstOperand_);
 
-    if (nextContinuable) {
-        resultStruct.setNext(nextContinuable);
+        if (ans !== null) {
+            this.bindResult(ans);
+            resultStruct.setValue(ans);
+        }
+
+        const nextContinuable = this.getNext();
+
+        if (nextContinuable) {
+            resultStruct.setNext(nextContinuable);
+        }
     }
-};
 
+    /**
+     * @param {!Identifier} id
+     * @return {?Value}
+     */
+    tryIdentifier_(id) {
+        return this.getEnv().get(/** @type {string} */ (id.getPayload()));
+    }
+}
 
-/**
- * @param {!r5js.ast.Identifier} id
- * @return {?r5js.runtime.Value}
- * @private
- */
-r5js.IdShim_.prototype.tryIdentifier_ = function(id) {
-    return this.getEnv().get(/** @type {string} */ (id.getPayload()));
-};
+exports = Identifier;
