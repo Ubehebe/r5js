3b427b4a5b0093079d7f865f87b2bfdeb77d1da2
Brendan Linn <brendan.linn@gmail.com>
2015-04-08 06:26:43 -0700

    [es6 module] ast: DottedList.

--- src/js/ast/dotted_list.js
+++ src/js/ast/dotted_list.js
@@ -1,72 +1,55 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.ast.DottedList');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const CdrHelper = goog.require('r5js.CdrHelper');
+const CompoundDatum = goog.require('r5js.ast.CompoundDatum');
+const Datum = goog.require('r5js.Datum');
+const IPair = goog.require('r5js.IPair');
+const List = goog.require('r5js.ast.List');
+const SiblingBuffer = goog.require('r5js.SiblingBuffer');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.ast.DottedList');
-
-
-goog.require('r5js.CdrHelper');
-goog.require('r5js.IPair');
-goog.require('r5js.SiblingBuffer');
-goog.require('r5js.ast.CompoundDatum');
-
-
-
-/**
- * @param {r5js.Datum} firstChild
- * @implements {r5js.IPair}
- * @extends {r5js.ast.CompoundDatum}
- * @struct
- * @constructor
- */
-r5js.ast.DottedList = function(firstChild) {
-  r5js.ast.DottedList.base(this, 'constructor');
-  if (firstChild) {
-    this.setFirstChild(firstChild);
-  }
-};
-goog.inherits(r5js.ast.DottedList, r5js.ast.CompoundDatum);
-r5js.IPair.addImplementation(r5js.ast.DottedList);
-
-
-/** @override */
-r5js.ast.DottedList.prototype.isImproperList = function() { return true; };
-
-
-/** @override */
-r5js.ast.DottedList.prototype.car = function() {
-  return /** @type {!r5js.Datum} */ (this.getFirstChild());
-};
-
-
-/** @override */
-r5js.ast.DottedList.prototype.cdr = function() {
-  const startOfCdr = this.getFirstChild().getNextSibling();
-  let ans;
-  if (startOfCdr) {
-    if (startOfCdr.getNextSibling()) {
-      ans = new r5js.SiblingBuffer().
-                appendSibling(startOfCdr).
-                toList(r5js.ast.DottedList);
-    } else {
-      ans = startOfCdr;
+/** @implements {IPair} */
+class DottedList extends CompoundDatum {
+    /** @param {Datum} firstChild */
+    constructor(firstChild) {
+        super();
+        if (firstChild) {
+            this.setFirstChild(firstChild);
+        }
     }
-    if (ans instanceof r5js.ast.CompoundDatum) {
-      ans.setCdrHelper(new r5js.CdrHelper(this, startOfCdr));
+
+    /** @override */
+    isImproperList() {
+        return true;
     }
-    return ans;
-  } else {
-    return new r5js.SiblingBuffer().toList(r5js.ast.List);
-  }
-};
+
+    /** @override */
+    car() {
+        return /** @type {!Datum} */ (this.getFirstChild());
+    }
+
+    /** @override */
+    cdr() {
+        const startOfCdr = this.getFirstChild().getNextSibling();
+        let ans;
+        if (startOfCdr) {
+            if (startOfCdr.getNextSibling()) {
+                ans = new SiblingBuffer()
+                    .appendSibling(startOfCdr)
+                    .toList(DottedList);
+            } else {
+                ans = startOfCdr;
+            }
+            if (ans instanceof CompoundDatum) {
+                ans.setCdrHelper(new CdrHelper(this, startOfCdr));
+            }
+            return ans;
+        } else {
+            return new SiblingBuffer().toList(List);
+        }
+    }
+}
+
+IPair.addImplementation(DottedList);
+
+exports = DottedList;
+
