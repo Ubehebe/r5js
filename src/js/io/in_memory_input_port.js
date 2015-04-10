c29b797d0e6053298b97b986734283198d9dda5d
Brendan Linn <brendan.linn@gmail.com>
2015-04-10 06:25:52 -0700

    [es6 module] io: InMemoryInputPort.

---
+++ src/js/io/in_memory_input_port.js
@@ -0,0 +1,71 @@
+goog.module('r5js.InMemoryInputPort');
+
+const Character = goog.require('r5js.ast.Character');
+const Datum = goog.require('r5js.Datum');
+const InMemoryPortBuffer = goog.require('r5js.InMemoryPortBuffer');
+const InputPort = goog.require('r5js.InputPort');
+const ReaderImpl = goog.require('r5js.ReaderImpl');
+const Scanner = goog.require('r5js.Scanner');
+
+/** @implements {InputPort} */
+class InMemoryInputPort {
+    /** @param {!InMemoryPortBuffer} buffer */
+    constructor(buffer) {
+        /** @private */ this.closed_ = false;
+        /** @const @private */ this.buffer_ = buffer;
+        /** @private {Datum} */ this.leftoverDatum_ = null;
+    }
+
+
+    /** @override */
+    isCharReady() {
+        return !this.buffer_.isEmpty();
+    }
+
+    /** @override */
+    close() {
+        this.closed_ = true;
+    }
+
+    /** @override */
+    read() {
+        const maybeDatum = this.readLeftoverDatum_();
+        if (maybeDatum) {
+            return maybeDatum;
+        } else if (this.buffer_.isEmpty()) {
+            return null;
+        } else {
+            const text = this.buffer_.getAndClear();
+            this.leftoverDatum_ = new ReaderImpl(new Scanner(text)).read();
+            return this.read();
+        }
+    }
+
+    /**
+     * @return {Datum}
+     * @private
+     */
+    readLeftoverDatum_() {
+        const retval = this.leftoverDatum_;
+        if (retval) {
+            this.leftoverDatum_ = this.leftoverDatum_.getNextSibling();
+        }
+        return retval;
+    }
+
+    /** @override */
+    peekChar() {
+        const c = this.buffer_.peekChar();
+        return c ? new Character(c) : null;
+    }
+
+    /** @override */
+    readChar() {
+        const c = this.buffer_.getChar();
+        return c ? new Character(c) : null;
+    }
+}
+
+InputPort.addImplementation(InMemoryInputPort);
+
+exports = InMemoryInputPort;
--- src/js/io/in_memory_port.js
+++ src/js/io/in_memory_port.js
@@ -1,4 +1,3 @@
-goog.provide('r5js.InMemoryInputPort');
 goog.provide('r5js.InMemoryOutputPort');
 
 goog.require('r5js.InMemoryPortBuffer');
@@ -9,76 +8,6 @@ goog.require('r5js.ReaderImpl');
 goog.require('r5js.Scanner');
 goog.require('r5js.ast.Character');
 goog.require('r5js.valutil');
-
-/**
- * @param {!r5js.InMemoryPortBuffer} buffer
- * @implements {r5js.InputPort}
- * @struct
- * @constructor
- */
-r5js.InMemoryInputPort = function(buffer) {
-  /** @private */ this.closed_ = false;
-  /** @const @private */ this.buffer_ = buffer;
-  /** @private {r5js.Datum} */ this.leftoverDatum_ = null;
-};
-r5js.InputPort.addImplementation(r5js.InMemoryInputPort);
-
-
-/** @override */
-r5js.InMemoryInputPort.prototype.isCharReady = function() {
-  return !this.buffer_.isEmpty();
-};
-
-
-/** @override */
-r5js.InMemoryInputPort.prototype.close = function() {
-  this.closed_ = true;
-};
-
-
-/** @override */
-r5js.InMemoryInputPort.prototype.read = function() {
-  const maybeDatum = this.readLeftoverDatum_();
-  if (maybeDatum) {
-    return maybeDatum;
-  } else if (this.buffer_.isEmpty()) {
-    return null;
-  } else {
-    const text = this.buffer_.getAndClear();
-    this.leftoverDatum_ = new r5js.ReaderImpl(
-        new r5js.Scanner(text)).read();
-    return this.read();
-  }
-};
-
-
-/**
- * @return {r5js.Datum}
- * @private
- */
-r5js.InMemoryInputPort.prototype.readLeftoverDatum_ = function() {
-  const retval = this.leftoverDatum_;
-  if (retval) {
-    this.leftoverDatum_ = this.leftoverDatum_.getNextSibling();
-  }
-  return retval;
-};
-
-
-/** @override */
-r5js.InMemoryInputPort.prototype.peekChar = function() {
-  const c = this.buffer_.peekChar();
-  return c ? new r5js.ast.Character(c) : null;
-};
-
-
-/** @override */
-r5js.InMemoryInputPort.prototype.readChar = function() {
-  const c = this.buffer_.getChar();
-  return c ? new r5js.ast.Character(c) : null;
-};
-
-
 
 /**
  * @param {!r5js.InMemoryPortBuffer} buffer
