6158e1784428f57d60b89ce682719243b86776a4
Brendan Linn <brendan.linn@gmail.com>
2015-04-11 06:47:56 -0700

    [es6 module] platform/html5: OutputPort.

--- src/js/platform/html5/output_port.js
+++ src/js/platform/html5/output_port.js
@@ -1,20 +1,23 @@
-goog.provide('r5js.platform.html5.OutputPort');
+goog.module('r5js.platform.html5.OutputPort');
 
-goog.require('r5js.OutputPort');
+const OutputPort = goog.require('r5js.OutputPort');
 
-r5js.platform.html5.OutputPort = /** @implements {r5js.OutputPort} */ class {
- /** @param {function(string)} postMessage */
- constructor(postMessage) {
-  /** @const @private */ this.postMessage_ = postMessage;
- }
+/** @implements {OutputPort} */
+class Html5OutputPort {
+    /** @param {function(string)} postMessage */
+    constructor(postMessage) {
+        /** @const @private */ this.postMessage_ = postMessage;
+    }
 
- /** @override */
- write(value) {
-  this.postMessage_(value);
- }
+    /** @override */
+    write(value) {
+        this.postMessage_(value);
+    }
 
- /** @override */
- close() {}
-};
+    /** @override */
+    close() {}
+}
 
-r5js.OutputPort.addImplementation(r5js.platform.html5.OutputPort);
+exports = Html5OutputPort;
+
+OutputPort.addImplementation(Html5OutputPort);
