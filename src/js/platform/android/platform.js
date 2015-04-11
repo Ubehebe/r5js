d109793dbf33df0a256cd03ba7621a90a4ed7ce7
Brendan Linn <brendan.linn@gmail.com>
2015-04-11 06:30:22 -0700

    [es6 module] platform/android: Platform.

--- src/js/platform/android/platform.js
+++ src/js/platform/android/platform.js
@@ -1,9 +1,10 @@
-goog.provide('r5js.curPlatform');
+goog.module('r5js.curPlatform');
 
-goog.require('r5js.Platform');
-goog.require('r5js.platform.common.newEvaluator');
+const Platform = goog.require('r5js.Platform');
+const newEvaluator = goog.require('r5js.platform.common.newEvaluator');
 
-r5js.platform.Android_ = /** @private @implements {r5js.Platform} */ class {
+/** @implements {Platform} */
+class Android {
     /** @override */
     exit(statusCode) {
         AndroidSchemePlatform.exit(statusCode);
@@ -11,11 +12,13 @@ r5js.platform.Android_ = /** @private @implements {r5js.Platform} */ class {
 
     /** @override */
     newEvaluator(opt_inputPort, opt_outputPort) {
-        return r5js.platform.common.newEvaluator(opt_inputPort, opt_outputPort);
+        return newEvaluator(opt_inputPort, opt_outputPort);
     }
-};
+}
 
-/** @return {!r5js.Platform} */
-r5js.curPlatform = function() {
-  return new r5js.platform.Android_();
-};
+/** @return {!Platform} */
+function curPlatform() {
+  return new Android();
+}
+
+exports = curPlatform;
