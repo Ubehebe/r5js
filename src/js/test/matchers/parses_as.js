64131f825ee137c7d68498a5fc831ffc824b4f6f
Brendan Linn <brendan.linn@gmail.com>
2015-04-10 17:13:15 -0700

    [es6 module] test/matchers: parseAs.

--- src/js/test/matchers/parses_as.js
+++ src/js/test/matchers/parses_as.js
@@ -1,81 +1,58 @@
-/* Copyright 2011-2014 Brendan Linn
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('parseAs');
+goog.module('parseAs');
 goog.setTestOnly('parseAs');
 
+const Datum = goog.require('r5js.Datum');
+const Nonterminal = goog.require('r5js.parse.Nonterminal');
+const ParserImpl = goog.require('r5js.ParserImpl');
+const ReaderImpl = goog.require('r5js.ReaderImpl');
+const Scanner = goog.require('r5js.Scanner');
+const Matcher = goog.require('tdd.matchers.Matcher');
 
-goog.require('r5js.Datum');
-goog.require('r5js.ParserImpl');
-goog.require('r5js.ReaderImpl');
-goog.require('r5js.Scanner');
-goog.require('r5js.parse.bnf');
-goog.require('tdd.matchers.Matcher');
+/** @implements {Matcher<string>} */
+class ParsesAs {
+    /** @param {!Nonterminal} expectedType */
+    constructor(expectedType) {
+        /** @const @private {!Nonterminal} */
+        this.expectedType_ = expectedType;
 
+        /** @private {!Nonterminal|null} */
+        this.actualType_ = null;
+    }
+
+    /** @override */
+    matches(value) {
+        let datumRoot;
+        try {
+            datumRoot = new ReaderImpl(new Scanner(value)).read();
+        } catch (e) {
+            return false;
+        }
+        const actualResult = (datumRoot instanceof Datum)
+            && new ParserImpl.ParserImpl(datumRoot).parse(this.expectedType_);
+        if (actualResult && actualResult.peekParse) {
+            this.actualType_ = /** @type {!Nonterminal} */ (actualResult.peekParse());
+        }
+        return this.actualType_ === this.expectedType_;
+    }
+
+    /** @override */
+    getFailureMessage(value) {
+        return 'expected ' +
+            value +
+            ' to parse as ' +
+            this.expectedType_ +
+            ', got ' +
+            this.actualType_;
+    }
+}
 
 /**
- * @param {!r5js.parse.Nonterminal} expectedType
- * @return {!tdd.matchers.Matcher}
+ * @param {!Nonterminal} expectedType
+ * @return {!Matcher}
  */
-const parseAs = function(expectedType) {
-  return new ParsesAs_(expectedType);
-};
+function parseAs(expectedType) {
+    return new ParsesAs(expectedType);
+}
 
+exports = parseAs;
 
-
-/**
- * @param {!r5js.parse.Nonterminal} expectedType
- * @implements {tdd.matchers.Matcher<string>}
- * @struct
- * @constructor
- * @private
- */
-const ParsesAs_ = function(expectedType) {
-  /** @const @private {!r5js.parse.Nonterminal} */
-  this.expectedType_ = expectedType;
-
-  /** @private {!r5js.parse.Nonterminal|null} */
-  this.actualType_ = null;
-};
-
-
-/** @override */
-ParsesAs_.prototype.matches = function(value) {
-  let datumRoot;
-  try {
-    datumRoot = new r5js.ReaderImpl(
-        new r5js.Scanner(value)).read();
-  } catch (e) {
-    return false;
-  }
-  const actualResult = (datumRoot instanceof r5js.Datum) &&
-      new r5js.ParserImpl.ParserImpl(datumRoot).parse(this.expectedType_);
-  if (actualResult && actualResult.peekParse) {
-    this.actualType_ = /** @type {!r5js.parse.Nonterminal} */ (
-        actualResult.peekParse());
-  }
-  return this.actualType_ === this.expectedType_;
-};
-
-
-/** @override */
-ParsesAs_.prototype.getFailureMessage = function(value) {
-  return 'expected ' +
-      value +
-      ' to parse as ' +
-      this.expectedType_ +
-      ', got ' +
-      this.actualType_;
-};
