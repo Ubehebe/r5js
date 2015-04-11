a3facaaa6d4554eb54be43b262d9ef041e3b253a
Brendan Linn <brendan.linn@gmail.com>
2015-04-10 17:22:16 -0700

    [es6 module] test/matchers: scanAs.

--- src/js/test/matchers/scans_as.js
+++ src/js/test/matchers/scans_as.js
@@ -1,67 +1,46 @@
-/* Copyright 2011-2014 Brendan Linn
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('scanAs');
+goog.module('scanAs');
 goog.setTestOnly('scanAs');
 
+const Datum = goog.require('r5js.Datum');
+const Scanner = goog.require('r5js.Scanner');
+const Matcher = goog.require('tdd.matchers.Matcher');
 
-goog.require('r5js.Scanner');
-goog.require('tdd.matchers.Matcher');
-
-
-/**
- * @param {function(new: r5js.Datum, ?)} expectedType
- * @return {!tdd.matchers.Matcher<string>}
- */
-const scanAs = function(expectedType) {
-  return new ScansAs_(expectedType);
-};
-
-
-
-/**
- * @param {function(new: r5js.Datum, ?)} expectedType
- * @implements {tdd.matchers.Matcher<string>}
- * @struct
- * @constructor
- * @private
- */
-const ScansAs_ = function(expectedType) {
-  /** @const @private */ this.expectedType_ = expectedType;
-};
-
-
-/** @override */
-ScansAs_.prototype.matches = function(value) {
-  try {
-    const scanner = new r5js.Scanner(value);
-    const token = scanner.nextToken();
-    // There should be exactly one token in the input.
-    // (For example, 1+2 should fail to scan as one number token,
-    // even though the whole input scans.)
-    if (!token || scanner.nextToken()) {
-      return false;
+/** @implements {Matcher<string>} */
+class ScansAs {
+    /** @param {function(new: Datum, ?)} expectedType */
+    constructor(expectedType) {
+        /** @const @private */ this.expectedType_ = expectedType;
     }
-    return token instanceof this.expectedType_;
-  } catch (e) {
-    return false; // some tests purposely cause scan errors
-  }
-};
 
+    /** @override */
+    matches(value) {
+        try {
+            const scanner = new Scanner(value);
+            const token = scanner.nextToken();
+            // There should be exactly one token in the input.
+            // (For example, 1+2 should fail to scan as one number token,
+            // even though the whole input scans.)
+            if (!token || scanner.nextToken()) {
+                return false;
+            }
+            return token instanceof this.expectedType_;
+        } catch (e) {
+            return false; // some tests purposely cause scan errors
+        }
+    }
 
-/** @override */
-ScansAs_.prototype.getFailureMessage = function(value) {
-  return 'expected ' + value + ' to scan as ' + this.expectedType_;
-};
+    /** @override */
+    getFailureMessage(value) {
+        return 'expected ' + value + ' to scan as ' + this.expectedType_;
+    }
+}
+
+/**
+ * @param {function(new: r5js.Datum, ?)} expectedType
+ * @return {!Matcher<string>}
+ */
+function scanAs(expectedType) {
+    return new ScansAs(expectedType);
+}
+
+exports = scanAs;
