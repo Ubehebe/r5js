440dd6b03deac7bcd9467dcc447eaba9534c2f0d
Brendan Linn <brendan.linn@gmail.com>
2015-04-11 06:41:41 -0700

    [es6 module] platform/html5: Client.

--- src/js/platform/html5/client.js
+++ src/js/platform/html5/client.js
@@ -1,83 +1,58 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.platform.html5.Client');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+const Evaluator = goog.require('r5js.Evaluator');
+const EventType = goog.require('goog.events.EventType');
+const message = goog.require('r5js.platform.html5.message');
+const Message = goog.require('r5js.platform.html5.Message');
+const MessageType = goog.require('r5js.platform.html5.MessageType');
+const OutputPort = goog.require('r5js.OutputPort');
+const Promise = goog.require('goog.Promise');
+const Resolver = goog.require('goog.promise.Resolver');
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+/** @implements {Evaluator} */
+class Client {
+    /** @param {!OutputPort} outputPort */
+    constructor(outputPort) {
+        /** @const @private */ this.worker_ = new Worker(r5js.platform.html5.Client.WORKER_SCRIPT);
+        /** @const @private */ this.outputPort_ = outputPort;
+        /** @const @private {!Array<!Resolver<?>>} */ this.resolvers_ = [];
+        /** @private */ this.messageIdCounter_ = 0;
 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+        this.worker_.addEventListener(EventType.MESSAGE, this.onMessage_.bind(this), false);
+    }
 
+    /** @override */
+    evaluate(input) {
+        const resolver = Promise.withResolver();
+        const messageId = ++this.messageIdCounter_;
+        this.resolvers_[messageId] = resolver;
+        this.worker_.postMessage(message.newEvalRequest(messageId, input));
+        return resolver.promise;
+    }
 
-goog.provide('r5js.platform.html5.Client');
+    /**
+     * @param {!Event} e
+     * @private
+     */
+    onMessage_(e) {
+        e = /** @type {!MessageEvent} */ (e);
+        const message = /** @type {!Message} */ (e.data);
+        switch (message.type) {
+            case MessageType.EVAL_RESPONSE:
+                this.resolvers_[message.id].resolve(message.content);
+                break;
+            case MessageType.EVAL_ERROR:
+                this.resolvers_[message.id].reject(message.content);
+                break;
+            case MessageType.OUTPUT:
+                this.outputPort_.write(/** @type {string} */ (message.content));
+                return;
+        }
+        delete this.resolvers_[message.id];
+    }
+}
 
-
-goog.require('goog.Promise');
-goog.require('goog.events.EventType');
-goog.require('r5js.Evaluator');
-goog.require('r5js.platform.html5.MessageType');
-
-
-
-/**
- * @param {!r5js.OutputPort} outputPort
- * @implements {r5js.Evaluator}
- * @struct
- * @constructor
- */
-r5js.platform.html5.Client = function(outputPort) {
-  /** @const @private */ this.worker_ =
-      new Worker(r5js.platform.html5.Client.WORKER_SCRIPT);
-
-  /** @const @private */ this.outputPort_ = outputPort;
-
-  this.worker_.addEventListener(
-      goog.events.EventType.MESSAGE, this.onMessage_.bind(this), false);
-
-  /** @private */ this.messageIdCounter_ = 0;
-
-  /** @const @private {!Array<!goog.promise.Resolver<?>>} */
-  this.resolvers_ = [];
-};
-
+exports = Client;
 
 /** @define {string} Location of the worker script. */
 goog.define('r5js.platform.html5.Client.WORKER_SCRIPT', '');
-
-
-/** @override */
-r5js.platform.html5.Client.prototype.evaluate = function(input) {
-  const resolver = goog.Promise.withResolver();
-  const messageId = ++this.messageIdCounter_;
-  this.resolvers_[messageId] = resolver;
-  this.worker_.postMessage(
-      r5js.platform.html5.message.newEvalRequest(messageId, input));
-  return resolver.promise;
-};
-
-
-/**
- * @param {!Event} e
- * @private
- */
-r5js.platform.html5.Client.prototype.onMessage_ = function(e) {
-  e = /** @type {!MessageEvent} */ (e);
-  const message = /** @type {!r5js.platform.html5.Message} */ (e.data);
-  switch (message.type) {
-    case r5js.platform.html5.MessageType.EVAL_RESPONSE:
-      this.resolvers_[message.id].resolve(message.content);
-      break;
-    case r5js.platform.html5.MessageType.EVAL_ERROR:
-      this.resolvers_[message.id].reject(message.content);
-      break;
-    case r5js.platform.html5.MessageType.OUTPUT:
-      this.outputPort_.write(/** @type {string} */ (message.content));
-      return;
-  }
-  delete this.resolvers_[message.id];
-};
