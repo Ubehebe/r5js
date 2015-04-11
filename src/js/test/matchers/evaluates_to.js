a3f30dc518bdfbc3640a8ae0efbe76b67eb4cdf2
Brendan Linn <brendan.linn@gmail.com>
2015-04-10 17:17:46 -0700

    [es6 module] test/matchers: evalTo.

--- src/js/test/matchers/evaluates_to.js
+++ src/js/test/matchers/evaluates_to.js
@@ -1,58 +1,36 @@
-/* Copyright 2011-2014 Brendan Linn
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-
-goog.provide('evalTo');
+goog.module('evalTo');
 goog.setTestOnly('evalTo');
 
+const Matcher = goog.require('tdd.matchers.Matcher');
 
-goog.require('tdd.matchers.Matcher');
+/** @implements {Matcher<string>} */
+class EvaluatesTo {
+    /** @param {string} expectedValue */
+    constructor(expectedValue) {
+        /** @const @private */ this.expectedValue_ = expectedValue;
+    }
 
+    /** @override */
+    matches(actualValue) {
+        return this.expectedValue_ === actualValue;
+    }
+
+    /** @override */
+    getFailureMessage(actualValue) {
+        return 'want ' +
+            this.expectedValue_ +
+            ' got ' +
+            actualValue;
+    }
+}
 
 /**
  * @param {string} value
- * @return {!tdd.matchers.Matcher<string>}
+ * @return {!Matcher<string>}
  */
-const evalTo = function(value) {
-  return new EvaluatesTo_(value);
-};
+function evalTo(value) {
+    return new EvaluatesTo(value);
+}
 
+exports = evalTo;
 
-
-/**
- * @param {string} expectedValue
- * @implements {tdd.matchers.Matcher<string>}
- * @struct
- * @constructor
- * @private
- */
-const EvaluatesTo_ = function(expectedValue) {
-  /** @const @private */ this.expectedValue_ = expectedValue;
-};
-
-
-/** @override */
-EvaluatesTo_.prototype.matches = function(actualValue) {
-  return this.expectedValue_ === actualValue;
-};
-
-
-/** @override */
-EvaluatesTo_.prototype.getFailureMessage = function(actualValue) {
-  return 'want ' +
-      this.expectedValue_ +
-      ' got ' +
-      actualValue;
-};
