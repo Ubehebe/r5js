f50c7c2d438180993f0c708852a467cfc6090dbc
Brendan Linn <brendan.linn@gmail.com>
2015-04-11 07:54:18 -0700

    [es6 module] repl: Terminal.

--- src/js/repl/terminal.js
+++ src/js/repl/terminal.js
@@ -1,28 +1,14 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.Terminal');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.Terminal');
-
-
+const Promise = goog.require('goog.Promise');
 
 /**
  * "Dumb" terminal that {@link r5js.Repl} can read to and write from.
  * In contrast to a Repl, a Terminal knows nothing about Scheme.
+ * @interface
  */
-r5js.Terminal = /** @interface */ class {
- /** @return {!goog.Promise<string>} The next line of input. */
+class Terminal {
+ /** @return {!Promise<string>} The next line of input. */
  getNextLineOfInput() {}
 
  /** @param {string} str String to print. */
@@ -30,4 +16,6 @@ r5js.Terminal = /** @interface */ class {
 
  /** @param {string} str Error message to print. */
  error(str) {}
-};
+}
+
+exports = Terminal;
