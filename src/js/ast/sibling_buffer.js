f0844a3b255b59f9225fcae2b1ceba8fbd24df0c
Brendan Linn <brendan.linn@gmail.com>
2015-04-16 07:28:59 -0700

    [es6 module] ast: SiblingBuffer.

--- src/js/ast/sibling_buffer.js
+++ src/js/ast/sibling_buffer.js
@@ -1,36 +1,15 @@
-/* Copyright 2011-2014 Brendan Linn
+goog.module('r5js.SiblingBuffer');
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-
-goog.provide('r5js.SiblingBuffer');
-
-
+const Datum = goog.require('r5js.Datum');
 
 /**
  * Just a buffer to accumulate siblings without the client having to do
  * the pointer arithmetic.
- * @struct
- * @constructor
  */
-r5js.SiblingBuffer = class {
-    constructor() {
-        /** @private {r5js.Datum} */
-        this.first_ = null;
-
-        /** @private {r5js.Datum} */
-        this.last_ = null;
+class SiblingBuffer {
+   constructor() {
+        /** @private {Datum} */ this.first_ = null;
+        /** @private {Datum} */ this.last_ = null;
     }
 
     /** @return {boolean} True iff the buffer is empty. */
@@ -39,8 +18,8 @@ r5js.SiblingBuffer = class {
     }
 
     /**
-     * @param {!r5js.Datum} node Node to append.
-     * @return {!r5js.SiblingBuffer} This object, for chaining.
+     * @param {!Datum} node Node to append.
+     * @return {!SiblingBuffer} This object, for chaining.
      */
     appendSibling(node) {
         if (!this.first_) {
@@ -53,21 +32,23 @@ r5js.SiblingBuffer = class {
         return this;
     }
 
-    /** @return {r5js.Datum} */
+    /** @return {Datum} */
     toSiblings() {
         return this.first_;
     }
 
     /**
-     * @param {function(new: r5js.ast.CompoundDatum, !r5js.Datum)} ctor
-     * Constructor to use for the returned list.
-     * @return {!r5js.ast.CompoundDatum}
+     * @param {function(new: T, !Datum)} ctor Constructor to use for the returned list.
+     * @return {T}
+     * @template T
      */
     toList(ctor) {
-        const ans = new ctor(/** @type {!r5js.Datum} */ (this.first_));
+        const ans = new ctor(/** @type {!Datum} */ (this.first_));
         if (this.last_ && ans) {
             this.last_.setParent(ans);
         }
         return ans;
     }
-};
+}
+
+exports = SiblingBuffer;
