4d75ec4b6c3389128236bdf5e3764dd95cacc7d4
Brendan Linn <brendan.linn@gmail.com>
2015-04-17 07:35:14 -0700

    [es6 module] ast: Macro.

--- src/js/ast/macro.js
+++ src/js/ast/macro.js
@@ -1,21 +1,18 @@
-goog.provide('r5js.ast.Macro');
+goog.module('r5js.ast.Macro');
 
+const SimpleDatum = goog.require('r5js.ast.SimpleDatum');
 
-goog.require('r5js.ast.SimpleDatum');
+class Macro extends SimpleDatum {
+    /** @param {!r5js.Macro} macro */
+    constructor(macro) {
+        super(macro);
+    }
 
+    /** @return {!r5js.Macro} */
+    getMacro() {
+        return this.payload.setIsLetOrLetrecSyntax();
+    }
+}
 
-
-r5js.ast.Macro = class extends r5js.ast.SimpleDatum {
- /**
-  * @param {!r5js.Macro} macro
-  */
- constructor(macro) {
-  super(macro);
- }
-
- /** @return {!r5js.Macro} */
- getMacro() {
-  return this.payload.setIsLetOrLetrecSyntax();
- }
-};
+exports = Macro;
 
