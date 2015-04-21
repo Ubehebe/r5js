8e564f8c961ce3f294d80d8869e93d4b00d87058
Brendan Linn <brendan.linn@gmail.com>
2015-04-21 07:45:45 -0700

    [es6 module] test: SchemeTestDriver.

--- src/js/test/scheme_test_driver.js
+++ src/js/test/scheme_test_driver.js
@@ -1,150 +1,125 @@
-/* Copyright 2011-2014 Brendan Linn
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-goog.provide('r5js.test.SchemeTestDriver');
+goog.module('r5js.test.SchemeTestDriver');
 goog.setTestOnly('r5js.test.SchemeTestDriver');
 
-
-goog.require('expect');
-goog.require('goog.Promise');
-goog.require('r5js.CallbackBackedPort');
-goog.require('r5js.Platform');
-goog.require('r5js.test.SchemeSources');
-goog.require('r5js.valutil');
-goog.require('tdd.LogLevel');
-goog.require('tdd.LogRecord');
-goog.require('tdd.ManualTestSuite');
-goog.require('tdd.ResultStruct');
-
-
+const CallbackBackedPort = goog.require('r5js.CallbackBackedPort');
+const curPlatform = goog.require('r5js.curPlatform');
+const Evaluator = goog.require('r5js.Evaluator');
+const expect = goog.require('expect');
+const InputPort = goog.require('r5js.InputPort');
+const Logger = goog.require('goog.log.Logger');
+const LogLevel = goog.require('tdd.LogLevel');
+const LogRecord = goog.require('tdd.LogRecord');
+const ManualTestSuite = goog.require('tdd.ManualTestSuite');
+const Platform = goog.require('r5js.Platform');
+const Promise = goog.require('goog.Promise');
+const ResultStruct = goog.require('tdd.ResultStruct');
+const SchemeSources = goog.require('r5js.test.SchemeSources');
+const TestType = goog.require('tdd.TestType');
 
 /**
  * Driver for running the unit tests written in Scheme.
- * @implements {tdd.ManualTestSuite}
- * @struct
- * @constructor
+ * @implements {ManualTestSuite}
  */
-r5js.test.SchemeTestDriver = function() {
-  /** @private */ this.result_ = new tdd.ResultStruct(0, 0, 0);
-  /** @private {goog.log.Logger} */ this.logger_ = null;
-};
-tdd.ManualTestSuite.addImplementation(r5js.test.SchemeTestDriver);
+class SchemeTestDriver {
+    constructor() {
+        /** @private */ this.result_ = new ResultStruct(0, 0, 0);
+        /** @private {Logger} */ this.logger_ = null;
+    }
+
+    /** @override */
+    getType() {
+        return TestType.UNIT;
+    }
+
+    /** @override */
+    toString() {
+        return 'r5js.test.SchemeTestDriver';
+    }
+
+    /** @override */
+    estimateSize() {
+        return 52; // TODO bl how to do this without evaluating?
+    }
+
+    /** @override */
+    execute(logger) {
+        this.logger_ = logger;
+        const platform = curPlatform();
+        let result = this.result_;
+        const onWrite = this.onWrite_.bind(this);
+        /** @type {SchemeSources} */ var sources;
+        /** @type {Evaluator} */ var evaluator;
+
+        return Promise.all([
+            SchemeSources.get(),
+            platform.newEvaluator(InputPort.NULL,
+                new CallbackBackedPort(onWrite))]).then(function (resolved) {
+            sources = resolved[0];
+            evaluator = resolved[1];
+        }).then(function () {
+            return new TestFrameworkTest(
+                /** @type {!SchemeSources} */ (sources)).execute(logger);
+        }).then(function (result_) {
+            result = result.merge(result_);
+            return evaluator.evaluate(
+                sources.testFramework + sources.r5RSTests);
+        }).then(function () {
+            return evaluator.evaluate(
+                sources.testFramework + sources.negativeTests);
+        }).then(function () {
+            return evaluator.evaluate(
+                sources.testFramework + sources.otherTests);
+        }).then(function () {
+            return this.result_;
+        }, undefined /* opt_onRejected */, this);
+    }
+
+    /**
+     * @param {string} str
+     * @private
+     */
+    onWrite_(str) {
+        let result = stringToResultStruct_(str);
+        if (result) {
+            this.logger_.logRecord(new LogRecord(
+                result.getNumFailed()
+                    ? LogLevel.FAILURE
+                    : LogLevel.SUCCESS,
+                'r5js.test.SchemeTestDriver',
+                result.name_));
+            this.result_ = this.result_.merge(result);
+        } else if (result = stringToFailureMessage_(str)) {
+            this.logger_.logRecord(new LogRecord(
+                LogLevel.FAILURE,
+                'r5js.test.SchemeTestDriver',
+                result));
+        }
+    }
+}
+ManualTestSuite.addImplementation(SchemeTestDriver);
 
 
-/** @override */
-r5js.test.SchemeTestDriver.prototype.getType = function() {
-  return tdd.TestType.UNIT;
-};
-
-
-/** @override */
-r5js.test.SchemeTestDriver.prototype.toString = function() {
-  return 'r5js.test.SchemeTestDriver';
-};
-
-
-/** @override */
-r5js.test.SchemeTestDriver.prototype.estimateSize = function() {
-  return 52; // TODO bl how to do this without evaluating?
-};
-
-
-/** @override */
-r5js.test.SchemeTestDriver.prototype.execute = function(logger) {
-  this.logger_ = logger;
-  const platform = r5js.curPlatform();
-  let result = this.result_;
-  const onWrite = this.onWrite_.bind(this);
-  /** @type {r5js.test.SchemeSources} */ var sources;
-  /** @type {r5js.Evaluator} */ var evaluator;
-
-  return goog.Promise.all([
-    r5js.test.SchemeSources.get(),
-    platform.newEvaluator(r5js.InputPort.NULL,
-        new r5js.CallbackBackedPort(onWrite))]).then(function(resolved) {
-    sources = resolved[0];
-    evaluator = resolved[1];
-  }).then(function() {
-    return new r5js.test.SchemeTestDriver.TestFrameworkTest_(
-        /** @type {!r5js.test.SchemeSources} */ (sources)).execute(logger);
-  }).then(function(result_) {
-    result = result.merge(result_);
-    return evaluator.evaluate(
-        sources.testFramework + sources.r5RSTests);
-  }).then(function() {
-    return evaluator.evaluate(
-        sources.testFramework + sources.negativeTests);
-  }).then(function() {
-    return evaluator.evaluate(
-        sources.testFramework + sources.otherTests);
-  }).then(function() {
-    return this.result_;
-  }, undefined /* opt_onRejected */, this);
-};
-
-
-/**
- * @param {string} str
- * @private
- */
-r5js.test.SchemeTestDriver.prototype.onWrite_ = function(str) {
-  let result = r5js.test.SchemeTestDriver.stringToResultStruct_(str);
-  if (result) {
-    this.logger_.logRecord(new tdd.LogRecord(
-        result.getNumFailed() ? tdd.LogLevel.FAILURE : tdd.LogLevel.SUCCESS,
-        'r5js.test.SchemeTestDriver',
-        result.name_));
-    this.result_ = this.result_.merge(result);
-  } else if (result = r5js.test.SchemeTestDriver.stringToFailureMessage_(
-      str)) {
-    this.logger_.logRecord(new tdd.LogRecord(
-        tdd.LogLevel.FAILURE,
-        'r5js.test.SchemeTestDriver',
-        result));
-  }
-};
-
-
-
-/**
- * @param {string} name
- * @param {number} numSucceeded
- * @param {number} numFailed
- * @extends {tdd.ResultStruct}
- * @struct
- * @constructor
- * @private
- */
-r5js.test.SchemeTestDriver.ResultStruct_ = function(
-    name, numSucceeded, numFailed) {
-  r5js.test.SchemeTestDriver.ResultStruct_.base(
-      this, 'constructor', numSucceeded, numFailed, 0 /* TODO bl */);
-  /** @const @private */ this.name_ = name;
-};
-goog.inherits(r5js.test.SchemeTestDriver.ResultStruct_, tdd.ResultStruct);
-
+class ResultStruct_ extends ResultStruct {
+    /**
+     * @param {string} name
+     * @param {number} numSucceeded
+     * @param {number} numFailed
+     */
+    constructor(name, numSucceeded, numFailed) {
+        super(numSucceeded, numFailed, 0 /* TODO bl */);
+        /** @const @private */ this.name_ = name;
+    }
+}
 
 /**
  * Parses a Scheme test framework output like this:
  * (foo-tests (3 tests) (1 failed))
  * into a {@link tdd.ResultStruct}, returning null if the parse failed.
-* @param {string} str
- * @return {r5js.test.SchemeTestDriver.ResultStruct_}
+ * @param {string} str
+ * @return {ResultStruct_}
  * @private
  */
-r5js.test.SchemeTestDriver.stringToResultStruct_ = function(str) {
+function stringToResultStruct_(str) {
   const regex = /\((.+) \((\d+) tests\) \((\d+) failed\)\)/;
   const matches = regex.exec(str);
   if (!matches) {
@@ -153,9 +128,8 @@ r5js.test.SchemeTestDriver.stringToResultStruct_ = function(str) {
   const name = matches[1];
   const numSucceeded = parseInt(matches[2], 10);
   const numFailed = parseInt(matches[3], 10);
-  return new r5js.test.SchemeTestDriver.ResultStruct_(
-      name, numSucceeded, numFailed);
-};
+  return new ResultStruct_(name, numSucceeded, numFailed);
+}
 
 
 /**
@@ -166,7 +140,7 @@ r5js.test.SchemeTestDriver.stringToResultStruct_ = function(str) {
  * @return {?string}
  * @private
  */
-r5js.test.SchemeTestDriver.stringToFailureMessage_ = function(str) {
+function stringToFailureMessage_(str) {
   const match = /\(fail .+ \(input (.*)\) \(want (.*)\) \(got (.*)\)\)/.exec(str);
   if (!match) {
     return null;
@@ -175,89 +149,76 @@ r5js.test.SchemeTestDriver.stringToFailureMessage_ = function(str) {
   const want = match[2];
   const got = match[3];
   return 'input ' + input + ': want ' + want + ', got ' + got;
-};
+}
+
+/** @implements {ManualTestSuite} */
+class TestFrameworkTest {
+    /** @param {!SchemeSources} sources */
+    constructor(sources) {
+        /** @const @private */ this.sources_ = sources;
+        /** @private */ this.actualResult_ = new ResultStruct_('', 0, 0);
+        /** @private {Logger} */ this.logger_ = null;
+    }
+
+    /**
+     * Must be kept manually in sync with the expected results of
+     * test/unit-test-tests.scm.
+     * @param {!tdd.ResultStruct} result
+     * @return {boolean}
+     * @private
+     */
+    static resultIsExpected_(result) {
+        return result.getNumRun() === 3
+            && result.getNumSucceeded() === 2
+            && result.getNumFailed() === 1;
+    }
+
+    /**
+     * @param {string} str
+     * @private
+     */
+    onWrite_(str) {
+        const result = stringToResultStruct_(str);
+        if (result) {
+            this.logger_.logRecord(new LogRecord(
+                LogLevel.SUCCESS,
+                'r5js.test.SchemeTestDriver',
+                result.name_));
+            this.actualResult_ = this.actualResult_.merge(result);
+        }
+    }
+
+    /** @override */
+    getType() {
+        return TestType.UNIT;
+    }
 
 
+    /** @override */
+    estimateSize() {
+        return 1;
+    }
 
-/**
- * @param {!r5js.test.SchemeSources} sources
- * @implements {tdd.ManualTestSuite}
- * @struct
- * @constructor
- * @private
- */
-r5js.test.SchemeTestDriver.TestFrameworkTest_ = function(sources) {
-  /** @const @private */ this.sources_ = sources;
-  /** @private */
-  this.actualResult_ = new r5js.test.SchemeTestDriver.ResultStruct_('', 0, 0);
+    /** @override */
+    toString() {
+        return 'r5js.test.SchemeTestDriver.TestFrameworkTest_';
+    }
 
-  /** @private {goog.log.Logger} */ this.logger_ = null;
-};
-tdd.ManualTestSuite.addImplementation(
-    r5js.test.SchemeTestDriver.TestFrameworkTest_);
+    /** @override */
+    execute(logger) {
+        this.logger_ = logger;
+        return curPlatform().newEvaluator(
+            InputPort.NULL, new CallbackBackedPort(this.onWrite_.bind(this))
+        ).then(evaluator => evaluator.evaluate(this.sources_.testFramework + this.sources_.testFrameworkTests),
+            undefined /* opt_onRejected */, this
+        ).then(() => TestFrameworkTest.resultIsExpected_(this.actualResult_),
+            undefined /* opt_onRejected */, this
+        ).then(success => goog.Promise.resolve(success
+                ? new tdd.ResultStruct(1, 0, 0)
+                : new tdd.ResultStruct(0, 1, 0)),
+            undefined /* opt_onRejected */, this);
+    }
+}
+ManualTestSuite.addImplementation(TestFrameworkTest);
 
-
-/**
- * Must be kept manually in sync with the expected results of
- * test/unit-test-tests.scm.
- * @param {!tdd.ResultStruct} result
- * @return {boolean}
- * @private
- */
-r5js.test.SchemeTestDriver.TestFrameworkTest_.resultIsExpected_ =
-    function(result) {
-  return result.getNumRun() === 3 &&
-      result.getNumSucceeded() === 2 &&
-      result.getNumFailed() === 1;
-};
-
-
-/**
- * @param {string} str
- * @private
- */
-r5js.test.SchemeTestDriver.TestFrameworkTest_.prototype.onWrite_ = function(
-    str) {
-  const result = r5js.test.SchemeTestDriver.stringToResultStruct_(str);
-  if (result) {
-    this.logger_.logRecord(new tdd.LogRecord(
-        tdd.LogLevel.SUCCESS,
-        'r5js.test.SchemeTestDriver',
-        result.name_));
-    this.actualResult_ = this.actualResult_.merge(result);
-  }
-};
-
-
-/** @override */
-r5js.test.SchemeTestDriver.TestFrameworkTest_.prototype.getType = function() {
-        return tdd.TestType.UNIT;
-};
-
-
-/** @override */
-r5js.test.SchemeTestDriver.TestFrameworkTest_.prototype.estimateSize = function() {
-    return 1;
-};
-
-
-/** @override */
-r5js.test.SchemeTestDriver.TestFrameworkTest_.prototype.toString = function() {
-    return 'r5js.test.SchemeTestDriver.TestFrameworkTest_';
-};
-
-
-/** @override */
-r5js.test.SchemeTestDriver.TestFrameworkTest_.prototype.execute = function(logger) {
-    this.logger_ = logger;
-    return r5js.curPlatform().newEvaluator(
-        r5js.InputPort.NULL, new r5js.CallbackBackedPort(this.onWrite_.bind(this))
-    ).then(evaluator => evaluator.evaluate(this.sources_.testFramework + this.sources_.testFrameworkTests),
-        undefined /* opt_onRejected */, this
-    ).then(() => r5js.test.SchemeTestDriver.TestFrameworkTest_.resultIsExpected_(this.actualResult_),
-        undefined /* opt_onRejected */, this
-    ).then(success => goog.Promise.resolve(success
-            ? new tdd.ResultStruct(1, 0, 0)
-            : new tdd.ResultStruct(0, 1, 0)),
-        undefined /* opt_onRejected */, this);
-};
+exports = SchemeTestDriver;
