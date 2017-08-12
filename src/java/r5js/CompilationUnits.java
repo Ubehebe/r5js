4c985fca0c79623d4507d9c32da88b3664cf820b
Brendan Linn <brendan.linn@gmail.com>
2017-08-12 08:29:51 -0700

    [bazel] rm unused CompilationUnits.

--- src/java/r5js/CompilationUnits.java
+++ src/java/r5js/CompilationUnits.java
@@ -1,26 +1,9 @@
 package r5js;
 
-import static r5js.EntryPoint.ANDROID_MAIN;
-import static r5js.EntryPoint.HTML5_CONSOLE_MAIN;
 import static r5js.EntryPoint.TEST_MAIN;
 
 interface CompilationUnits {
-    static final CompilationUnit ANDROID_REPL = CompilationUnit.of(
-            "android.js", Platform.ANDROID)
-            .entryPoint(ANDROID_MAIN)
-            .build();
-
-    static final CompilationUnit ANDROID_TESTS = CompilationUnit.of(
-            "android-tests.js", Platform.ANDROID)
-            .entryPoint(TEST_MAIN)
-            .build();
-
-    static final CompilationUnit EMBEDDED_MAIN = CompilationUnit.of(
-            "embedded.js", Platform.EMBEDDED)
-            .entryPoint(EntryPoint.EMBEDDED_MAIN)
-            .build();
-
-    static final CompilationUnit HTML5_WORKER = CompilationUnit.of(
+    CompilationUnit HTML5_WORKER = CompilationUnit.of(
             "worker.js", Platform.HTML5)
             .entryPoint(EntryPoint.HTML5_WORKER)
             .build();
@@ -29,7 +12,7 @@ interface CompilationUnits {
      * This contains {@link r5js.EntryPoint#TEST_MAIN}, so it isn't suitable to serve
      * in a production app.
      */
-    static final CompilationUnit HTML5_DEV_CLIENT = CompilationUnit.of(
+    CompilationUnit HTML5_DEV_CLIENT = CompilationUnit.of(
             "html5-devclient.js", Platform.HTML5)
             .entryPoint(EntryPoint.TEST_MAIN)
             .entryPoint(EntryPoint.HTML5_REPL_MAIN)
@@ -44,28 +27,12 @@ interface CompilationUnits {
             })
             .build();
 
-    static final CompilationUnit HTML5_PRODUCTION_CLIENT = CompilationUnit.of(
-            "client.js", Platform.HTML5)
-            .entryPoint(HTML5_CONSOLE_MAIN)
-            .customCompilerOptions(options -> {
-                options.setDefineToStringLiteral(
-                        "r5js.platform.html5.Client.WORKER_SCRIPT",
-                        HTML5_WORKER.buildArtifactName);
-                return options;
-            })
-            .build();
-
-    static final CompilationUnit NASHORN_TESTS = CompilationUnit.of(
-            "nashorn-tests.js", Platform.NASHORN)
-            .entryPoint(TEST_MAIN)
-            .build();
-
-    static final CompilationUnit NODE_REPL = CompilationUnit.of(
+    CompilationUnit NODE_REPL = CompilationUnit.of(
             "node-repl.js", Platform.NODE)
             .entryPoint(EntryPoint.NODE_REPL_MAIN)
             .build();
 
-    static final CompilationUnit NODE_TESTS = CompilationUnit.of(
+    CompilationUnit NODE_TESTS = CompilationUnit.of(
             "node-tests.js", Platform.NODE)
             .entryPoint(TEST_MAIN)
             .build();
