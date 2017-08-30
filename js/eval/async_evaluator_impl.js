f8cb5f108d9800a52b0a39d3748f5bac3c63634e
Brendan Linn <brendan.linn@gmail.com>
2017-08-30 07:18:00 -0700

    eval: no need for AsyncEvaluator to be an interface.
    there's only one impl.

--- js/eval/async_evaluator_impl.js
+++ js/eval/async_evaluator_impl.js
@@ -1,6 +1,5 @@
 goog.module('r5js.async.Evaluator.Impl');
 
-const AsyncEvaluator = goog.require('r5js.async.Evaluator');
 const Evaluator = goog.require('r5js.Evaluator');
 const InputPort = goog.require('r5js.InputPort');
 const OutputPort = goog.require('r5js.OutputPort');
@@ -11,25 +10,29 @@ const boot = goog.require('r5js.boot');
 /**
  * @param {!InputPort=} inputPort
  * @param {!OutputPort=} outputPort
- * @return {!AsyncEvaluator}
+ * @return {!AsyncEvaluatorImpl}
  */
 function newAsyncEvaluator(inputPort=InputPort.NULL, outputPort=OutputPort.NULL) {
     const sources = new SchemeSources();
     const syncEvaluator = boot(sources.syntax, sources.procedures, inputPort, outputPort);
-    return new EvaluatorImpl(syncEvaluator);
+    return new AsyncEvaluatorImpl(syncEvaluator);
 }
 
-/**
- * AsyncEvaluator implementation that simply wraps a synchronous evaluator in promises.
- * @implements {AsyncEvaluator}
- */
-class EvaluatorImpl {
+/** Wraps a synchronous evaluator in promises. */
+class AsyncEvaluatorImpl {
     /** @param {!Evaluator} evaluator */
     constructor(evaluator) {
         /** @const @private */ this.evaluator_ = evaluator;
     }
 
-    /** @override */
+    /**
+     * @param {string} input Input to evaluate.
+     * @return {!Promise<string>}  If evaluation succeeds,
+     * this promise will be resolved with a string representation of the Scheme
+     * value (as if it was serialized with the {@code write} procedure).
+     * If evaluation fails, the promise will be rejected with an {@link r5js.Error}
+     * explaining what went wrong.
+     */
     evaluate(input) {
         try {
             return Promise.resolve(this.evaluator_.evaluate(input));
