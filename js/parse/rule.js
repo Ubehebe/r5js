49db22ed54c2904da06c91be704b75e5b3a89912
Brendan Linn <brendan.linn@gmail.com>
2018-02-25 11:46:38 -0800

    [ts] migrate r5js.DatumStream.

--- js/parse/rule.js
+++ js/parse/rule.js
@@ -1,7 +1,7 @@
 goog.module('r5js.parse.bnf.Rule');
 
 const {Datum} = require('/js/read/shim_collect_es6_sources.es6/node_modules/__main__/js/ast/datum');
-const DatumStream = goog.require('r5js.DatumStream');
+const {DatumStream} = require('/js/parse/shim_collect_es6_sources.es6/node_modules/__main__/js/parse/datum_stream');
 
 /** @interface */
 class Rule {
