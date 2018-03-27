9db28032d9d214f720118ab61947b14d443cd032
Brendan Linn <brendan.linn@gmail.com>
2018-03-26 21:07:46 -0700

    migrate //js/test:scheme_test to ts.
    that's the last real js file in the tree. now for the cleanup.

---
+++ js/test/scheme_test.ts
@@ -0,0 +1,81 @@
+import {SchemeSources} from "./scheme_sources";
+import {AsyncEvaluator} from "../eval/async_evaluator";
+import {NULL_INPUT_PORT} from "../io/input_port";
+import {CallbackBackedPort} from "../io/callback_backed_port";
+import {ResultStruct, stringToResultStruct, TestFramework} from "./test_framework";
+
+const sources = SchemeSources.get();
+const evaluator = new AsyncEvaluator(NULL_INPUT_PORT, new CallbackBackedPort(handleWriteFromScheme));
+let numFailures: number;
+
+/**
+ * Runs the unit tests written in Scheme (//scm/r5rs-tests.scm, etc.).
+ *
+ * The Scheme unit tests do not correctly propagate their counts (number of tests
+ * run/failed/succeeded) to jsunit. However, they do correctly propagate whether they succeeded
+ * or failed, and jsunit propagates this to bazel, so they are an effective pre-commit hook.
+ *
+ * TODO: write a Skylark scheme_test macro or rule that hides all the JS implementation details.
+ * It could just invoke node and have the Scheme tests write a proto or JSON.
+ * But we need better node support before doing that.
+ */
+describe("pure scheme tests", () => {
+  beforeEach(() => {
+    numFailures = 0;
+  });
+
+  it("should evaluate the test framework without problems", (done) => {
+    new TestFramework(sources).execute().then(result => {
+      expect(result).not.toBeNull();
+      done();
+    });
+  });
+
+  it("should run r5rs-tests.scm successfully", (done) => {
+    evaluator.evaluate(sources.testFramework + sources.r5RSTests).then(result => {
+      expect(result).not.toBeNull();
+      expect(numFailures).toBe(0);
+      done();
+    });
+  });
+
+  it("should run negative-tests.scm successfully", (done) => {
+    evaluator.evaluate(sources.testFramework + sources.negativeTests).then(result => {
+      expect(result).not.toBeNull();
+      expect(numFailures).toBe(0);
+      done();
+    });
+  });
+
+  it("should run other-tests.scm successfully", (done) => {
+    evaluator.evaluate(sources.testFramework + sources.otherTests).then(result => {
+      expect(result).not.toBeNull();
+      expect(numFailures).toBe(0);
+      done();
+    });
+  });
+});
+
+function handleWriteFromScheme(str) {
+  let result: ResultStruct | string | null = stringToResultStruct(str);
+  if (result) {
+    const success = result.numFailed === 0;
+    if (success) {
+      console.log(`[ok] r5js.test.SchemeTest ${result.name}`);
+    } else {
+      console.error(`[fail] r5js.test.SchemeTest ${result.name}`);
+    }
+  } else if (result = stringToFailureMessage(str)) {
+    ++numFailures;
+    console.error(`[fail] r5js.test.SchemeTest ${result}`);
+  }
+}
+
+/**
+ * Parses a Scheme test framework output like `(fail foo-tests (input (+ 1 1)) (want 3) (got 2))`
+ * into a string, returning null if the parse failed.
+ */
+function stringToFailureMessage(str: string): string | null {
+  const match = /\(fail .+ \(input (.*)\) \(want (.*)\) \(got (.*)\)\)/.exec(str);
+  return match && `input ${match[1]}: want ${match[2]}, got ${match[3]}`;
+}
---
+++ js/test/test_framework.ts
@@ -0,0 +1,68 @@
+import {SchemeSources} from "./scheme_sources";
+import {AsyncEvaluator} from "../eval/async_evaluator";
+import {NULL_INPUT_PORT} from "../io/input_port";
+import {CallbackBackedPort} from "../io/callback_backed_port";
+
+export class TestFramework {
+
+  private actualResult = new ResultStruct();
+
+  constructor(private readonly sources: SchemeSources) {}
+
+  private onWrite_(str: string) {
+    const result = stringToResultStruct(str);
+    if (result) {
+      console.log(`[ok] r5js.test.TestFramework ${result.name}`);
+      this.actualResult = this.actualResult.merge(result);
+    }
+  }
+
+  execute(): Promise<ResultStruct> {
+    const evaluator = new AsyncEvaluator(
+        NULL_INPUT_PORT, new CallbackBackedPort(output => this.onWrite_(output)));
+    return evaluator.evaluate(this.sources.testFramework + this.sources.testFrameworkTests)
+        .then(() => resultIsExpected(this.actualResult))
+        .then(success => success ? new ResultStruct(1, 0) : new ResultStruct(0, 1))
+        .then(result => Promise.resolve(result));
+  }
+}
+
+/** Must be kept manually in sync with the expected results of test/unit-test-tests.scm. */
+function resultIsExpected(result: ResultStruct): boolean {
+  return result.numRun() === 3
+      && result.numSucceeded === 2
+      && result.numFailed === 1;
+}
+
+/**
+ * Parses a Scheme test framework output like this: `(foo-tests (3 tests) (1 failed))` into a
+ * ResultStruct, returning null if the parse failed.
+ */
+export function stringToResultStruct(str): ResultStruct | null {
+  const regex = /\((.+) \((\d+) tests\) \((\d+) failed\)\)/;
+  const matches = regex.exec(str);
+  if (!matches) {
+    return null;
+  }
+  const name = matches[1];
+  const numSucceeded = parseInt(matches[2], 10);
+  const numFailed = parseInt(matches[3], 10);
+  return new ResultStruct(numSucceeded, numFailed, name);
+}
+
+export class ResultStruct {
+  constructor(
+      readonly numSucceeded: number = 0,
+      readonly numFailed: number = 0,
+      readonly name?: string) {}
+
+  numRun(): number {
+    return this.numSucceeded + this.numFailed;
+  }
+
+  merge(other: ResultStruct): ResultStruct {
+    return new ResultStruct(
+        this.numSucceeded + other.numSucceeded,
+        this.numFailed + other.numFailed);
+  }
+}
