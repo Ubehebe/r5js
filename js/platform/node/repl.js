c43abcf39041bd291e666f361257b5fbb481ea65
Brendan Linn <brendan.linn@gmail.com>
2019-03-28 07:30:44 -0700

    Revert "platform/node: rm repl.js and terminal.js."
    This reverts commit 7189c59addd7967c3f24214f7954e054f9d73125.
    
    these files were removed in order to speed the ts migration (see the
    commit's description). now that that is accomplished, i would like to
    resurrect the node functionality before dealing with the browser
    functionality. ideally, there should be a runnable bazel target that
    will drop into a scheme repl.

---
+++ js/platform/node/repl.js
@@ -0,0 +1,23 @@
+goog.module('r5js.platform.node.repl');
+
+const CallbackBackedPort = goog.require('r5js.CallbackBackedPort');
+const InputPort = goog.require('r5js.InputPort');
+const Node = goog.require('r5js.platform.Node');
+const Repl = goog.require('r5js.Repl');
+const Terminal = goog.require('r5js.platform.node.Terminal');
+const curPlatform = goog.require('r5js.curPlatform');
+
+/** The main REPL method. */
+function repl() {
+    const platform = /** @type {!Node} */ (curPlatform());
+    /** @type {Terminal} */ let terminal = null;
+    const stdin = InputPort.NULL;
+    const stdout = new CallbackBackedPort(output => terminal.print(output));
+    const evaluator = platform.newEvaluator(stdin, stdout);
+    terminal = platform.getTerminal();
+    new Repl(terminal, evaluator).start();
+}
+
+goog.exportSymbol('r5js.repl.main', repl);
+// nodejs hack. See comment in goog.promise.testSuiteAdapter.
+goog.exportSymbol('setTimeout', setTimeout);
