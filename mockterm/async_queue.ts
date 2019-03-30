3d5f541c02173d04a7b3f115dff9c991a0ad3ea0
Brendan Linn <brendan.linn@gmail.com>
2019-03-29 18:02:50 -0700

    mockterm: port async_queue.js to ts.
    TODO: this class seems pretty trivial considering modern
    apis (promise, etc.). it can probable be replaced.

---
+++ mockterm/async_queue.ts
@@ -0,0 +1,35 @@
+export class AsyncQueue {
+
+  private readonly queue: any[] = [];
+  private running = false;
+
+  // TODO: a more flexible design would be to specify a latency for every object enqueued.
+  constructor(private readonly latency: number) {}
+
+  isRunning(): boolean {
+    return this.running;
+  }
+
+  enqueue(cb: any): this {
+    this.queue.push(cb);
+    if (!this.running) {
+      setTimeout(() => this.run(), this.latency);
+      this.running = true;
+    }
+    return this;
+  }
+
+  dequeue(): any {
+    return this.queue.shift();
+  }
+
+  run() {
+    const todo = this.dequeue();
+    if (todo) {
+      todo();
+      setTimeout(() => this.run(), this.latency);
+    } else {
+      this.running = false;
+    }
+  }
+}
