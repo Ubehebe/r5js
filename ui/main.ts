daf3bc86fdfcae0de4a5349067133cab16faca8c
Brendan Linn <brendan.linn@gmail.com>
2019-04-03 11:34:00 -0700

    ui/main.ts: simplify main method.
    manualResize() is no longer needed. the MockTerminal ctor sizes its
    text appropriately without a resize event.

--- ui/main.ts
+++ ui/main.ts
@@ -6,11 +6,6 @@ import {MockTerminal} from "./mockterm";
 document.addEventListener('DOMContentLoaded', main, false);
 
 function main() {
-  manualResize();  // get the nav and hero text looking good
-  setupTerminal();
-}
-
-function setupTerminal() {
   const sources = new SchemeSources();
   const evaluator = boot(sources.syntax, sources.procedures);
   new MockTerminal({
@@ -25,13 +20,8 @@ function setupTerminal() {
     inputCompleteHandler: isLineComplete,
     numColumns: 80,
     charLatency: 5,
-    lineLatency: 500})
+    lineLatency: 500,
+  })
       .println(";; r5js") // TODO display banner
       .start();
-}
-
-function manualResize() {
-  const e = document.createEvent('Event');
-  e.initEvent('resize', false, false);
-  dispatchEvent(e);
 }
